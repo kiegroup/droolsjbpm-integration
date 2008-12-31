6ec84f97419da7a1e6b5a66aa055b5dbf33aa87d
Mark Proctor <mdproctor@gmail.com>
2008-12-31 16:49:41 +0000

    JBRULES-1903 Pipeline support for DataLoaders and Feeders -Added XStream support -Added Service support
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@24513 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-pipeline/drools-transformer-xstream/src/test/java/org/drools/runtime/pipeline/impl/DroolsXStreamStatefulSessionTest.java
+++ drools-pipeline/drools-transformer-xstream/src/test/java/org/drools/runtime/pipeline/impl/DroolsXStreamStatefulSessionTest.java
@@ -23,7 +23,7 @@ import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.dataloader.DataLoaderFactory;
 import org.drools.runtime.dataloader.StatefulKnowledgeSessionDataLoader;
 import org.drools.runtime.dataloader.impl.StatefulKnowledgeSessionDataLoaderImpl;
-import org.drools.runtime.dataloader.impl.StatefulKnowledgeSessionReceiverAdapter;
+import org.drools.runtime.dataloader.impl.EntryPointReceiverAdapter;
 import org.drools.runtime.pipeline.Expression;
 import org.drools.runtime.pipeline.PipelineFactory;
 import org.drools.runtime.pipeline.Splitter;
@@ -55,7 +55,7 @@ public class DroolsXStreamStatefulSessionTest extends TestCase {
 
         XStream xstream = new XStream();
         Transformer transformer = PipelineFactory.newXStreamTransformer( xstream );
-        transformer.addReceiver( PipelineFactory.newStatefulKnowledgeSessionReceiverAdapter() );
+        transformer.addReceiver( PipelineFactory.newEntryPointReceiverAdapter() );
 
         StatefulKnowledgeSessionDataLoader dataLoader = DataLoaderFactory.newStatefulKnowledgeSessionDataLoader( ksession,
                                                                                                                  transformer );
@@ -94,7 +94,7 @@ public class DroolsXStreamStatefulSessionTest extends TestCase {
         transformer.addReceiver( expression );
         Splitter splitter = PipelineFactory.newIterateSplitter();
         expression.addReceiver( splitter );
-        splitter.addReceiver( PipelineFactory.newStatefulKnowledgeSessionReceiverAdapter() );
+        splitter.addReceiver( PipelineFactory.newEntryPointReceiverAdapter() );
 
         StatefulKnowledgeSessionDataLoader dataLoader = DataLoaderFactory.newStatefulKnowledgeSessionDataLoader( ksession,
                                                                                                                  transformer );
