792e0357480193201890cfcbd3786f277b29d300
Mark Proctor <mdproctor@gmail.com>
2010-01-09 05:19:13 +0000

    JBRULES-2351 OSGi Ready -refactor of dir structures, plus first service registrations
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30995 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-container/drools-osgi/org.drools.osgi.impl/src/main/java/org/drools/osgi/impl/Activator.java
@@ -0,0 +1,30 @@
+package org.drools.osgi.impl;
+
+import java.util.Hashtable;
+
+import org.drools.builder.KnowledgeBuilderProvider;
+import org.drools.builder.impl.KnowledgeBuilderProviderImpl;
+import org.drools.io.ResourceProvider;
+import org.drools.io.impl.ResourceProviderImpl;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator {
+    private ServiceRegistration kbuilderReg;
+    private ServiceRegistration resourceReg;
+
+    public void start(BundleContext bc) throws Exception {
+        this.kbuilderReg = bc.registerService( KnowledgeBuilderProvider.class.getName(),
+                                               new KnowledgeBuilderProviderImpl(),
+                                               new Hashtable() );
+        this.resourceReg = bc.registerService( ResourceProvider.class.getName(),
+                                               new ResourceProviderImpl(),
+                                               new Hashtable() );
+        System.out.println( "registered" );
+    }
+
+    public void stop(BundleContext bc) throws Exception {
+        this.kbuilderReg.unregister();
+        this.resourceReg.unregister();
+    }
+}
