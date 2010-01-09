4bd97f0d923a95b6aee6fc45493890f78e564901
Mark Proctor <mdproctor@gmail.com>
2010-01-09 05:33:44 +0000

    JBRULES-2351 OSGi Ready -refactor of dir structures, plus first service registrations
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@30996 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-container/drools-osgi/org.drools.osgi.api/src/main/java/org/drools/osgi/api/Activator.java
@@ -0,0 +1,19 @@
+package org.drools.osgi.api;
+
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext arg0) throws Exception {
+        
+    }
+
+    public void stop(BundleContext arg0) throws Exception {
+        
+    }
+
+}
