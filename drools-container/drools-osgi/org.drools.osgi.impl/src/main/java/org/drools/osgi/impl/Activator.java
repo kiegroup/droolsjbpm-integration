7d054b96a9cd5718c9c7b501cd5110df6a7af744
Mark Proctor <mdproctor@gmail.com>
2010-01-10 06:28:30 +0000

    JBRULES-2351 OSGi Ready -Classloader reworking, to make sure it can always find the user classes as well as the drools classes.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31000 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-container/drools-osgi/org.drools.osgi.impl/src/main/java/org/drools/osgi/impl/Activator.java
+++ drools-container/drools-osgi/org.drools.osgi.impl/src/main/java/org/drools/osgi/impl/Activator.java
@@ -2,17 +2,22 @@ package org.drools.osgi.impl;
 
 import java.util.Hashtable;
 
+import org.drools.KnowledgeBaseProvider;
 import org.drools.builder.KnowledgeBuilderProvider;
 import org.drools.builder.impl.KnowledgeBuilderProviderImpl;
+import org.drools.impl.KnowledgeBaseProviderImpl;
 import org.drools.io.ResourceProvider;
 import org.drools.io.impl.ResourceProviderImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-public class Activator implements BundleActivator {
+public class Activator
+    implements
+    BundleActivator {
     private ServiceRegistration kbuilderReg;
     private ServiceRegistration resourceReg;
+    private ServiceRegistration kbaseReg;
 
     public void start(BundleContext bc) throws Exception {
         this.kbuilderReg = bc.registerService( KnowledgeBuilderProvider.class.getName(),
@@ -21,11 +26,15 @@ public class Activator implements BundleActivator {
         this.resourceReg = bc.registerService( ResourceProvider.class.getName(),
                                                new ResourceProviderImpl(),
                                                new Hashtable() );
-        System.out.println( "registered" );
+        this.kbaseReg = bc.registerService( KnowledgeBaseProvider.class.getName(),
+                                            new KnowledgeBaseProviderImpl(),
+                                            new Hashtable() );
+        System.out.println( "impl registered" );
     }
 
     public void stop(BundleContext bc) throws Exception {
         this.kbuilderReg.unregister();
         this.resourceReg.unregister();
+        this.kbaseReg.unregister();
     }
 }
