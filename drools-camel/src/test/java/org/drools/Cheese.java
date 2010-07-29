b5c27f9cf538ae16058675dfc9a81b328d9aa30c
Toni Rikkola <toni@rikkola.net>
2010-07-29 17:14:26 +0000

    updating serialVersionUID to 510l
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@34317 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-camel/src/test/java/org/drools/Cheese.java
+++ drools-camel/src/test/java/org/drools/Cheese.java
@@ -46,7 +46,7 @@ public class Cheese
     /**
      *
      */
-    private static final long serialVersionUID = 400L;
+    private static final long serialVersionUID = 510l;
     private String            type;
     private int               price;
     private int               oldPrice;
