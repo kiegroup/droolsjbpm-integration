baa7a41022b732e1a13e3a27934192f812d0324e
Alessandro Lazarotti <alessandrolt2@gmail.com>
2012-07-13 11:53:37 -0300

    BZ-840040 BZ-840043 JBPM-3714 Implementing Shared Entity Manager support

--- drools-container/drools-spring/src/main/java/org/drools/container/spring/beans/persistence/DroolsSpringJpaManager.java
+++ drools-container/drools-spring/src/main/java/org/drools/container/spring/beans/persistence/DroolsSpringJpaManager.java
@@ -45,10 +45,24 @@ public class DroolsSpringJpaManager
 
     private boolean              internalAppScopedEntityManager;
 
+    /**
+     * true for JTA transaction and false for non-JTA transaction. 
+     */
+    private boolean isJTA;
+
+    /**
+     * "IS_JTA_TRANSACTION" is used to pass a boolean flag in.
+     * EnvironmentName.IS_JTA_TRANSACTION should added by BRMS team.
+     * These are signed jar and need to include the whole package in the patch jar. 
+     * 
+     * @param env
+     */
     public DroolsSpringJpaManager(Environment env) {
         this.env = env;
         this.emf = (EntityManagerFactory) env.get( EnvironmentName.ENTITY_MANAGER_FACTORY );
-
+        isJTA = true;
+        Boolean bool = (Boolean) env.get("IS_JTA_TRANSACTION");
+        if (bool != null) isJTA = bool.booleanValue();
         getApplicationScopedPersistenceContext(); // we create this on initialisation so that we own the EMF reference
                                                   // otherwise Spring will close it after the transaction finishes
     }
@@ -78,13 +92,13 @@ public class DroolsSpringJpaManager
             }
         }
         if ( TransactionSynchronizationManager.isActualTransactionActive() ) {
-            this.appScopedEntityManager.joinTransaction();
+            if (isJTA) this.appScopedEntityManager.joinTransaction();
         }
-        return new JpaPersistenceContext( this.appScopedEntityManager );
+        return new JpaPersistenceContext( this.appScopedEntityManager, isJTA );
     }
 
     public PersistenceContext getCommandScopedPersistenceContext() {
-        return  new JpaPersistenceContext( (EntityManager) this.env.get( EnvironmentName.CMD_SCOPED_ENTITY_MANAGER ) );
+        return  new JpaPersistenceContext( (EntityManager) this.env.get( EnvironmentName.CMD_SCOPED_ENTITY_MANAGER ), isJTA );
     }
 
     public void beginCommandScopedEntityManager() {
@@ -104,8 +118,10 @@ public class DroolsSpringJpaManager
                           em );
         }
 
-        this.getCommandScopedPersistenceContext().joinTransaction();
-        this.appScopedEntityManager.joinTransaction();
+        if (isJTA) {
+        	this.getCommandScopedPersistenceContext().joinTransaction();
+        	this.appScopedEntityManager.joinTransaction();
+        }
 
     }
 
