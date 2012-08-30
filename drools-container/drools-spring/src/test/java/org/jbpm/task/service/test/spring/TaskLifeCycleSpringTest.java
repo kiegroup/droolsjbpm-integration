622092f4cf03caab0c44e4b82975ca5df421e101
Marco Rietveld <mrietvel@redhat.com>
2012-08-30 22:13:17 +0200

    Revert "- Ignoring Task Test Server Test inside integration due wrong configurations"

--- drools-container/drools-spring/src/test/java/org/jbpm/task/service/test/spring/TaskLifeCycleSpringTest.java
+++ drools-container/drools-spring/src/test/java/org/jbpm/task/service/test/spring/TaskLifeCycleSpringTest.java
@@ -2,22 +2,17 @@ package org.jbpm.task.service.test.spring;
 
 import static org.jbpm.task.service.base.async.TaskLifeCycleBaseAsyncTest.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TaskLifeCycleSpringTest extends BaseSpringTest {
 
-    @Ignore
+    @Test
     public void testLifeCycle() throws Exception {    
         runTestLifeCycle(client, users, groups);
     }
     
-    @Ignore
+    @Test
     public void testLifeCycleMultipleTasks() throws Exception { 
         runTestLifeCycleMultipleTasks(client, users, groups);
-    }
-    @Test
-    public void dummyTest(){
-    
     }
 }
--- drools-container/drools-spring/src/test/java/org/jbpm/task/service/test/spring/TaskServiceBaseUserGroupCallbackSpringTest.java
+++ drools-container/drools-spring/src/test/java/org/jbpm/task/service/test/spring/TaskServiceBaseUserGroupCallbackSpringTest.java
@@ -1,27 +1,21 @@
 package org.jbpm.task.service.test.spring;
 
 import static org.jbpm.task.service.base.async.TaskServiceBaseUserGroupCallbackAsyncTest.*;
-import org.junit.Ignore;
 
 import org.junit.Test;
 public class TaskServiceBaseUserGroupCallbackSpringTest extends BaseSpringNoUserGroupSetupTest {
     
     @Test
-    public void dummyTest(){
-    
-    }
-    
-    @Ignore
     public void testTasksOwnedQueryWithI18N() { 
         runTestTasksOwnedQueryWithI18N(client, users, groups);
     }
 
-    @Ignore
+    @Test
     public void testPotentialOwnerQueries() { 
         runTestPotentialOwnerQueries(client, users, groups);
     }
 
-    @Ignore
+    @Test
     public void testPeopleAssignmentQueries() { 
         runTestPeopleAssignmentQueries(client, users, groups, taskSession);
     }
