1d0909e061fb0e2a17f7aa6783094081cb1e015c
Maciej Swiderski <mswiders@redhat.com>
2012-09-05 18:33:33 +0200

    added poisson distribution type for time generation

---
+++ src/main/java/org/jbpm/simulation/impl/time/PoissonTimeGenerator.java
@@ -0,0 +1,32 @@
+package org.jbpm.simulation.impl.time;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.math.random.RandomData;
+import org.apache.commons.math.random.RandomDataImpl;
+import org.jbpm.simulation.TimeGenerator;
+import org.jbpm.simulation.util.SimulationConstants;
+import org.jbpm.simulation.util.SimulationUtils;
+
+public class PoissonTimeGenerator implements TimeGenerator {
+
+    private Map<String, Object> data;
+    private static RandomData generator = new RandomDataImpl();
+    private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
+    
+    public PoissonTimeGenerator(Map<String, Object> data) {
+        this.data = data;
+    }
+
+
+
+    public long generateTime() {
+        TimeUnit tu = SimulationUtils.getTimeUnit(data);
+        long mean = SimulationUtils.asLong(data.get(SimulationConstants.MEAN));
+        mean = timeUnit.convert(mean, tu);
+                
+        return  (long) generator.nextPoisson(mean);
+    }
+
+}
