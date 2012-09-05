c014d19764a42f2bde3278b83aa05dac38e96a18
Tihomir Surdilovic <tsurdilo@redhat.com>
2012-09-05 12:14:32 -0400

    updated to use bps spec model

--- src/main/java/org/jbpm/simulation/impl/BPMN2SimulationDataProvider.java
+++ src/main/java/org/jbpm/simulation/impl/BPMN2SimulationDataProvider.java
@@ -1,7 +1,6 @@
 package org.jbpm.simulation.impl;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
@@ -12,104 +11,108 @@ import org.eclipse.bpmn2.Definitions;
 import org.eclipse.bpmn2.ExtensionAttributeValue;
 import org.eclipse.bpmn2.FlowElement;
 import org.eclipse.bpmn2.FlowElementsContainer;
-import org.eclipse.bpmn2.Process;
-import org.eclipse.bpmn2.RootElement;
+import org.eclipse.bpmn2.Relationship;
 import org.eclipse.emf.ecore.util.FeatureMap;
-import org.eclipse.emf.ecore.util.FeatureMapUtil.FeatureEList;
+import org.jboss.drools.CostParameters;
+import org.jboss.drools.DecimalParameterType;
 import org.jboss.drools.DroolsPackage;
-import org.jboss.drools.MetadataType;
-import org.jboss.drools.MetaentryType;
+import org.jboss.drools.ElementParametersType;
+import org.jboss.drools.FloatingParameterType;
+import org.jboss.drools.NormalDistributionType;
+import org.jboss.drools.Parameter;
+import org.jboss.drools.ParameterValue;
+import org.jboss.drools.ProcessAnalysisDataType;
+import org.jboss.drools.RandomDistributionType;
+import org.jboss.drools.ResourceParameters;
+import org.jboss.drools.Scenario;
+import org.jboss.drools.UniformDistributionType;
 import org.jbpm.simulation.SimulationDataProvider;
 import org.jbpm.simulation.util.BPMN2Utils;
 import org.jbpm.simulation.util.SimulationConstants;
 import org.jbpm.simulation.util.SimulationUtils;
 
 public class BPMN2SimulationDataProvider implements SimulationDataProvider {
+    private Definitions def;
 
-    private FlowElementsContainer container;
-    private Map<String, FlowElement> localCache = new HashMap<String, FlowElement>();
-    
-    public BPMN2SimulationDataProvider(FlowElementsContainer container) {
-        this.container = container;
+    public BPMN2SimulationDataProvider(Definitions def) {
+        this.def = def;
     }
     
     public BPMN2SimulationDataProvider(String bpmn2xml) {
-
-        this(new ByteArrayInputStream(getBytes(bpmn2xml)));
-
+    	this.def = BPMN2Utils.getDefinitions(new ByteArrayInputStream(getBytes(bpmn2xml)));
     }
     
-    public BPMN2SimulationDataProvider(InputStream bpmn2Stream) {
-
-        Definitions defs = BPMN2Utils.getDefinitions(bpmn2Stream);
-        List<RootElement> rootElements = defs.getRootElements();
-        for (RootElement root : rootElements) {
-            if (root instanceof Process) {
-                // TODO add support for multiple processes in definition
-                this.container = (Process) root;
-                break;
-            }
-        }
-    }
     public Map<String, Object> getSimulationDataForNode(Node node) {
         String nodeId = (String) node.getMetaData().get("UniqueId");
-        
         return getSimulationDataForNode(nodeId);
     }
     public Map<String, Object> getSimulationDataForNode(
             String nodeId) {
-        
-        FlowElement foundElement = null;
-        if (localCache.containsKey(nodeId)) {
-            foundElement = localCache.get(nodeId);
-        } else {
-            foundElement = findElementInContainer(container, nodeId);
-            localCache.put(nodeId, foundElement);
-        }
         Map<String, Object> properties = new HashMap<String, Object>();
-        // extract required properties from flow element and put them into a map
-        List<ExtensionAttributeValue> extensions = foundElement.getExtensionValues();
-        for (ExtensionAttributeValue extAttrValue : extensions) {
-            FeatureMap extensionElements = extAttrValue.getValue();
-           
-            
-            @SuppressWarnings("unchecked")
-            
-            FeatureEList<MetadataType> metadataTypeExtensions = (FeatureEList<MetadataType>) extensionElements
-                                                 .get(DroolsPackage.Literals.DOCUMENT_ROOT__METADATA, true);
-            if(metadataTypeExtensions != null && metadataTypeExtensions.size() > 0) {
-             MetadataType metaType = metadataTypeExtensions.get(0);
-             for(Object metaEntryObj : metaType.getMetaentry()) {
-                 MetaentryType entry = (MetaentryType) metaEntryObj;
-                 if(entry.getName() != null && entry.getName().equals("staffavailability")) {
-                     properties.put("staffavailability", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("workinghours")) {
-                     properties.put("workinghours", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("costpertimeunit")) {
-                     properties.put("costpertimeunit", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("duration")) {
-                     properties.put("duration", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("timeunit")) {
-                     properties.put("timeunit", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("range")) {
-                     properties.put("range", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("standarddeviation")) {
-                     properties.put("standarddeviation", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("distributiontype")) {
-                     properties.put("distributiontype", entry.getValue());
-                 }
-                 if(entry.getName() != null && entry.getName().equals("probability")) {
-                     properties.put("probability", entry.getValue());
-                 }
-             }
+        Scenario scenario = getDefaultScenario(def);
+        if(scenario != null) {
+        	String baseTimeUnitValue = "";
+        	String baseCurrencyUnitValue = "";
+        	if(scenario.getScenarioParameters() != null) {
+        		baseCurrencyUnitValue = scenario.getScenarioParameters().getBaseCurrencyUnit();
+        		baseTimeUnitValue = scenario.getScenarioParameters().getBaseTimeUnit().getName();
             }
+        	
+        	if(scenario.getElementParameters() != null) {
+        		for(ElementParametersType eleType : scenario.getElementParameters()) {
+        			if(eleType.getElementId().equals(nodeId)) {
+        				if(eleType.getControlParameters() != null && eleType.getControlParameters().getProbability() != null) {
+        					FloatingParameterType valType = (FloatingParameterType) eleType.getControlParameters().getProbability().getParameterValue().get(0);
+                			properties.put("probability", valType.getValue());
+        				}
+        				if(eleType.getTimeParameters() != null) {
+        					if(eleType.getTimeParameters().getProcessingTime() != null) {
+        						Parameter processingTime = eleType.getTimeParameters().getProcessingTime();
+                    			ParameterValue paramValue =  processingTime.getParameterValue().get(0);
+                    			if(paramValue instanceof NormalDistributionType) {
+                    				NormalDistributionType ndt = (NormalDistributionType) paramValue;
+                    				properties.put("standarddeviation", ndt.getStandardDeviation());
+                    				properties.put("distributiontype", "normal");
+                    			} else if(paramValue instanceof UniformDistributionType) {
+                    				UniformDistributionType udt = (UniformDistributionType) paramValue;
+                    				properties.put("max", udt.getMax());
+                    				properties.put("min", udt.getMin());
+                    				properties.put("distributiontype", "uniform");
+                    			} else if(paramValue instanceof RandomDistributionType) {
+                    				RandomDistributionType rdt = (RandomDistributionType) paramValue;
+                    				properties.put("max", rdt.getMax());
+                    				properties.put("min", rdt.getMin());
+                    				properties.put("distributiontype", "random");
+                    			}
+                    			if(eleType.getTimeParameters().getTimeUnit() != null) {
+                    				properties.put("timeunit", eleType.getTimeParameters().getTimeUnit().getName());
+                    			} else {
+                    				properties.put("timeunit", baseTimeUnitValue);
+                    			}
+        					}
+        				}
+        				if(eleType.getCostParameters() != null) {
+        					CostParameters costParams = eleType.getCostParameters();
+        					if(costParams.getUnitCost() != null) {
+        						DecimalParameterType unitCostVal = (DecimalParameterType) costParams.getUnitCost().getParameterValue().get(0);
+        						properties.put("unitcost", unitCostVal.getValue().toString()); 
+        					}
+        					properties.put("currency", costParams.getCurrencyUnit() == null ? baseCurrencyUnitValue : costParams.getCurrencyUnit());
+        				}
+        				if(eleType.getResourceParameters() != null) {
+        					ResourceParameters resourceParams = eleType.getResourceParameters();
+        					if(resourceParams.getQuantity() != null) {
+        						FloatingParameterType quantityVal = (FloatingParameterType) resourceParams.getQuantity().getParameterValue().get(0);
+    	            			properties.put("quantity", quantityVal.getValue()); 
+        					}
+        					if(resourceParams.getWorkinghours() != null) {
+        						FloatingParameterType workingHoursVal = (FloatingParameterType) resourceParams.getWorkinghours().getParameterValue().get(0);
+    	            			properties.put("workinghours", workingHoursVal.getValue()); 
+        					}
+        				}
+        			}
+        		}
+        	}
         }
         return properties;
     }
@@ -155,5 +158,24 @@ public class BPMN2SimulationDataProvider implements SimulationDataProvider {
         } catch (UnsupportedEncodingException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
+    }
+    
+    private Scenario getDefaultScenario(Definitions def) {
+    	if(def.getRelationships() != null && def.getRelationships().size() > 0) {
+        	// current support for single relationship
+        	Relationship relationship = def.getRelationships().get(0);
+        	for(ExtensionAttributeValue extattrval : relationship.getExtensionValues()) {
+                FeatureMap extensionElements = extattrval.getValue();
+                @SuppressWarnings("unchecked")
+                List<ProcessAnalysisDataType> processAnalysisExtensions = (List<ProcessAnalysisDataType>) extensionElements.get(DroolsPackage.Literals.DOCUMENT_ROOT__PROCESS_ANALYSIS_DATA, true);
+                if(processAnalysisExtensions != null && processAnalysisExtensions.size() > 0) {
+                	ProcessAnalysisDataType processAnalysis = processAnalysisExtensions.get(0);
+                	if(processAnalysis.getScenario() != null && processAnalysis.getScenario().size() > 0) {
+                		return processAnalysis.getScenario().get(0);
+                	}
+                }
+        	}
+        }
+    	return null;
     }
 }
