105ed6aec66b40c02f35a516762e742571e4f832
mariofusco <mario.fusco@gmail.com>
2012-11-20 17:11:12 +0100

    [JBRULES-3668] sample projects to generate a kjar and use it inside a kcontainer

---
+++ drools-kproject-example/src/test/java/org/drools/kproject/KProjectTest.java
@@ -0,0 +1,42 @@
+package org.drools.kproject;
+
+import org.kie.KnowledgeBase;
+import org.kie.KnowledgeBaseFactory;
+import org.kie.definition.type.FactType;
+import org.kie.runtime.StatefulKnowledgeSession;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import static junit.framework.Assert.assertEquals;
+
+public class KProjectTest {
+
+    @Test @Ignore
+    public void testKJar() throws Exception {
+        // Simulate having the jar in the classpath
+        URLClassLoader urlClassLoader = new URLClassLoader( new URL[] { new java.io.File("drools-maven-example/target/classes/test.jar").toURI().toURL() } );
+        Thread.currentThread().setContextClassLoader( urlClassLoader );
+
+        StatefulKnowledgeSession ksession = KnowledgeBaseFactory.getStatefulKnowlegeSession("org.test.KSession1");
+        useKSession(ksession);
+    }
+
+    private void useKSession(StatefulKnowledgeSession ksession) throws InstantiationException, IllegalAccessException {
+        KnowledgeBase kbase = ksession.getKnowledgeBase();
+        FactType aType = kbase.getFactType( "org.drools.test", "FactA" );
+        Object a = aType.newInstance();
+        FactType bType = kbase.getFactType( "org.drools.test", "FactB" );
+        Object b = bType.newInstance();
+        aType.set( a, "fieldB", b );
+        bType.set( b, "fieldA", a );
+
+        ksession.insert( a );
+        ksession.insert( b );
+
+        int rules = ksession.fireAllRules();
+        assertEquals( 1, rules );
+    }
+}
