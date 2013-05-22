1c40eafc06568fc43b35cb4e15ef1d37ada9b3f4
Marco Rietveld <mrietvel@redhat.com>
2013-05-22 15:04:07 +0200

    task rest call cleanup

--- kie-remote/kie-services-remote/src/main/java/org/kie/services/remote/rest/TaskResource.java
+++ kie-remote/kie-services-remote/src/main/java/org/kie/services/remote/rest/TaskResource.java
@@ -20,7 +20,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 
 import org.jboss.resteasy.spi.BadRequestException;
@@ -38,6 +37,7 @@ import org.kie.services.client.serialization.jaxb.JaxbCommandsRequest;
 import org.kie.services.client.serialization.jaxb.JaxbCommandsResponse;
 import org.kie.services.client.serialization.jaxb.impl.JaxbTaskSummaryListResponse;
 import org.kie.services.remote.cdi.ProcessRequestBean;
+import org.kie.services.remote.rest.jaxb.JaxbGenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,8 +56,22 @@ public class TaskResource extends ResourceBase {
     @Inject
     private IdentityProvider identityProvider;
     
-    private static String[] allowedOperations = { "activate", "claim", "claimnextavailable", "complete", "delegate", "exit",
-            "fail", "forward", "release", "resume", "skip", "start", "stop", "suspend", "nominate" };
+    private static String[] allowedOperations = { 
+        "activate", 
+        "claim", 
+        "claimnextavailable", 
+        "complete", 
+        "delegate", 
+        "exit",
+        "fail", 
+        "forward", 
+        "release", 
+        "resume", 
+        "skip", 
+        "start", 
+        "stop", 
+        "suspend", 
+        "nominate" };
 
     @POST
     @Consumes(MediaType.APPLICATION_XML)
@@ -67,20 +81,51 @@ public class TaskResource extends ResourceBase {
         return processJaxbCommandsRequest(cmdsRequest, processRequestBean);
     }
 
+    private static String [] allowedQueryParams = {
+        "workItemId", 
+        "taskId", 
+        "businessAdministrator", 
+        "potentialOwner",
+        "status",
+        "taskOwner",
+        "processInstanceId",
+        "language"
+    };
+    
     @GET
     @Produces(MediaType.APPLICATION_XML)
     @Path("/query")
     public JaxbTaskSummaryListResponse query(@Context UriInfo uriInfo) {
-        MultivaluedMap<String, String> multiParams = uriInfo.getQueryParameters();
         Map<String, List<String>> params = getRequestParams(request);
-        List<Long> workItemIdList = getLongListParam("workItemId", false, params, "query", true);
-        List<Long> taskIdList = getLongListParam("taskId", false, params, "query", true);
-        List<String> busAdminList = getStringListParam("businessAdministrator", false, params, "query");
-        List<String> potOwnList = getStringListParam("potentialOwner", false, params, "query");
-        List<String> statusStrList = getStringListParam("status", false, params, "query");
-        List<String> taskOwnList = getStringListParam("taskOwner", false, params, "query");
-        List<Long> procInstIdList = getLongListParam("processInstanceId", false, params, "query", true);
-        String language = getStringParam("languauge", false, params, "query");
+        List<Long> workItemIdList = getLongListParam(allowedQueryParams[0], false, params, "query", true);
+        List<Long> taskIdList = getLongListParam(allowedQueryParams[1], false, params, "query", true);
+        List<String> busAdminList = getStringListParam(allowedQueryParams[2], false, params, "query");
+        List<String> potOwnList = getStringListParam(allowedQueryParams[3], false, params, "query");
+        List<String> statusStrList = getStringListParam(allowedQueryParams[4], false, params, "query");
+        List<String> taskOwnList = getStringListParam(allowedQueryParams[5], false, params, "query");
+        List<Long> procInstIdList = getLongListParam(allowedQueryParams[6], false, params, "query", true);
+        String language = getStringParam(allowedQueryParams[7], false, params, "query");
+        for( String queryParam : params.keySet() ) { 
+            boolean allowed = false;
+            for( String allowedParam : allowedQueryParams ) { 
+                if( allowedParam.equalsIgnoreCase(queryParam) ) { 
+                   allowed = true;
+                   break;
+                }
+            }
+            if( ! allowed ) { 
+                throw new BadRequestException(queryParam + " is an unknown and unsupported query param for the task query operation." );
+            }
+        }
+        
+        /**
+         * TODO: talk with Mauricio about new query command that accepts all of the above params? 
+        String andStr = getStringParam("and", false, params, "query");
+        boolean and = false;
+        if( andStr != null && Boolean.parseBoolean(andStr) ) {
+            and = true;
+        }
+        */
 
         // clean up params
         if (language == null) {
@@ -125,6 +170,7 @@ public class TaskResource extends ResourceBase {
                 } else {
                     for (Long procInstId : procInstIdList) {
                         cmd = new GetTasksByProcessInstanceIdCommand(procInstId);
+                        @SuppressWarnings("unchecked")
                         List<Long> procInstTaskIdList = (List<Long>) processRequestBean.doTaskOperation(cmd);
                         for (Long taskId : procInstTaskIdList) {
                             cmd = new GetTaskCommand(taskId);
@@ -169,6 +215,7 @@ public class TaskResource extends ResourceBase {
 
         while (!cmds.isEmpty()) {
             cmd = cmds.poll();
+            @SuppressWarnings("unchecked")
             List<TaskSummary> taskSummaryList = (List<TaskSummary>) processRequestBean.doTaskOperation(cmd);
             if (taskSummaryList != null && !taskSummaryList.isEmpty()) {
                 for (TaskSummary taskSummary : taskSummaryList) {
@@ -176,7 +223,7 @@ public class TaskResource extends ResourceBase {
                 }
             }
         }
-
+        
         return new JaxbTaskSummaryListResponse(results);
     }
 
@@ -190,8 +237,9 @@ public class TaskResource extends ResourceBase {
     }
 
     @POST
+    @Produces(MediaType.APPLICATION_XML)
     @Path("/{id: [0-9-]+}/{oper: [a-zA-Z]+}")
-    public void doTaskOperation(@PathParam("id") long taskId, @PathParam("oper") String operation) { 
+    public JaxbGenericResponse doTaskOperation(@PathParam("id") long taskId, @PathParam("oper") String operation) { 
         Map<String, List<String>> params = getRequestParams(request);
         operation = checkThatOperationExists(operation, allowedOperations);        
         String userId = identityProvider.getName();
@@ -230,6 +278,8 @@ public class TaskResource extends ResourceBase {
             cmd = new StartTaskCommand(taskId, userId);
         } else if ("stop".equals(operation)) {
             cmd = new StopTaskCommand(taskId, userId);
+        } else if ("suspend".equals(operation)) {
+            cmd = new SuspendTaskCommand(taskId, userId);
         } else if ("nominate".equals(operation)) {
             List<OrganizationalEntity> potentialOwners = getOrganizationalEntityListFromParams(params);
             cmd = new NominateTaskCommand(taskId, userId, potentialOwners);
@@ -237,6 +287,7 @@ public class TaskResource extends ResourceBase {
             throw new BadRequestException("Unsupported operation: /task/" + taskId + "/" + operation);
         }
         processRequestBean.doTaskOperation(cmd);
+        return new JaxbGenericResponse(request);
     }
 
 }
