a9a663214777af5560c2d60873bcafb16e23626b
Marco Rietveld <mrietvel@redhat.com>
2013-10-14 16:25:16 +0200

    - kie-services-client: added more serialization tests

--- kie-remote/kie-services-client/src/test/java/org/kie/services/client/jaxb/JaxbSerializationTest.java
+++ kie-remote/kie-services-client/src/test/java/org/kie/services/client/jaxb/JaxbSerializationTest.java
@@ -1,6 +1,9 @@
 package org.kie.services.client.jaxb;
 
+import static junit.framework.Assert.*;
+import org.junit.Test;
 import org.kie.services.client.SerializationTest;
+import org.kie.services.client.api.command.AcceptedCommands;
 import org.kie.services.client.serialization.jaxb.JaxbSerializationProvider;
 
 public class JaxbSerializationTest extends SerializationTest {
@@ -15,4 +18,14 @@ public class JaxbSerializationTest extends SerializationTest {
         return JaxbSerializationProvider.convertStringToJaxbObject(xmlObject);
     }
 
+    @Test
+    public void acceptedCommandsTest() throws Exception { 
+       for( Class<?> cmdClass : AcceptedCommands.getSet() ) { 
+           try { 
+               cmdClass.getConstructor(new Class[0]);
+           } catch(Exception e) { 
+               fail( "Class " + cmdClass.getSimpleName() + " does not have a no-arg constructor.");
+           }
+       }
+    }
 }
