79743e719a772c871d9faa4571346edb261d70dc
mariofusco <mario.fusco@gmail.com>
2013-10-30 15:40:09 +0100

    add kie-examples-api

---
+++ kie-examples-api/default-kiesession/src/main/java/org/kie/example/api/defaultkiesession/DefaultKieSessionExample.java
@@ -0,0 +1,26 @@
+package org.kie.example.api.defaultkiesession;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import java.io.PrintStream;
+
+public class DefaultKieSessionExample {
+
+    public void go(PrintStream out) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+
+        KieSession kSession = kContainer.newKieSession();
+        kSession.setGlobal("out", out);
+        kSession.insert(new Message("Dave", "Hello, HAL. Do you read me, HAL?"));
+        kSession.fireAllRules();
+    }
+
+
+    public static void main(String[] args) {
+        new DefaultKieSessionExample().go(System.out);
+    }
+
+}
