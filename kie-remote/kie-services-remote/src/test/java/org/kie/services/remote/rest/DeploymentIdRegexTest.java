52fe23ee4929a71ab0030a632a2fde97225936e9
Marco Rietveld <mrietvel@redhat.com>
2013-12-09 19:43:10 +0100

    BZ-1039682 - Using a whitelist regex uniformly for the deployment id

--- kie-remote/kie-services-remote/src/test/java/org/kie/services/remote/rest/DeploymentIdRegexTest.java
+++ kie-remote/kie-services-remote/src/test/java/org/kie/services/remote/rest/DeploymentIdRegexTest.java
@@ -11,12 +11,28 @@ import org.junit.Test;
 public class DeploymentIdRegexTest {
 
     @Test
-    public void regextTest() {
+    public void sameRegexUsedEverywhereTest() {
+        Path pathAnno = RuntimeResource.class.getAnnotation(Path.class);
+        String path = pathAnno.value();
+        path = path.replace("/runtime/{deploymentId: ", "");
+        String runRegex = path.substring(0, path.length()-1);
+
+        pathAnno = DeploymentResource.class.getAnnotation(Path.class);
+        path = pathAnno.value();
+        path = path.replace("/deployment/{deploymentId: ", "");
+        String depRegex = path.substring(0, path.length()-1);
+        
+        assertEquals("DeploymentResource deploymentId regex does not match RuntimeResource's", depRegex, runRegex);
+    }
+
+    @Test
+    public void deploymentIdRegexTest() {
         Path pathAnno = DeploymentResource.class.getAnnotation(Path.class);
         String path = pathAnno.value();
         path = path.replace("/deployment/{deploymentId: ", "");
         String regex = path.substring(0, path.length()-1);
 
+        // Test : groups
         String test = "a:b:c";
         assertTrue(test, Pattern.matches(regex, test));
         test = "a:b:c:d";
@@ -44,6 +60,25 @@ public class DeploymentIdRegexTest {
         assertTrue(test, Pattern.matches(regex, all));
         String[] groups = all.split(":");
         assertEquals(5, groups.length);
+
+        // Test use of "-", "." and "_"
+        test = "group.subgroup:artifact-id:1.0";
+        assertTrue(test, Pattern.matches(regex, test));
+        test = "group.sub_group:artifact_id:1.0.0.Final";
+        assertTrue(test, Pattern.matches(regex, test));
+        
+        test = "group.subgroup:artifact-id:1.0";
+        assertTrue(test, Pattern.matches(regex, test));
+        test = "group.sub_group:artifact_id:v1.0.0.Final";
+        assertTrue(test, Pattern.matches(regex, test));
+       
+        // Test other
+        test = "::v:kbase";
+        assertFalse(test, Pattern.matches(regex, test));
+        test = "::v";
+        assertFalse(test, Pattern.matches(regex, test));
+        test = "$:#:v";
+        assertFalse(test, Pattern.matches(regex, test));
     }
 
 }
