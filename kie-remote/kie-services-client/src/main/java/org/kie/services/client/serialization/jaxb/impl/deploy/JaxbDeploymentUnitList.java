10bff4b5e64807ed3046d75cea6efb3dac12535d
Marco Rietveld <mrietvel@redhat.com>
2014-02-07 12:56:08 +0100

    REST /deployment/ operation (retrieving deployments list) did not work with XML/JAXB

--- kie-remote/kie-services-client/src/main/java/org/kie/services/client/serialization/jaxb/impl/deploy/JaxbDeploymentUnitList.java
+++ kie-remote/kie-services-client/src/main/java/org/kie/services/client/serialization/jaxb/impl/deploy/JaxbDeploymentUnitList.java
@@ -6,17 +6,28 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import org.kie.services.client.serialization.jaxb.impl.process.JaxbProcessInstanceResponse;
 
 @XmlRootElement(name="deployment-unit-list")
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlSeeAlso(value={JaxbDeploymentUnit.class})
 public class JaxbDeploymentUnitList {
 
-    @XmlElement(name ="deployment-unit")
+    @XmlElements({
+        @XmlElement(name ="deployment-unit", type=JaxbDeploymentUnit.class)
+    })
     private List<JaxbDeploymentUnit> deploymentUnitList = new ArrayList<JaxbDeploymentUnit>();
 
     public JaxbDeploymentUnitList() { 
         // default for JAXB, etc.
+    }
+    
+    public JaxbDeploymentUnitList(List<JaxbDeploymentUnit> depUnitList) {
+        this.deploymentUnitList = depUnitList;
     }
     
     public List<JaxbDeploymentUnit> getDeploymentUnitList() {
