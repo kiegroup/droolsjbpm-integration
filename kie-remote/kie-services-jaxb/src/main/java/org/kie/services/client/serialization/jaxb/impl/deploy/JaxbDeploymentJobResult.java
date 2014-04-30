2bfffd246ad7c9513f765d6c25c6ee60ff4bdcc0
Marco Rietveld <mrietvel@redhat.com>
2014-04-30 14:52:19 +0200

    Revert "BZ-1050894 - A deployment unit in JaxbDeploymentJobResult is wrongly marshalled into JSON"
    This reverts commit b4402bf7a3bb262a68d9bff3f88bcb30c0f641f7.

--- kie-remote/kie-services-jaxb/src/main/java/org/kie/services/client/serialization/jaxb/impl/deploy/JaxbDeploymentJobResult.java
+++ kie-remote/kie-services-jaxb/src/main/java/org/kie/services/client/serialization/jaxb/impl/deploy/JaxbDeploymentJobResult.java
@@ -5,14 +5,10 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlSeeAlso;
-
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 
 @XmlRootElement(name="deployment-job-result")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlSeeAlso(JaxbDeploymentUnit.class)
 public class JaxbDeploymentJobResult {
 
     @XmlElement
