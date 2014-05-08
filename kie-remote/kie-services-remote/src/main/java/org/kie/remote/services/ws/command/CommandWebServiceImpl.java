5e6b27e9e9ff0e97926c3f62fe77a3212d635c6d
Marco Rietveld <mrietvel@redhat.com>
2014-05-08 09:47:26 +0200

    kie-services-remote: Uncommenting webservice anno because of problems with jaxb + maps

--- kie-remote/kie-services-remote/src/main/java/org/kie/remote/services/ws/command/CommandWebServiceImpl.java
+++ kie-remote/kie-services-remote/src/main/java/org/kie/remote/services/ws/command/CommandWebServiceImpl.java
@@ -1,7 +1,5 @@
 package org.kie.remote.services.ws.command;
 
-import javax.jws.WebService;
-
 import org.kie.remote.services.rest.ResourceBase;
 import org.kie.remote.services.ws.wsdl.generated.CommandService;
 import org.kie.remote.services.ws.wsdl.generated.CommandWebServiceException;
@@ -9,11 +7,13 @@ import org.kie.services.client.serialization.jaxb.impl.JaxbCommandsRequest;
 import org.kie.services.client.serialization.jaxb.impl.JaxbCommandsResponse;
 import org.kie.services.shared.ServicesVersion;
 
-@WebService(
-        serviceName = "CommandService", 
-        portName = "CommandServicePort", 
-        name = "CommandService", 
-        targetNamespace = CommandWebServiceImpl.NAMESPACE)
+// Comment back in once integration testing has been done!
+
+//@WebService(
+//        serviceName = "CommandService", 
+//        portName = "CommandServicePort", 
+//        name = "CommandService", 
+//        targetNamespace = CommandWebServiceImpl.NAMESPACE)
 public class CommandWebServiceImpl extends ResourceBase implements CommandService {
 
     final static String NAMESPACE = "http://services.remote.kie.org/" + ServicesVersion.VERSION + "/command";
