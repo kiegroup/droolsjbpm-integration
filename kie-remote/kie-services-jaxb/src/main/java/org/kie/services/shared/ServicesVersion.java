a5ee3c194839333500e60eee7e76550be4e7bd86
kiereleaseuser <kiereleaseuser@gmail.com>
2014-06-07 01:49:17 +0200

    upgrade to new version: 6.2.0-SNAPSHOT

--- kie-remote/kie-services-jaxb/src/main/java/org/kie/services/shared/ServicesVersion.java
+++ kie-remote/kie-services-jaxb/src/main/java/org/kie/services/shared/ServicesVersion.java
@@ -3,6 +3,6 @@ package org.kie.services.shared;
 public class ServicesVersion {
 
     // Do not modify this: this is automatically incremented by the maven replacer plugin
-    public final static String VERSION = "6.1.0.1";
+    public final static String VERSION = "6.2.0.1";
     
 }
