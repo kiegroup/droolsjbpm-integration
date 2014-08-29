b17d362f9338fde7b6bf9540a7ff8d5683c9cf58
Petr Siroky <psiroky@redhat.com>
2014-08-29 15:15:41 +0200

    kie-server: execute the integration tests using failsafe plugin

--- kie-server/kie-server-services/src/test/java/org/kie/server/integrationtests/KieServerIntegrationTest.java
+++ kie-server/kie-server-services/src/test/java/org/kie/server/integrationtests/KieServerIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.kie.server.impl;
+package org.kie.server.integrationtests;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,6 +27,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
@@ -57,15 +58,14 @@ import org.kie.server.services.rest.KieServerRestImpl;
 
 import com.thoughtworks.xstream.XStream;
 
-public class KieServerTest {
+public class KieServerIntegrationTest {
 
-    private static final int PORT = findFreePort();
-    public static final String BASE_URI = "http://localhost:" + PORT + "/server";
+    //private static final int PORT = findFreePort();
+    public static final String BASE_URI = "http://localhost:8080/kie-server-services/services/rest/server";
     private static MavenRepository repository;
     private static ReleaseId releaseId1 = new ReleaseId("foo.bar", "baz", "2.1.0.GA");
     private static ReleaseId releaseId2 = new ReleaseId("foo.bar", "baz", "2.1.1.GA");
 
-    private TJWSEmbeddedJaxrsServer server;
     private KieServicesClient       client;
 
     @BeforeClass
@@ -73,7 +73,7 @@ public class KieServerTest {
         createAndDeployKJar(releaseId1);
         createAndDeployKJar(releaseId2);
         // this initialization only needs to be done once per VM
-        RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+        //RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
     }
 
     @AfterClass
@@ -82,13 +82,7 @@ public class KieServerTest {
 
     @Before
     public void setup() throws Exception {
-        startServer();
         startClient();
-    }
-
-    @After
-    public void tearDown() {
-        server.stop();
     }
 
     @Test
@@ -162,7 +156,7 @@ public class KieServerTest {
 
     @Test
     public void testGetContainerInfoNonExisting() throws Exception {
-        ServiceResponse<KieContainerResource> reply = client.getContainerInfo("kie1");
+        ServiceResponse<KieContainerResource> reply = client.getContainerInfo("non-existing-container");
         Assert.assertEquals(ServiceResponse.ResponseType.FAILURE, reply.getType());
     }
 
@@ -258,9 +252,10 @@ public class KieServerTest {
 
         String payload = BatchExecutionHelper.newXStreamMarshaller().toXML(batch);
 
-        KieServerCommand create = new CreateContainerCommand(new KieContainerResource( "kie1", releaseId1, null));
-        KieServerCommand call = new CallContainerCommand("kie1", payload);
-        KieServerCommand dispose = new DisposeContainerCommand("kie1");
+        String containerId = "command-script-container";
+        KieServerCommand create = new CreateContainerCommand(new KieContainerResource( containerId, releaseId1, null));
+        KieServerCommand call = new CallContainerCommand(containerId, payload);
+        KieServerCommand dispose = new DisposeContainerCommand(containerId);
 
         List<KieServerCommand> cmds = Arrays.asList(create, call, dispose);
         CommandScript script = new CommandScript(cmds);
@@ -360,13 +355,6 @@ public class KieServerTest {
         repository = MavenRepository.getMavenRepository();
         repository.deployArtifact(releaseId, jar, pom);
         return jar;
-    }
-
-    private void startServer() throws Exception {
-        server = new TJWSEmbeddedJaxrsServer();
-        server.setPort(PORT);
-        server.start();
-        server.getDeployment().getRegistry().addSingletonResource(new KieServerRestImpl());
     }
 
     private void startClient() throws Exception {
