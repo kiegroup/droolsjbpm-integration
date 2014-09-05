a22fb7876fc8409bda740a07a7072851dbacda90
Petr Siroky <psiroky@redhat.com>
2014-09-05 15:25:01 +0200

    kie-server: use custom settings.xml in tests
     * for now just declare the Maven repo in target/ so the
       tests are deploying kjars there. This way we can
       be sure everything that is needed for tests is
       properly prepared as the target/ dir will be removed
       before running the tests. Deploying to ~/.m2 may lead
       to situations where locally the tests are passing, but
       on CI server they fail (the artifact is in local repo,
       but not in the repo of the CI server)

--- kie-server/kie-server-services/src/test/java/org/kie/server/integrationtests/KieServerBaseIntegrationTest.java
+++ kie-server/kie-server-services/src/test/java/org/kie/server/integrationtests/KieServerBaseIntegrationTest.java
@@ -1,7 +1,42 @@
 package org.kie.server.integrationtests;
 
+import org.drools.compiler.kie.builder.impl.InternalKieModule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.scanner.MavenRepository;
+import org.kie.server.api.model.ReleaseId;
+import org.kie.server.client.KieServicesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public abstract class KieServerBaseIntegrationTest {
+    private static Logger logger = LoggerFactory.getLogger(KieServerBaseIntegrationTest.class);
 
     public static final String BASE_URI = System.getProperty("kie.server.base.uri",
             "http://localhost:8080/kie-server-services/services/rest/server");
+
+    private static MavenRepository repository;
+
+    protected KieServicesClient client;
+
+    @BeforeClass
+    public static void logSettings() {
+        logger.debug("Kie Server base URI: " + BASE_URI);
+    }
+
+    @BeforeClass
+    public static void configureCustomSettingsXml() {
+        System.setProperty("kie.maven.settings.custom",
+                ClassLoader.class.getResource("/kie-server-testing-custom-settings.xml").getFile());
+        logger.debug("Value of 'kie.maven.settings.custom' property:" + System.getProperty("kie.maven.settings.custom"));
+    }
+
 }
