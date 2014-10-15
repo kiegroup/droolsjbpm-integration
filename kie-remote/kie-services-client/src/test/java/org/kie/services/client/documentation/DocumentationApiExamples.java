cb4d5fc3760673bb17c80233c6f6d525bafccb55
Marco Rietveld <mrietvel@redhat.com>
2014-10-15 17:44:36 +0200

    kie-services-client: test/documentation code -- updated to show how to send parameters when starting a process

--- kie-remote/kie-services-client/src/test/java/org/kie/services/client/documentation/DocumentationApiExamples.java
+++ kie-remote/kie-services-client/src/test/java/org/kie/services/client/documentation/DocumentationApiExamples.java
@@ -1,7 +1,9 @@
 package org.kie.services.client.documentation;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
@@ -9,9 +11,10 @@ import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.task.TaskService;
 import org.kie.api.task.model.TaskSummary;
 import org.kie.services.client.api.RemoteJmsRuntimeEngineFactory;
-import org.kie.services.client.api.RemoteRestRuntimeFactory;
+import org.kie.services.client.api.RemoteRestRuntimeEngineFactory;
 import org.kie.services.client.api.RemoteRuntimeEngineFactory;
 import org.kie.services.client.api.command.RemoteRuntimeEngine;
+import org.kie.services.client.builder.objects.MyType;
 
 public class DocumentationApiExamples {
 
@@ -20,12 +23,20 @@ public class DocumentationApiExamples {
         
         // Setup remote JMS runtime engine factory
         RemoteJmsRuntimeEngineFactory remoteJmsFactory 
-            = new RemoteJmsRuntimeEngineFactory(deploymentId, serverUrl, user, password);
+            = RemoteJmsRuntimeEngineFactory.newBuilder()
+                .addDeploymentId(deploymentId)
+                .addJbossServerUrl(serverUrl)
+                .addUserName(user)
+                .addPassword(password)
+                .build();
 
         // Interface with JMS api
         RuntimeEngine engine = remoteJmsFactory.newRuntimeEngine();
         KieSession ksession = engine.getKieSession();
-        ProcessInstance processInstance = ksession.startProcess("com.burns.reactor.maintenance.cycle");
+        
+        Map<String, Object> params = new HashMap<String, Object>(1);
+        params.put("paramName", new MyType("name", 2));
+        ProcessInstance processInstance = ksession.startProcess("com.burns.reactor.maintenance.cycle", params);
         long procId = processInstance.getId();
         TaskService taskService = engine.getTaskService();
         List<Long> tasks = taskService.getTasksByProcessInstanceId(procId);
@@ -37,7 +48,12 @@ public class DocumentationApiExamples {
         
         // Setup the factory class with the necessarry information to communicate with the REST services
         RemoteRuntimeEngineFactory restSessionFactory 
-            = new RemoteRestRuntimeFactory(deploymentId, instanceUrl, user, password);
+            = RemoteRestRuntimeEngineFactory.newBuilder()
+            .addDeploymentId(deploymentId)
+            .addUrl(instanceUrl)
+            .addUserName(user)
+            .addPassword(password)
+            .build();
 
         // Create KieSession and TaskService instances and use them
         RemoteRuntimeEngine engine = restSessionFactory.newRuntimeEngine();
@@ -47,8 +63,10 @@ public class DocumentationApiExamples {
         // Each opertion on a KieSession, TaskService or AuditLogService (client) instance 
         // sends a request for the operation to the server side and waits for the response
         // If something goes wrong on the server side, the client will throw an exception. 
+        Map<String, Object> params = new HashMap<String, Object>(1);
+        params.put("paramName", new MyType("name", 2));
         ProcessInstance processInstance 
-            = ksession.startProcess("com.burns.reactor.maintenance.cycle");
+            = ksession.startProcess("com.burns.reactor.maintenance.cycle", params);
         long procId = processInstance.getId();
 
         String taskUserId = user;
