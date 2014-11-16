629c113b5f1a675b8a6f612c34c41917b08003c0
Petr Siroky <psiroky@redhat.com>
2014-11-16 15:53:46 +0100

    kie-server-client: centralize error handling

--- kie-server/kie-server-client/src/main/java/org/kie/server/client/KieServicesClient.java
+++ kie-server/kie-server-client/src/main/java/org/kie/server/client/KieServicesClient.java
@@ -93,87 +93,68 @@ public class KieServicesClient {
     }
 
     public ServiceResponse<KieScannerResource> updateScanner(String id, KieScannerResource resource) {
-        return makeHttpPostRequestAndProcessResponse(baseURI + "/containers/" + id + "/scanner", resource, ServiceResponse.class);
+        return makeHttpPostRequestAndProcessResponse(baseURI + "/containers/" + id + "/scanner", resource,
+                ServiceResponse.class);
     }
 
     public ServiceResponse<ReleaseId> updateReleaseId(String id, ReleaseId releaseId) {
-        return makeHttpPostRequestAndProcessResponse(baseURI + "/containers/" + id + "/release-id", releaseId, ServiceResponse.class);
+        return makeHttpPostRequestAndProcessResponse(baseURI + "/containers/" + id + "/release-id", releaseId,
+                ServiceResponse.class);
     }
 
     private <T> T makeHttpGetRequestAndProcessResponse(String uri, Class<T> type) {
         KieRemoteHttpRequest request = newRequest(uri).get();
         KieRemoteHttpResponse response = request.response();
-        try {
-            if (response.code() == Response.Status.OK.getStatusCode()) {
-                return serializationProvider.deserialize(response.body(), type);
-            } else {
-                handleGenericHttpErrorCodes(request, response);
-            }
 
-        } catch (SerializationException e) {
-            throw new KieServicesClientException("Error while serializing data received from server!", e);
+        if (response.code() == Response.Status.OK.getStatusCode()) {
+            return deserialize(response.body(), type);
+        } else {
+            throw createExceptionForUnexpectedResponseCode(request, response);
         }
-        throw new KieServicesClientException("Unexpected HTTP response code: " + response.code() + ". Message: " +
-                response.message());
     }
 
     private <T> T makeHttpPostRequestAndProcessResponse(String uri, Object bodyObject, Class<T> type) {
-        return makeHttpPostRequestAndProcessResponse(uri, serializationProvider.serialize(bodyObject), type);
+        return makeHttpPostRequestAndProcessResponse(uri, serialize(bodyObject), type);
     }
 
     private <T> T makeHttpPostRequestAndProcessResponse(String uri, String body, Class<T> type) {
         KieRemoteHttpRequest request = newRequest(uri).body(body).post();
         KieRemoteHttpResponse response = request.response();
-        try {
-            if (response.code() == Response.Status.OK.getStatusCode()) {
-                return serializationProvider.deserialize(response.body(), type);
-            } else {
-                handleGenericHttpErrorCodes(request, response);
-            }
-        } catch (SerializationException e) {
-            throw new KieServicesClientException("Error while serializing data received from server!", e);
+
+        if (response.code() == Response.Status.OK.getStatusCode()) {
+            return deserialize(response.body(), type);
+        } else {
+            throw createExceptionForUnexpectedResponseCode(request, response);
         }
-        throw new KieServicesClientException("Unexpected HTTP response code: " + response.code() + ". Message: " +
-                response.message());
     }
 
     private <T> T makeHttpPutRequestAndProcessResponse(String uri, Object bodyObject, Class<T> type) {
-        return makeHttpPutRequestAndProcessResponse(uri, serializationProvider.serialize(bodyObject), type);
+        return makeHttpPutRequestAndProcessResponse(uri, serialize(bodyObject), type);
     }
 
     private <T> T makeHttpPutRequestAndProcessResponse(String uri, String body, Class<T> type) {
         KieRemoteHttpRequest request = newRequest(uri).body(body).put();
         KieRemoteHttpResponse response = request.response();
-        try {
-            if (response.code() == Response.Status.CREATED.getStatusCode()) {
-                return serializationProvider.deserialize(response.body(), type);
-            } else if (response.code() == Response.Status.BAD_REQUEST.getStatusCode()) {
-                return serializationProvider.deserialize(response.body(), type);
-            } else {
-                handleGenericHttpErrorCodes(request, response);
-            }
-        } catch (SerializationException e) {
-            throw new KieServicesClientException("Error while serializing data received from server!", e);
+
+        if (response.code() == Response.Status.CREATED.getStatusCode()) {
+            return deserialize(response.body(), type);
+        } else if (response.code() == Response.Status.BAD_REQUEST.getStatusCode()) {
+            return deserialize(response.body(), type);
+        } else {
+            throw createExceptionForUnexpectedResponseCode(request, response);
         }
-        throw new KieServicesClientException("Unexpected HTTP response code: " + response.code() + ". Message: " +
-                response.message());
     }
 
 
     private <T> T makeHttpDeleteRequestAndProcessResponse(String uri, Class<T> type) {
         KieRemoteHttpRequest request = newRequest(uri).delete();
         KieRemoteHttpResponse response = request.response();
-        try {
-            if (response.code() == Response.Status.OK.getStatusCode()) {
-                return serializationProvider.deserialize(response.body(), type);
-            } else {
-                handleGenericHttpErrorCodes(request, response);
-            }
-        } catch (SerializationException e) {
-            throw new KieServicesClientException("Error while serializing data received from server!", e);
+
+        if (response.code() == Response.Status.OK.getStatusCode()) {
+            return deserialize(response.body(), type);
+        } else {
+            throw createExceptionForUnexpectedResponseCode(request, response);
         }
-        throw new KieServicesClientException("Unexpected HTTP response code: " + response.code() + ". Message: " +
-                response.message());
     }
 
     private KieRemoteHttpRequest newRequest(String uri) {
@@ -186,14 +167,28 @@ public class KieServicesClient {
         return httpRequest;
     }
 
-    private void handleGenericHttpErrorCodes(KieRemoteHttpRequest request, KieRemoteHttpResponse response) {
-        if (response.code() == Response.Status.NOT_FOUND.getStatusCode()) {
-            throw new KieServicesClientException("Requested resource with URI '" + request.getUri() + "' not found! Message: " +
-                    response.message());
-        } else if (response.code() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()) {
-            throw new KieServicesClientException("Internal server error! " + response.message() + ". Whole body: " +
-                    response.body());
+    private String serialize(Object object) {
+        try {
+            return serializationProvider.serialize(object);
+        } catch (SerializationException e) {
+            throw new KieServicesClientException("Error while serializing request data!", e);
         }
+    }
+
+    private <T> T deserialize(String content, Class<T> type) {
+        try {
+            return serializationProvider.deserialize(content, type);
+        } catch (SerializationException e) {
+            throw new KieServicesClientException("Error while deserializing data received from server!", e);
+        }
+    }
+
+    private RuntimeException createExceptionForUnexpectedResponseCode(KieRemoteHttpRequest request,
+            KieRemoteHttpResponse response) {
+        String summaryMessage = "Unexpected HTTP response code when requesting URI '" + request.getUri() + "'! Error code: " +
+                response.code() + ", message: " + response.message();
+        logger.debug(summaryMessage + ", response body: " + response.body());
+        return new KieServicesClientException(summaryMessage);
     }
 
 }
