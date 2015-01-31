c6b5279eac37300b77a0f6aa22e47faecd40e7c2
Petr Siroky <psiroky@redhat.com>
2015-01-31 17:02:59 +0100

    kie-server: fix compilation issues on JDK6
     * these constructors were introduced in JDK7,
       but we still need to support JDK6 so the
       only choice is to remove them

--- kie-server/kie-server-jms/src/main/java/org/kie/server/jms/JMSRuntimeException.java
+++ kie-server/kie-server-jms/src/main/java/org/kie/server/jms/JMSRuntimeException.java
@@ -23,7 +23,4 @@ public class JMSRuntimeException
         super( cause );
     }
 
-    public JMSRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super( message, cause, enableSuppression, writableStackTrace );
-    }
 }
--- kie-server/kie-server-services/src/main/java/org/kie/server/services/api/KieServerRuntimeException.java
+++ kie-server/kie-server-services/src/main/java/org/kie/server/services/api/KieServerRuntimeException.java
@@ -17,7 +17,4 @@ public class KieServerRuntimeException extends RuntimeException {
         super( cause );
     }
 
-    public KieServerRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super( message, cause, enableSuppression, writableStackTrace );
-    }
 }
