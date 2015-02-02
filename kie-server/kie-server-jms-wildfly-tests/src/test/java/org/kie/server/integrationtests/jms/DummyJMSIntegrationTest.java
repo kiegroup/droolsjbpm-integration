c9cddda49ffe221d3c7e3f6eaa1313b6afb8fc60
Petr Siroky <psiroky@redhat.com>
2015-02-02 21:01:08 +0100

    kie-server: add working dummy JMS test
     * the maven based kjars used in -integ-tests
       are not yet available for the JMS tests,
       this needs to be fixed
    
     * the tests are disabled by default. They
       are activated when one the container profiles
       is specified (wildfly81x, wildfly82x)

--- kie-server/kie-server-jms-wildfly-tests/src/test/java/org/kie/server/integrationtests/jms/DummyJMSIntegrationTest.java
+++ kie-server/kie-server-jms-wildfly-tests/src/test/java/org/kie/server/integrationtests/jms/DummyJMSIntegrationTest.java
@@ -1,45 +1,32 @@
 package org.kie.server.integrationtests.jms;
 
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.kie.server.api.model.*;
-import org.slf4j.Logger;
 
-import javax.jms.*;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Properties;
 
-@Ignore
-public class JMSCRUDTest
-        extends BaseJMSIntegrationTest {
+import javax.jms.*;
 
-    private static ReleaseId releaseId1 = new ReleaseId( "org.kie.server.testing", "container-crud-tests1", "2.1.0.GA" );
-    private static ReleaseId releaseId2 = new ReleaseId( "org.kie.server.testing", "container-crud-tests1", "2.1.1.GA" );
+public class DummyJMSIntegrationTest {
 
-    private static final Logger log = org.slf4j.LoggerFactory.getLogger( JMSCRUDTest.class );
-
-    @BeforeClass
-    public static void initialize()
-            throws Exception {
-        createAndDeployKJar( releaseId1 );
-        createAndDeployKJar( releaseId2 );
-    }
+    private static final Logger log = LoggerFactory.getLogger(DummyJMSIntegrationTest.class);
 
     // Set up all the default values
-    private static final String DEFAULT_MESSAGE            = "Hello, World!";
+    private static final String DEFAULT_MESSAGE = "<script><commands><list-containers/></commands></script>";
     private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
-    private static final String DEFAULT_DESTINATION = "java:/jms/queue/KieServerRequest";
+    private static final String DEFAULT_DESTINATION = "java:/jms/queue/KIE.SERVER.REQUEST";
     private static final String DEFAULT_MESSAGE_COUNT = "1";
-    private static final String DEFAULT_USERNAME = "kieserver";
-    private static final String DEFAULT_PASSWORD = "kieserver";
+    private static final String DEFAULT_USERNAME = "yoda";
+    private static final String DEFAULT_PASSWORD = "usetheforce123@";
     private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
     private static final String PROVIDER_URL = "http-remoting://localhost:8080";
 
-
     @Test
-    public void testCreateContainer() throws Exception {
+    public void testSendBasicJMSMessage() throws Exception {
         ConnectionFactory connectionFactory = null;
         Connection connection = null;
         Session session = null;
@@ -50,7 +37,6 @@ public class JMSCRUDTest
         Context context = null;
 
         try {
-
             final Properties env = new Properties();
             env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
             env.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, PROVIDER_URL));
@@ -69,7 +55,8 @@ public class JMSCRUDTest
             log.info("Found destination \"" + destinationString + "\" in JNDI");
 
             // Create the JMS connection, session, producer, and consumer
-            connection = connectionFactory.createConnection(System.getProperty("username", DEFAULT_USERNAME), System.getProperty("password", DEFAULT_PASSWORD));
+            connection = connectionFactory.createConnection(System.getProperty("username", DEFAULT_USERNAME),
+                    System.getProperty("password", DEFAULT_PASSWORD));
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             producer = session.createProducer(destination);
             // consumer = session.createConsumer(destination);
@@ -83,6 +70,8 @@ public class JMSCRUDTest
             // Send the specified number of messages
             for (int i = 0; i < count; i++) {
                 message = session.createTextMessage(content);
+                message.setJMSCorrelationID(new String("cid=" + i));
+                message.setIntProperty("serialization_type", 0);
                 producer.send(message);
             }
 
@@ -99,6 +88,6 @@ public class JMSCRUDTest
                 connection.close();
             }
         }
-    }
 
+    }
 }
