a19bc4efb672260794786a9158ebdbc7e483a82a
Petr Siroky <psiroky@redhat.com>
2015-02-26 15:11:34 +0100

    kie-server-tests: properly configure the marshalling

--- kie-server-parent/kie-server-tests/kie-server-integ-tests/src/test/java/org/kie/server/integrationtests/shared/RestJmsSharedBaseIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests/src/test/java/org/kie/server/integrationtests/shared/RestJmsSharedBaseIntegrationTest.java
@@ -2,37 +2,37 @@ package org.kie.server.integrationtests.shared;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.kie.server.api.marshalling.MarshallingFormat;
 import org.kie.server.client.KieServicesClient;
 import org.kie.server.client.KieServicesConfiguration;
 import org.kie.server.client.KieServicesFactory;
 import org.kie.server.integrationtests.KieServerBaseIntegrationTest;
 
-import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public abstract class RestJmsSharedBaseIntegrationTest extends KieServerBaseIntegrationTest {
-    
+
     @Parameterized.Parameters(name = "{index}: {0} {1}")
     public static Collection<Object[]> data() throws Exception {
         KieServicesConfiguration restConfiguration = createKieServicesRestConfiguration();
 
         Collection<Object[]> parameterData = new ArrayList<Object[]>(Arrays.asList(new Object[][]
                         {
-                                {MediaType.APPLICATION_XML_TYPE, restConfiguration},
-                                {MediaType.APPLICATION_JSON_TYPE, restConfiguration},
+                                {MarshallingFormat.JAXB, restConfiguration},
+                                {MarshallingFormat.JSON, restConfiguration},
                         }
         ));
-                
+
 
         if (PROVIDER_URL != null) {
             KieServicesConfiguration jmsConfiguration = createKieServicesJmsConfiguration();
             parameterData.addAll(Arrays.asList(new Object[][]
                             {
-                                    {MediaType.APPLICATION_XML_TYPE, jmsConfiguration},
-                                    {MediaType.APPLICATION_JSON_TYPE, jmsConfiguration}
+                                    {MarshallingFormat.JAXB, jmsConfiguration},
+                                    {MarshallingFormat.JSON, jmsConfiguration}
                             })
             );
         }
@@ -41,17 +41,20 @@ public abstract class RestJmsSharedBaseIntegrationTest extends KieServerBaseInte
     }
 
     @Parameterized.Parameter(0)
-    public MediaType MEDIA_TYPE;
+    public MarshallingFormat marshallingFormat;
 
     @Parameterized.Parameter(1)
     public KieServicesConfiguration configuration;
 
     protected KieServicesClient createDefaultClient() throws Exception {
         if (LOCAL_SERVER) {
-            return KieServicesFactory.newKieServicesRestClient(BASE_HTTP_URL, null, null);
+            KieServicesConfiguration localServerConfig =
+                    KieServicesFactory.newRestConfiguration(BASE_HTTP_URL, null, null).setMarshallingFormat(marshallingFormat);
+            return KieServicesFactory.newKieServicesClient(localServerConfig);
         } else {
+            configuration.setMarshallingFormat(marshallingFormat);
             return KieServicesFactory.newKieServicesClient(configuration);
         }
     }
-    
+
 }
