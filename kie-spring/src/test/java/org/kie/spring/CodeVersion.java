20e2c83af2a169895c5732a760c32525c562fc46
kiereleaseuser <kiereleaseuser@gmail.com>
2015-03-09 19:54:55 +0100

    upgrade to 6.2.1-SNAPSHOT

--- kie-spring/src/test/java/org/kie/spring/CodeVersion.java
+++ kie-spring/src/test/java/org/kie/spring/CodeVersion.java
@@ -3,6 +3,6 @@ package org.kie.spring;
 public class CodeVersion {
 
     // Do not modify this: this is automatically incremented by the maven replacer plugin
-    public final static String VERSION = "6.2.0-SNAPSHOT";
+    public final static String VERSION = "6.2.1-SNAPSHOT";
 
 }
