91ec67692e8097b9d5a53bba297be1ea1222d0e4
Maciej Swiderski <mswiders@redhat.com>
2015-05-07 11:18:35 +0200

    DROOLS-733 - Unify Drools and jBPM execution servers - added missed files that where gitignored by mistake

---
+++ kie-server-parent/kie-server-remote/kie-server-rest/kie-server-rest-jbpm/src/main/java/org/kie/server/remote/rest/jbpm/resources/Messages.java
@@ -0,0 +1,15 @@
+package org.kie.server.remote.rest.jbpm.resources;
+
+public class Messages {
+
+    public static final String PROCESS_DEFINITION_NOT_FOUND = "Could not find process definition \"{0}\" in deployment \"{1}\"";
+    public static final String PROCESS_DEFINITION_FETCH_ERROR = "Error when retrieving process definition \"{0}\" in deployment \"{1}\":: Cause: {2}";
+
+    public static final String PROCESS_INSTANCE_NOT_FOUND = "Could not find process instance with id \"{0}\"";
+
+    public static final String CONTAINER_NOT_FOUND = "Could not find container \"{0}\"";
+
+    public static final String CREATE_RESPONSE_ERROR = "Unable to create response: {0}";
+
+    public static final String UNEXPECTED_ERROR = "Unexpected error during processing: {0}";
+}
