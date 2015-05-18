fd405d6364727ab83dcd5b84c2a84d2b22118adb
Marco Rietveld <mrietvel@redhat.com>
2015-05-18 16:28:50 +0200

    Added comment for why test failed

--- kie-remote/kie-remote-client/src/test/java/org/kie/remote/client/jaxb/JaxbRemoteClientSerializationTest.java
+++ kie-remote/kie-remote-client/src/test/java/org/kie/remote/client/jaxb/JaxbRemoteClientSerializationTest.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Ignore
+// Ignored because this test fails on Jenkins (but not locally)
+// It probably fails on Jenkins because the port for the H2 database is not available 
+// (possibly because another test did not stop it's H2 instance?)
 public class JaxbRemoteClientSerializationTest extends AbstractRemoteClientSerializationTest {
 
     protected static final Logger logger = LoggerFactory.getLogger(JaxbRemoteClientSerializationTest.class); 
