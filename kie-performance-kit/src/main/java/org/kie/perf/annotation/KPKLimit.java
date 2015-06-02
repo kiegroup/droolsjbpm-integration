49829240f539209ae3d434c66e2fb54d0f6db169
Ivo Bek <ibek@redhat.com>
2015-06-02 11:02:08 +0200

    kie-performance-kit
    kie-performance-kit: refactoring of executor
    
    kie-performance-kit: Added warmup
    
    kie-performance-kit: added runtimeManagerStrategy
    
    kie-performance-kit: Fixed metrics during warmup
    
    kie-performance-kit: added pessimistic locking and concurrent user count properties
    
    kie-performance-kit: Added CsvSingleReporter and MemoryUsageGaugeSet
    
    kie-performance-kit: extended TestConfig, added support for optional measures MemoryUsage, FileDescriptors and ThreadStates
    
    kie-performance-kit: Format report output

---
+++ kie-performance-kit/src/main/java/org/kie/perf/annotation/KPKLimit.java
@@ -0,0 +1,14 @@
+package org.kie.perf.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = { ElementType.TYPE })
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface KPKLimit {
+
+    int value();
+    
+}
