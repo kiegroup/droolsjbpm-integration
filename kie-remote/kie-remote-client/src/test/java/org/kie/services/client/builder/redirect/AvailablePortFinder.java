953263b9f6bf7f9fe1df8d984a110d68fd148676
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-06-23 15:28:06 +0200

    added missing ASL licenses

--- kie-remote/kie-remote-client/src/test/java/org/kie/services/client/builder/redirect/AvailablePortFinder.java
+++ kie-remote/kie-remote-client/src/test/java/org/kie/services/client/builder/redirect/AvailablePortFinder.java
@@ -1,5 +1,3 @@
-package org.kie.services.client.builder.redirect;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -8,9 +6,9 @@ package org.kie.services.client.builder.redirect;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,6 +16,8 @@ package org.kie.services.client.builder.redirect;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package org.kie.services.client.builder.redirect;
 
 import java.io.IOException;
 import java.net.DatagramSocket;
