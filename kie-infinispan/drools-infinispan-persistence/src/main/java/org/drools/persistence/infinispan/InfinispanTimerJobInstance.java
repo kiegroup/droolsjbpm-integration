1e702a9eb35df594a4796c02b073450dba0475cd
Mario Fusco <mario.fusco@gmail.com>
2015-07-07 19:25:12 +0200

    [DROOLS-795] fix timers serialization
    (cherry picked from commit 1a3d2e07161f5a3df60e25f0dff97c997023d54f)
    (cherry picked from commit e32fb815a5354bd8b70ad43516252a083e422a11)

--- kie-infinispan/drools-infinispan-persistence/src/main/java/org/drools/persistence/infinispan/InfinispanTimerJobInstance.java
+++ kie-infinispan/drools-infinispan-persistence/src/main/java/org/drools/persistence/infinispan/InfinispanTimerJobInstance.java
@@ -1,13 +1,15 @@
 package org.drools.persistence.infinispan;
 
 import org.drools.core.command.CommandService;
-import org.drools.core.time.AcceptsTimerJobFactoryManager;
 import org.drools.core.time.InternalSchedulerService;
 import org.drools.core.time.Job;
 import org.drools.core.time.JobContext;
 import org.drools.core.time.JobHandle;
+import org.drools.core.time.TimerService;
 import org.drools.core.time.Trigger;
+import org.drools.core.time.impl.CommandServiceTimerJobFactoryManager;
 import org.drools.core.time.impl.DefaultTimerJobInstance;
+import org.drools.core.time.impl.TimerJobFactoryManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +32,8 @@ public class InfinispanTimerJobInstance extends DefaultTimerJobInstance {
     public Void call() throws Exception {
         try { 
             JDKCallableJobCommand command = new JDKCallableJobCommand( this );
-            CommandService commandService = ((AcceptsTimerJobFactoryManager)scheduler).getTimerJobFactoryManager().getCommandService();
+            TimerJobFactoryManager timerJobFactoryManager = ( (TimerService)scheduler ).getTimerJobFactoryManager();
+            CommandService commandService = ( (CommandServiceTimerJobFactoryManager) timerJobFactoryManager ).getCommandService();
             commandService.execute( command );
             return null;
         } catch( Exception e ) { 
