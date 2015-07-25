51b414c8d40fc78e63b02485e2a8d2dc52709805
Petr Siroky <psiroky@redhat.com>
2015-07-25 21:49:24 +0200

    distribution: fix runExamples.sh
     * the droolsjbpm binaries need to be included as well
       for the examples app
    
     * synced the help message with the .bat file

--- droolsjbpm-integration-distribution/src/main/assembly/scripts/runExamples.sh
+++ droolsjbpm-integration-distribution/src/main/assembly/scripts/runExamples.sh
@@ -5,17 +5,16 @@ cd `dirname $0`
 
 mainClasspath=
 for i in binaries/*.jar; do mainClasspath=${mainClasspath}:$i; done
+for i in ../binaries/*.jar; do mainClasspath=${mainClasspath}:$i; done
 mainClass=org.drools.examples.DroolsJbpmIntegrationExamplesApp
 
-echo "Usage: ./runExamples.sh"
-echo "For example: ./runExamples.sh"
-echo "Some notes:"
-echo "- Working dir should be the directory of this script."
-echo "- Java is recommended to be JDK and java 6 for optimal performance"
-echo "- The environment variable JAVA_HOME should be set to the JDK installation directory"
-echo "  For example (linux): export JAVA_HOME=/usr/lib/jvm/java-6-sun"
-echo "  For example (mac): export JAVA_HOME=/Library/Java/Home"
-echo
+echo "Usage: runExamples.bat"
+echo "Notes:"
+echo "- Java must be installed. Get the JRE ^(http://www.java.com^) or the JDK."
+echo "- For optimal performance, Java is recommended to be OpenJDK 7 or higher."
+echo "- For JDK, the environment variable JAVA_HOME should be set to the JDK installation directory"
+echo "  For example: set "JAVA_HOME=C:\Program Files\Java\jdk1.6.0""
+echo "- The working dir should be the directory of this script."
 echo "Starting examples app..."
 
 # You can use -Xmx128m or less too, but it might be slower
