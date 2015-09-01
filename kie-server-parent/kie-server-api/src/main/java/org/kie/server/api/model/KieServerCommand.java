68e33af67e6ded0926ffe195f296c42af25619d3
Maciej Swiderski <mswiders@redhat.com>
2015-09-01 17:40:56 +0200

    BZ-1258880 Remove @XmlRootElement annotation on top of KieServerCommand
    (cherry picked from commit b85511294737dfbc841046bd804b0ffa3443c485)

--- kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/model/KieServerCommand.java
+++ kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/model/KieServerCommand.java
@@ -15,12 +15,8 @@
 
 package org.kie.server.api.model;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.kie.api.command.Command;
 
-@XmlRootElement
 public interface KieServerCommand extends Command<ServiceResponse> {
 
 }
