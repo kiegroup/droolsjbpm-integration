6560429b25583e4e0a93bd0c8c6cbed2bc0a3d8a
Marco Rietveld <mrietvel@redhat.com>
2015-09-23 16:45:37 +0200

    BZ-1265725 - The /rest/query/task operation is returning lists with duplicate TaskSummary's

--- kie-remote/kie-remote-services/src/test/java/org/kie/remote/services/rest/query/JpaCriteriaApiDevelopmentTest.java
+++ kie-remote/kie-remote-services/src/test/java/org/kie/remote/services/rest/query/JpaCriteriaApiDevelopmentTest.java
@@ -1,33 +1,33 @@
 package org.kie.remote.services.rest.query;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaBuilder.In;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Subquery;
 
-import org.jbpm.process.audit.ProcessInstanceLog;
-import org.jbpm.process.audit.ProcessInstanceLog_;
-import org.jbpm.process.audit.VariableInstanceLog;
-import org.jbpm.process.audit.VariableInstanceLog_;
+import org.jbpm.services.task.impl.model.OrganizationalEntityImpl;
+import org.jbpm.services.task.impl.model.OrganizationalEntityImpl_;
+import org.jbpm.services.task.impl.model.PeopleAssignmentsImpl;
+import org.jbpm.services.task.impl.model.PeopleAssignmentsImpl_;
 import org.jbpm.services.task.impl.model.TaskDataImpl_;
 import org.jbpm.services.task.impl.model.TaskImpl;
 import org.jbpm.services.task.impl.model.TaskImpl_;
+import org.jbpm.services.task.impl.model.UserImpl_;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.kie.api.task.model.OrganizationalEntity;
+import org.kie.api.task.model.User;
+import org.kie.internal.task.api.model.InternalPeopleAssignments;
 
-@Ignore
+//@Ignore
 public class JpaCriteriaApiDevelopmentTest extends AbstractQueryResourceTest {
 
     @Before
@@ -36,106 +36,103 @@ public class JpaCriteriaApiDevelopmentTest extends AbstractQueryResourceTest {
         engine = getRuntimeEngine();
         ksession = engine.getKieSession();
         taskService = engine.getTaskService();
-        
+
         addObjectProcessInstances = false;
         setupTestData();
     }
-    
+
     @After
     public void cleanup() {
-        if( runtimeManager != null ) { 
+        if( runtimeManager != null ) {
             runtimeManager.disposeRuntimeEngine(engine);
             runtimeManager.close();
         }
     }
 
     @Test
-    public void multipleTasksPerProcessInstanceJoinTest() throws Exception {
-       
+    public void developmentTest() throws Exception {
+
         EntityManager em = getEmf().createEntityManager();
         CriteriaBuilder builder = em.getCriteriaBuilder();
-       
-        boolean basis = false;
-        // VAR
 
-        Query query;
-        { 
-            CriteriaQuery varLogQuery = builder.createQuery(VariableInstanceLog.class);
-            varLogQuery.from(VariableInstanceLog.class);
-            query = em.createQuery(varLogQuery);
-        }
-        List<VariableInstanceLog> varLogs = query.getResultList(); 
-        long procInstId = varLogs.get(0).getProcessInstanceId();
-
-        if( basis ) { 
-            int numVarLogs = varLogs.size();
-            logger.debug( "Var logs: " + numVarLogs );
-            Set<Long> uniqueIds = new HashSet<Long>(numVarLogs);
-            for( VariableInstanceLog varLog : varLogs ) { 
-                assertTrue( "Unique var logs", uniqueIds.add(varLog.getId()) );
-            }
-
-            // PROC
-            { 
-                CriteriaQuery procLogQuery = builder.createQuery(ProcessInstanceLog.class);
-                procLogQuery.from(ProcessInstanceLog.class);
-                query = em.createQuery(procLogQuery);
-            }
-            List<ProcessInstanceLog> procLogs = query.getResultList(); 
-
-            int numProcLogs = procLogs.size();
-            logger.debug( "Proc logs: " + numProcLogs );
-
-            // TASK
-            {
-                CriteriaQuery taskQuery = builder.createQuery(TaskImpl.class);
-                taskQuery.from(TaskImpl.class);
-                query = em.createQuery(taskQuery);
-            }
-            List<TaskImpl> tasks = query.getResultList(); 
-
-            int numTasks = tasks.size();
-            logger.debug( "Tasks: " + numTasks );
-        } 
-        
-        // TEST
+        TypedQuery query;
+        // TASK
         {
-            CriteriaQuery varLogQuery = builder.createQuery(VariableInstanceLog.class);
-            Root<VariableInstanceLog> varLogRoot = varLogQuery.from(VariableInstanceLog.class);
-            Root<ProcessInstanceLog> procLogRoot = varLogQuery.from(ProcessInstanceLog.class);
-            
-            varLogQuery.select(varLogRoot);
-
-            Predicate varProcJoinPred = builder.equal( 
-                    varLogRoot.get(VariableInstanceLog_.processInstanceId), 
-                    procLogRoot.get(ProcessInstanceLog_.processInstanceId));
-           
-            Subquery<Long> taskQuery = varLogQuery.subquery(Long.class);
-            Root<TaskImpl> taskQueryRoot = taskQuery.from(TaskImpl.class);
-            taskQuery.select(taskQueryRoot.get(TaskImpl_.taskData).get(TaskDataImpl_.processInstanceId));
-            Predicate taskQueryPred = builder.equal(
-                    varLogRoot.get(VariableInstanceLog_.processInstanceId),
-                    taskQueryRoot.get(TaskImpl_.taskData).get(TaskDataImpl_.processInstanceId));
-            taskQuery.where(taskQueryPred);
-            
-            Predicate taskSubqueryPred = builder.in(varLogRoot.get(VariableInstanceLog_.processInstanceId)).value(taskQuery);
-                    
-            Predicate pred = builder.and(
-                    varProcJoinPred,
-                    taskSubqueryPred,
-                    builder.equal(varLogRoot.get(VariableInstanceLog_.processInstanceId), procInstId) );
-           
-            query = em.createQuery(varLogQuery);
+            CriteriaQuery taskQuery = builder.createQuery(TaskImpl.class);
+            taskQuery.from(TaskImpl.class);
+            query = em.createQuery(taskQuery);
         }
-        
-        varLogs = query.getResultList(); 
+        List<TaskImpl> tasks = query.getResultList();
 
-        int numVarLogs = varLogs.size();
-        Set<Long> uniqueIds = new HashSet<Long>(numVarLogs);
-        for( VariableInstanceLog varLog : varLogs ) { 
-            assertTrue( "Unique var logs", uniqueIds.add(varLog.getId()) );
+        int numTasks = tasks.size();
+        logger.debug( "Tasks: " + numTasks );
+
+        String potOwner = null;
+        long goodTaskId = -1;
+        for( TaskImpl task : tasks ) {
+           List<OrganizationalEntity> potOwners = task.getPeopleAssignments().getPotentialOwners();
+           if( potOwners.size() > 0 ) {
+               goodTaskId = task.getId();
+               potOwner = potOwners.get(0).getId();
+           }
+           List<OrganizationalEntity> stakeHolders
+               = ((InternalPeopleAssignments) task.getPeopleAssignments()).getTaskStakeholders();
+           List<OrganizationalEntity> busAdmins = task.getPeopleAssignments().getBusinessAdministrators();
+           User actOwner = task.getTaskData().getActualOwner();
         }
-        
-        Assert.assertEquals( 7, numVarLogs );
+        safeResearch(em, tasks, goodTaskId, potOwner);
     }
+
+    private void safeResearch(EntityManager em, List<TaskImpl> tasks, long taskId, String potOwner) {
+        try {
+            doResearch(em, tasks, taskId, potOwner);
+        } catch( Throwable t ) {
+            t.printStackTrace();
+        }
+    }
+
+    private void doResearch(EntityManager em, List<TaskImpl> tasks, long taskId, String user) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        TypedQuery query;
+        // TASK
+        {
+            CriteriaQuery taskQuery = builder.createQuery(TaskImpl.class);
+            Root<TaskImpl> taskRoot = taskQuery.from(TaskImpl.class);
+
+            Join<TaskImpl, PeopleAssignmentsImpl> taskPeopleJoin = taskRoot.join(TaskImpl_.peopleAssignments);
+            Join<PeopleAssignmentsImpl, OrganizationalEntityImpl> peoplePotJoin
+                = taskRoot.join(TaskImpl_.peopleAssignments).join(PeopleAssignmentsImpl_.potentialOwners, JoinType.LEFT);
+            Predicate potOwnerPred = builder.equal( peoplePotJoin.get(OrganizationalEntityImpl_.id), user );
+
+//            Join<PeopleAssignmentsImpl, OrganizationalEntityImpl> stakeHolderJoin = taskPeopleJoin.join(PeopleAssignmentsImpl_.taskStakeholders, JoinType.LEFT);
+//            Predicate stakeHolderPred = builder.equal( stakeHolderJoin.get(OrganizationalEntityImpl_.id), user );
+
+            Join<PeopleAssignmentsImpl, OrganizationalEntityImpl> busAdminJoin
+                = taskRoot.join(TaskImpl_.peopleAssignments).join(PeopleAssignmentsImpl_.businessAdministrators, JoinType.LEFT);
+            Predicate busAdminPred = builder.equal( busAdminJoin.get(OrganizationalEntityImpl_.id), user );
+
+            Predicate taskIdPred = builder.equal( taskRoot.get(TaskImpl_.id), taskId );
+
+//            Predicate actualOwnPred = builder.equal( taskRoot.get(TaskImpl_.taskData).get(TaskDataImpl_.actualOwner).get(UserImpl_.id), user);
+//            Predicate initiatorPred = builder.equal( taskRoot.get(TaskImpl_.taskData).get(TaskDataImpl_.createdBy).get(UserImpl_.id), user);
+
+            Predicate joinPred = builder.or( potOwnerPred, busAdminPred );
+
+            Predicate queryPred = builder.and( taskIdPred, potOwnerPred);
+            taskQuery.where(queryPred);
+            taskQuery.groupBy(taskRoot.get(TaskImpl_.id));
+
+            query = em.createQuery(taskQuery);
+        }
+
+
+        List<TaskImpl> newTasks = query.getResultList();
+        int numTasks = newTasks.size();
+        List<Long> taskIds = new ArrayList<Long>();
+        for( TaskImpl task : newTasks ) {
+           taskIds.add(task.getId());
+        }
+        newTasks.isEmpty();
+    }
+
 }
--- kie-remote/kie-remote-services/src/test/java/org/kie/remote/services/rest/query/QueryResourceQueryBuilderTest.java
+++ kie-remote/kie-remote-services/src/test/java/org/kie/remote/services/rest/query/QueryResourceQueryBuilderTest.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -59,20 +59,20 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         engine = getRuntimeEngine();
         ksession = engine.getKieSession();
         taskService = engine.getTaskService();
-        
+
         jpaService = new RemoteServicesQueryJPAService(getEmf());
-       
+
         addObjectProcessInstances = false;
         setupTestData();
     }
-    
+
     @After
     public void cleanup() {
-        if( runtimeManager != null ) { 
+        if( runtimeManager != null ) {
             runtimeManager.disposeRuntimeEngine(engine);
             runtimeManager.close();
         }
-        if( jpaService != null ) { 
+        if( jpaService != null ) {
             jpaService.dispose();
             jpaService = null;
         }
@@ -80,31 +80,31 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
 
     // TESTS ----------------------------------------------------------------------------------------------------------------------
 
-    
+
     @Test
-    public void queryModificationServiceTest() throws Exception {
+    public void generalQueryBuilderTest() throws Exception {
         RemoteServicesQueryCommandBuilder taskQueryBuilder = new RemoteServicesQueryCommandBuilder(USER_ID);
-        
+
         List<TaskSummaryImpl> taskResult = jpaService.doTaskSummaryQuery(
-                taskQueryBuilder.getTaskUserId(), 
-                userGroupCallback, 
+                taskQueryBuilder.getTaskUserId(),
+                userGroupCallback,
                 taskQueryBuilder.getQueryWhere());
-        
+
         assertNotNull( "Null taskResult!", taskResult );
         assertFalse( "No task summaries found.", taskResult.isEmpty() );
-    
+
         TaskSummary taskSum = taskResult.get(0);
         String deploymentId = taskSum.getDeploymentId();
         long procInstId = taskSum.getProcessInstanceId();
-       
+
         Calendar cal = GregorianCalendar.getInstance();
         cal.roll(Calendar.DAY_OF_YEAR, 1);
         Date tomorrow = cal.getTime();
         cal.roll(Calendar.DAY_OF_YEAR, -2);
         Date yesterday = cal.getTime();
- 
+
         AuditLogService auditLogService = new JPAAuditLogService(getEmf());
-        
+
         logger.debug( "tomorrow: " + QUERY_PARAM_DATE_FORMAT.format(tomorrow) );
         logger.debug( "yesterday: " + QUERY_PARAM_DATE_FORMAT.format(yesterday) );
         taskQueryBuilder.clear();
@@ -119,19 +119,19 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         .taskStatus(Status.Completed)
         .value("check-" + procInstId)
         .variableId("inputStr");
-        
+
         taskResult = jpaService.doTaskSummaryQuery(
-                taskQueryBuilder.getTaskUserId(), 
-                userGroupCallback, 
+                taskQueryBuilder.getTaskUserId(),
+                userGroupCallback,
                 taskQueryBuilder.getQueryWhere());
-        
+
         assertNotNull( "Null taskResult!", taskResult );
         assertFalse( "No task summaries found.", taskResult.isEmpty() );
         assertEquals( "Num task summaries found.", 1, taskResult.size() );
         assertEquals( taskSum.getId(), taskResult.get(0).getId() );
-        
+
         RemoteServicesQueryCommandBuilder varLogQueryBuilder = new RemoteServicesQueryCommandBuilder();
-        
+
         varLogQueryBuilder
         .deploymentId(deploymentId)
         .startDateMin(yesterday)
@@ -149,7 +149,7 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         assertNotNull( "Null var Result!", varResult );
         assertFalse( "No var logs found.", varResult.isEmpty() );
 
-        for( org.kie.api.runtime.manager.audit.VariableInstanceLog log : varResult ) { 
+        for( org.kie.api.runtime.manager.audit.VariableInstanceLog log : varResult ) {
             assertEquals( "deployment id", deploymentId, log.getExternalId() );
             assertTrue( "incorrect start date", log.getDate().after(yesterday) );
             assertTrue( "incorrect end date", log.getDate().before(tomorrow) );
@@ -158,15 +158,15 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
             assertTrue( "Incorrect var name: " + log.getVariableId(), log.getVariableId().startsWith("input") );
             assertTrue( "Incorrect var value: " + log.getValue(), log.getValue().endsWith("-" + procInstId) );
         }
-        
+
         RemoteServicesQueryCommandBuilder procLogQueryBuilder = new RemoteServicesQueryCommandBuilder();
-       
-        ParametrizedQuery<org.kie.api.runtime.manager.audit.ProcessInstanceLog> procQuery 
+
+        ParametrizedQuery<org.kie.api.runtime.manager.audit.ProcessInstanceLog> procQuery
             = auditLogService.processInstanceLogQuery().processInstanceId(procInstId).build();
         List<org.kie.api.runtime.manager.audit.ProcessInstanceLog> procLogs = procQuery.getResultList();
         assertFalse( "No proc logs?!?", procLogs.isEmpty() );
-       
-        ParametrizedQuery<org.kie.api.runtime.manager.audit.VariableInstanceLog> varQuery 
+
+        ParametrizedQuery<org.kie.api.runtime.manager.audit.VariableInstanceLog> varQuery
         = auditLogService.variableInstanceLogQuery()
         .intersect()
         .processInstanceId(procInstId)
@@ -174,29 +174,29 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         .like().value("*-" + procInstId)
         .variableId("input*")
         .build();
-        
+
         List<org.kie.api.runtime.manager.audit.VariableInstanceLog> varLogs = varQuery.getResultList();
         assertFalse( "No last var logs?!?", varLogs.isEmpty() );
-    
+
         procLogQueryBuilder
         .taskId(taskSum.getId())
         .taskStatus(Status.Completed)
         .like().value("*-" + procInstId)
         .variableId("input*");
-        
+
         List<ProcessInstanceLog> procResult = jpaService.doQuery(procLogQueryBuilder.getQueryWhere(), ProcessInstanceLog.class);
         Set<Long> uniqueIds = new HashSet<Long>(procResult.size());
-        for( ProcessInstanceLog procLog : procResult ) { 
+        for( ProcessInstanceLog procLog : procResult ) {
             assertTrue( "Duplicate process instance log found with id " + procLog.getId(), uniqueIds.add(procLog.getId()) );
         }
-        
+
         assertNotNull( "Null proc Result!", procResult );
         assertFalse( "No proc logs found.", procResult.isEmpty() );
         assertEquals( "Num proc logs found.", 1, procResult.size() );
         org.kie.api.runtime.manager.audit.ProcessInstanceLog procLog = procResult.get(0);
         assertEquals( "Incorrect proc inst id: " + procLog.getProcessInstanceId(), procInstId, procLog.getProcessInstanceId().longValue() );
         assertEquals( "Incorrect external id: " + procLog.getExternalId(), deploymentId, procLog.getExternalId() );
-      
+
         // variable value
         org.kie.api.runtime.manager.audit.VariableInstanceLog varLog = varLogs.get(0);
         varQuery = auditLogService.variableInstanceLogQuery()
@@ -205,12 +205,12 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         .last()
         .variableValue(varLog.getVariableId(), varLog.getValue())
         .build();
-        
+
         varLogs = varQuery.getResultList();
         assertFalse( "No last var logs?!?", varLogs.isEmpty() );
         assertEquals( "Num varlogs", 1, varLogs.size());
         assertEquals( "Num varlogs", varLog, varLogs.get(0) );
-   
+
         // variable value regex
         varQuery = auditLogService.variableInstanceLogQuery()
         .intersect()
@@ -218,7 +218,7 @@ public class QueryResourceQueryBuilderTest extends AbstractQueryResourceTest {
         .like()
         .variableValue(varLog.getVariableId(), "*" + varLog.getValue().substring(3))
         .build();
-        
+
         varLogs = varQuery.getResultList();
         assertFalse( "No last var logs?!?", varLogs.isEmpty() );
         assertEquals( "Num varlogs", 1, varLogs.size());
