d8039747fe78e4d7ba4c5a54c738851dec9d4f0b
Marco Rietveld <mrietvel@redhat.com>
2015-11-06 01:29:44 +0100

    BZ-1277962 - REST Query API: taskId_min and taskId_max parameters are reversed closes #223

--- kie-remote/kie-remote-services/src/main/java/org/kie/remote/services/rest/query/helpers/InternalTaskQueryHelper.java
+++ kie-remote/kie-remote-services/src/main/java/org/kie/remote/services/rest/query/helpers/InternalTaskQueryHelper.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -42,12 +42,12 @@ import org.kie.services.client.serialization.jaxb.impl.query.JaxbVariableInfo;
 import org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary;
 
 /**
- * This class contains the core logic for processing the query parameters from a 
+ * This class contains the core logic for processing the query parameters from a
  * REST task query call to a {@link JaxbQueryTaskResult} instance.
  */
 public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQueryTaskResult> {
 
-    public InternalTaskQueryHelper(ResourceBase resourceBase) { 
+    public InternalTaskQueryHelper(ResourceBase resourceBase) {
        super(resourceBase);
     }
 
@@ -56,14 +56,14 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
      * @see org.kie.remote.services.rest.query.AbstractInternalQueryHelper#createAndSetQueryBuilders(java.lang.String)
      */
     @Override
-    protected void createAndSetQueryBuilders(String identity) { 
-        if( identity == null || identity.trim().isEmpty() ) { 
+    protected void createAndSetQueryBuilders(String identity) {
+        if( identity == null || identity.trim().isEmpty() ) {
             throw KieRemoteRestOperationException.forbidden("Unknown and unauthorized user [" + identity + "] when querying tasks" );
         }
         // setup query builders
         RemoteServicesQueryCommandBuilder taskQueryBuilder = new RemoteServicesQueryCommandBuilder(identity);
         RemoteServicesQueryCommandBuilder varInstLogQueryBuilder = new RemoteServicesQueryCommandBuilder();
-        setQueryBuilders(taskQueryBuilder, varInstLogQueryBuilder); 
+        setQueryBuilders(taskQueryBuilder, varInstLogQueryBuilder);
     }
 
     /*
@@ -71,29 +71,29 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
      * @see org.kie.remote.services.rest.query.AbstractInternalQueryHelper#doQueryAndCreateResultObjects(boolean, boolean)
      */
     @Override
-    public JaxbQueryTaskResult doQueryAndCreateResultObjects( boolean onlyRetrieveLastVarLogs, boolean workFlowInstanceVariables, int [] pageInfo) { 
+    public JaxbQueryTaskResult doQueryAndCreateResultObjects( boolean onlyRetrieveLastVarLogs, boolean workFlowInstanceVariables, int [] pageInfo) {
 
         // setup
         RemoteServicesQueryCommandBuilder [] queryBuilders = getQueryBuilders();
         RemoteServicesQueryCommandBuilder taskQueryBuilder = queryBuilders[0];
         RemoteServicesQueryCommandBuilder varInstLogQueryBuilder = queryBuilders[1];
 
-        if( variableCriteriaInQuery(varInstLogQueryBuilder.getQueryWhere().getCriteria()) && onlyRetrieveLastVarLogs ) { 
+        if( variableCriteriaInQuery(varInstLogQueryBuilder.getQueryWhere().getCriteria()) && onlyRetrieveLastVarLogs ) {
             taskQueryBuilder.last();
             varInstLogQueryBuilder.last();
         }
-        
+
         /**
          * TODO: Add pagination information to task query:
          * The problem:
          * A. Because paging is *by process instance id* and we're querying tasks, there is no normal way to set the offset or max results
-         * B. for big data sets, the serialization + transport costs (of retrieving the entire set and paging in memory) will always be far larger 
+         * B. for big data sets, the serialization + transport costs (of retrieving the entire set and paging in memory) will always be far larger
          * than any complex queries we do to limit the output.
-         * 
+         *
          * So, to solve this, we need to do 2 things:
          * 1. Add a WHERE or JOIN subquery to the task (JPQL) query that limits the results to the max results (ignoring the offset if necessary)
-         * 
-         * Something like: 
+         *
+         * Something like:
          * ---
          * SELECT ..
          * FROM Task t
@@ -102,12 +102,12 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
          * AND (select count(distinct p) from t.taskData.processInstanceId p ) <= :maxResultsParam
          * ..
          * ---
-         * 
-         * 2. If possible, add a WHERE subquery to the task (JPQL) query that limits the results to the offset 
-         * Something like: 
+         *
+         * 2. If possible, add a WHERE subquery to the task (JPQL) query that limits the results to the offset
+         * Something like:
          * ---
-         * SELECT .. 
-         * FROM Task t 
+         * SELECT ..
+         * FROM Task t
          * ..
          * WHERE <ORIGINAL QUERY>
          * AND t.taskData.processInstance in (
@@ -116,37 +116,33 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
          *    WHERE <ORIGINAL QUERY>
          *    GROUP BY t
          *    OFFSET :offset)
-         * --- 
-         *    
-         * 3. Writing the above by hand is a little ridiculous (and all of the string builder stuff in the query builders). 
+         * ---
+         *
+         * 3. Writing the above by hand is a little ridiculous (and all of the string builder stuff in the query builders).
          * Realistically, the query builder implementations should be using the JPA CriteriaBuilder API. I can't remember why
-         * I chose not to that at the beginning, but I'm pretty sure it was a mistake not to do that!  
+         * I chose not to that at the beginning, but I'm pretty sure it was a mistake not to do that!
          */
-        
+
         // task queries
         taskQueryBuilder.ascending(OrderBy.processInstanceId);
         RemoteServicesQueryJPAService jpaService = resourceBase.getJPAService();
-        
-        List taskSummaryImpls = jpaService.doTaskSummaryQuery(
+
+        List<TaskSummary> taskSummaries = jpaService.doTaskSummaryQuery(
                 taskQueryBuilder.getTaskUserId(),
                 resourceBase.getUserGroupCallback(),
                 taskQueryBuilder.getQueryWhere());
-        List<TaskSummary> taskSummaries = new ArrayList<TaskSummary>(taskSummaryImpls.size());
-        for( Object impl : taskSummaryImpls ) { 
-            taskSummaries.add((TaskSummary) impl);
-        }
-       
-        if( pageInfo[1] > 0 ) { 
+
+        if( pageInfo[1] > 0 ) {
             long [] procInstIds = getIncludedProcessInstanceIdsAndRemoveUnneededTaskSummaries(taskSummaries, pageInfo);
-            varInstLogQueryBuilder.processInstanceId(procInstIds); 
+            varInstLogQueryBuilder.processInstanceId(procInstIds);
         }
-       
+
         // variable queries
         varInstLogQueryBuilder.ascending(OrderBy.processInstanceId);
         List<VariableInstanceLog> varLogs = jpaService.doQuery(
                 varInstLogQueryBuilder.getQueryWhere(),
                 VariableInstanceLog.class);
-        
+
         // UNFINISHED FEATURE: using in-memory/proces instance variabels instead of audit/history logs
         List<JaxbVariableInfo> procVars = null;
         if( workFlowInstanceVariables ) {
@@ -162,47 +158,47 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
 
         // create result
         JaxbQueryTaskResult result = createQueryTaskResult(taskSummaries, varLogs, procVars, pageInfo );
-        
+
         return result;
     }
 
-    private long [] getIncludedProcessInstanceIdsAndRemoveUnneededTaskSummaries(List<TaskSummary> taskSummaries, int [] pageInfo) { 
+    private long [] getIncludedProcessInstanceIdsAndRemoveUnneededTaskSummaries(List<TaskSummary> taskSummaries, int [] pageInfo) {
         // manage offset (will increase performance with large sets, slow performance with small sets)
         int offset = getOffset(pageInfo);
         Set<Long> exclProcInstids = new HashSet<Long>();
-        
+
         // 1. Remove task summaries before offset (offset is per proc inst id)
         Iterator<TaskSummary> iter = taskSummaries.iterator();
-        while( iter.hasNext() && exclProcInstids.size() < offset ) { 
+        while( iter.hasNext() && exclProcInstids.size() < offset ) {
             TaskSummary taskSum = iter.next();
             exclProcInstids.add(taskSum.getProcessInstanceId());
             iter.remove();
         }
-        
+
         // 2a. Add process instance ids from included task summaries
         // 2b. Remove unneeded task summaries
         Set<Long> inclProcInstids = new HashSet<Long>();
         iter = taskSummaries.iterator();
-        while( iter.hasNext() ) { 
+        while( iter.hasNext() ) {
             Long procInstId = iter.next().getProcessInstanceId();
-            if( ! exclProcInstids.contains(procInstId) && inclProcInstids.size() < pageInfo[1] ) { 
+            if( ! exclProcInstids.contains(procInstId) && inclProcInstids.size() < pageInfo[1] ) {
                 inclProcInstids.add(procInstId);
                 continue;
-            }  
+            }
             if( ! inclProcInstids.contains(procInstId) ) {
                 iter.remove();
             }
         }
-        
+
         // 3. create process instance ids array (for criteria to variable log queriy)
         long [] procInstIdArr = new long[inclProcInstids.size()];
         int i = 0;
-        for( Long boxedId : inclProcInstids )  { 
+        for( Long boxedId : inclProcInstids )  {
             procInstIdArr[i++] = boxedId;
-        } 
+        }
         return procInstIdArr;
     }
-        
+
     /**
      * Create a {@link JaxbQueryTaskResult} instance from the given information.
      * @param taskSummaries A list of {@link TaskSummary} instances
@@ -215,8 +211,8 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
         JaxbQueryTaskResult result = new JaxbQueryTaskResult();
 
         LinkedHashMap<Long, JaxbQueryTaskInfo> procInstIdTaskInfoMap = new LinkedHashMap<Long, JaxbQueryTaskInfo>();
-        int maxNumResultsNeeded = getMaxNumResultsNeeded(pageInfo); 
-        int i = 0; 
+        int maxNumResultsNeeded = getMaxNumResultsNeeded(pageInfo);
+        int i = 0;
         Set<Long> uniqueTaskSummaryIds = new HashSet<Long>(0);
         while( procInstIdTaskInfoMap.size() < maxNumResultsNeeded && i < taskSummaries.size() ) {
             TaskSummary taskSum  = taskSummaries.get(i++);
@@ -225,7 +221,7 @@ public class InternalTaskQueryHelper extends AbstractInternalQueryHelper<JaxbQue
             JaxbQueryTaskInfo taskInfo = createJaxbQueryTaskInfo(procInstId, procInstIdTaskInfoMap);
             taskInfo.getTaskSummaries().add(new JaxbTaskSummary(taskSum));
         }
-        
+
         Set<Long> uniqueVarLogIds = new HashSet<Long>(0);
         for( VariableInstanceLog varLog : varLogs ) {
             assert uniqueVarLogIds.add(varLog.getId()) : "Duplicate variable instance log found with id " + varLog.getId();
