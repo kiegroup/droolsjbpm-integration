3becd7f965b586bedd6b096200b2df164b75ed5e
Maciej Swiderski <mswiders@redhat.com>
2016-01-07 10:15:16 +0100

    BZ-1296017 - IllegalAnnotationsException 'JAXB can't handle interfaces.' in kie-server

---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-drools/src/test/java/org/kie/server/integrationtests/drools/ClassesInludedInKJarIntegrationTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.server.integrationtests.drools;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.drools.core.runtime.impl.ExecutionResultImpl;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.command.BatchExecutionCommand;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.ExecutionResults;
+import org.kie.server.api.marshalling.Marshaller;
+import org.kie.server.api.marshalling.MarshallerFactory;
+import org.kie.server.api.model.KieContainerResource;
+import org.kie.server.api.model.ReleaseId;
+import org.kie.server.api.model.ServiceResponse;
+
+import static org.junit.Assert.*;
+
+public class ClassesInludedInKJarIntegrationTest extends DroolsKieServerBaseIntegrationTest {
+
+    private static ReleaseId releaseId = new ReleaseId("org.kie.server.testing", "classes-included-kjar",
+            "1.0.0-SNAPSHOT");
+
+    private static final String CONTAINER_ID = "stateful-session";
+    private static final String KIE_SESSION = "kbase1.stateful";
+    private static final String PERSON_1_OUT_IDENTIFIER = "person1";
+    private static final String PERSON_2_OUT_IDENTIFIER = "person2";
+    private static final String PERSON_CLASS_NAME = "org.kie.server.testing.Person";
+    private static final String PERSON_NAME = "Darth";
+    private static final String PERSON_SURNAME_FIELD = "surname";
+    private static final String PERSON_DUPLICATED_FIELD = "duplicated";
+    private static final String PERSON_EXPECTED_SURNAME = "Vader";
+
+    private static ClassLoader kjarClassLoader;
+
+    @BeforeClass
+    public static void deployArtifacts() {
+        buildAndDeployCommonMavenParent();
+        buildAndDeployMavenProject(ClassLoader.class.getResource("/kjars-sources/classes-included-kjar").getFile());
+
+        kjarClassLoader = KieServices.Factory.get().newKieContainer(releaseId).getClassLoader();
+    }
+
+    @Override
+    protected void addExtraCustomClasses(Map<String, Class<?>> extraClasses) throws Exception {
+        extraClasses.put(PERSON_CLASS_NAME, Class.forName(PERSON_CLASS_NAME, true, kjarClassLoader));
+    }
+
+    @Test
+    public void testStateIsKeptBetweenCalls() {
+        Marshaller marshaller = MarshallerFactory.getMarshaller(new HashSet<Class<?>>(extraClasses.values()), configuration.getMarshallingFormat(), kjarClassLoader);
+        client.createContainer(CONTAINER_ID, new KieContainerResource(CONTAINER_ID, releaseId));
+
+        List<Command<?>> commands = new ArrayList<Command<?>>();
+        BatchExecutionCommand executionCommand = commandsFactory.newBatchExecution(commands, KIE_SESSION);
+
+        Object person = createInstance(PERSON_CLASS_NAME, PERSON_NAME, "");
+        commands.add(commandsFactory.newInsert(person, PERSON_1_OUT_IDENTIFIER));
+        commands.add(commandsFactory.newFireAllRules());
+
+        ServiceResponse<String> reply1 = ruleClient.executeCommands(CONTAINER_ID, executionCommand);
+        assertEquals(ServiceResponse.ResponseType.SUCCESS, reply1.getType());
+        // first call should set the surname for the inserted person
+        String result1 = reply1.getResult();
+        ExecutionResults actualData = marshaller.unmarshall(result1, ExecutionResultImpl.class);
+
+        Object result = actualData.getValue(PERSON_1_OUT_IDENTIFIER);
+
+        assertEquals("Expected surname to be set to 'Vader'. Got response: " + result1, PERSON_EXPECTED_SURNAME, valueOf(result, PERSON_SURNAME_FIELD));
+        // and 'duplicated' flag should stay false, as only one person is in working memory
+        assertEquals("The 'duplicated' field should be false! Got response: " + result1, false, valueOf(result, PERSON_DUPLICATED_FIELD));
+
+
+        // insert second person and fire the rules. The duplicated field will be set to true if there are two
+        // persons with name "Darth Vader" the first one is from second call and this call inserts the second one.
+        // In case the state of the session was not kept between the calls, the field would not be set
+
+        commands = new ArrayList<Command<?>>();
+
+        executionCommand = commandsFactory.newBatchExecution(commands, KIE_SESSION);
+
+        person = createInstance(PERSON_CLASS_NAME, PERSON_NAME, "");
+        commands.add(commandsFactory.newInsert(person, PERSON_2_OUT_IDENTIFIER));
+        commands.add(commandsFactory.newFireAllRules());
+
+        ServiceResponse<String> reply2 = ruleClient.executeCommands(CONTAINER_ID, executionCommand);
+        String result2 = reply2.getResult();
+        assertEquals(ServiceResponse.ResponseType.SUCCESS, reply2.getType());
+
+        actualData = marshaller.unmarshall(result2, ExecutionResultImpl.class);
+
+        result = actualData.getValue(PERSON_2_OUT_IDENTIFIER);
+        // and 'duplicated' flag should stay false, as only one person is in working memory
+        assertEquals("The 'duplicated' field should be false! Got response: " + result2, true, valueOf(result, PERSON_DUPLICATED_FIELD));
+
+    }
+
+}
