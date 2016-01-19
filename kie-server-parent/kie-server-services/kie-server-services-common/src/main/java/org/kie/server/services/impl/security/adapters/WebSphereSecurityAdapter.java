f105aa6e85edb44be3933c745d3decb79940a88c
Maciej Swiderski <mswiders@redhat.com>
2016-01-19 17:25:15 +0100

    DROOLS-1032 - WebSphere JMS on Kie server produces errors in log

--- kie-server-parent/kie-server-services/kie-server-services-common/src/main/java/org/kie/server/services/impl/security/adapters/WebSphereSecurityAdapter.java
+++ kie-server-parent/kie-server-services/kie-server-services-common/src/main/java/org/kie/server/services/impl/security/adapters/WebSphereSecurityAdapter.java
@@ -16,9 +16,11 @@
 package org.kie.server.services.impl.security.adapters;
 
 import java.lang.reflect.Method;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.naming.InitialContext;
+import javax.security.auth.Subject;
 
 import org.kie.server.api.security.SecurityAdapter;
 import org.slf4j.Logger;
@@ -48,9 +50,11 @@ public class WebSphereSecurityAdapter implements SecurityAdapter {
     public String getUser(Object ... params) {
         if (active) {
             try {
-                Method method = subject.getMethod("getCallerPrincipal", new Class[]{});
-                String principal = (String) method.invoke(null, new Object[]{});
-
+                String principal = getPrincipalFromSubject(params);
+                if (principal == null) {
+                    Method method = subject.getMethod("getCallerPrincipal", new Class[]{});
+                    principal = (String) method.invoke(null, new Object[]{});
+                }
                 return principal;
             } catch (Exception e) {
                 logger.error( "Unable to get user from subject due to {}", e.getMessage(), e );
@@ -71,7 +75,7 @@ public class WebSphereSecurityAdapter implements SecurityAdapter {
 
             try {
                 Method method = registry.getClass().getMethod( "getGroupsForUser", new Class[]{ String.class } );
-                List rolesIn = (List) method.invoke( registry, new Object[]{ getUser() } );
+                List rolesIn = (List) method.invoke( registry, new Object[]{ getUser(params) } );
                 if ( rolesIn != null ) {
                     for ( Object o : rolesIn ) {
                         proles.add(o.toString());
@@ -83,5 +87,31 @@ public class WebSphereSecurityAdapter implements SecurityAdapter {
 
         }
         return proles;
+    }
+
+    protected String getPrincipalFromSubject(Object[] params) {
+
+        if (params != null && params.length > 0) {
+            try {
+                for (Object p : params) {
+                    if (p instanceof Subject) {
+                        Principal principal = ((Subject) p).getPrincipals().iterator().next();
+                        if (principal != null) {
+                            try {
+                                // WSPrincipal.getUserName is the actual (short) name of the user
+                                Method method = principal.getClass().getMethod( "getUserName", new Class[0] );
+                                return (String) method.invoke( principal, new Object[0] );
+                            } catch (Exception e) {
+                                // while principal.getName is the full name including registry specific data
+                                return principal.getName();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                logger.debug("Not able to find principal in params due to {}", e.getMessage());
+            }
+        }
+        return null;
     }
 }
