fb4d2e101d4209dafbd626289310c651cabafa44
Edson Tirelli <ed.tirelli@gmail.com>
2016-02-03 19:22:56 -0500

    PLANNER-505: initial implementation for the Optaplaner Kie Server Extension
    (cherry picked from commit 5851781f7b59e72cbe24a9d720a46a511331e6be)
    (cherry picked from commit 50460416649ccfb09199c53eaea5ab28f4ba3575)

--- kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/marshalling/MarshallerFactory.java
+++ kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/marshalling/MarshallerFactory.java
@@ -36,9 +36,9 @@ public class MarshallerFactory {
             case XSTREAM:
                 return new XStreamMarshaller( classes, classLoader );
             case JAXB:
-                return new JaxbMarshaller(classes, classLoader); // has to be implemented
+                return new JaxbMarshaller(classes, classLoader);
             case JSON:
-                return new JSONMarshaller(classes, classLoader); // has to be implemented
+                return new JSONMarshaller(classes, classLoader);
             default:
                 logger.error( "Unsupported marshalling format: " + format );
         }
---
+++ kie-server-parent/kie-server-remote/kie-server-client/src/main/java/org/kie/server/client/helper/OptaplannerServicesClientBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.server.client.helper;
+
+import org.kie.server.client.KieServicesConfiguration;
+import org.kie.server.client.RuleServicesClient;
+import org.kie.server.client.SolverServicesClient;
+import org.kie.server.client.impl.RuleServicesClientImpl;
+import org.kie.server.client.impl.SolverServicesClientImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OptaplannerServicesClientBuilder
+        implements KieServicesClientBuilder {
+
+    @Override
+    public String getImplementedCapability() {
+        return "OptaPlanner";
+    } // we should consolidate this into a constant in the kie-server-api jar
+
+    @Override
+    public Map<Class<?>, Object> build(KieServicesConfiguration configuration, ClassLoader classLoader) {
+        Map<Class<?>, Object> services = new HashMap<Class<?>, Object>();
+
+        services.put(SolverServicesClient.class, new SolverServicesClientImpl(configuration, classLoader));
+
+        return services;
+    }
+}
