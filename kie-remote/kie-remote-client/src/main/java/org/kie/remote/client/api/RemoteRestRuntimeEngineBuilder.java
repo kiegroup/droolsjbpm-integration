9330decaeaa270ef376213fa5aa215084dc5838b
Marco Rietveld <mrietvel@redhat.com>
2016-03-15 13:51:49 +0100

    BZ-1310510,1325945 - 6.2.0: Remote java API: cannot get tasks assigned to different users (cherry picked from commit 429e4c2852398f9d6924164e0ec3f9e5791c6dbc)

--- kie-remote/kie-remote-client/src/main/java/org/kie/remote/client/api/RemoteRestRuntimeEngineBuilder.java
+++ kie-remote/kie-remote-client/src/main/java/org/kie/remote/client/api/RemoteRestRuntimeEngineBuilder.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -21,7 +21,7 @@ import org.kie.api.runtime.manager.RuntimeEngine;
 
 
 /**
- * This is fluent API builder class for creating a remote {@link RuntimeEngine} instance 
+ * This is fluent API builder class for creating a remote {@link RuntimeEngine} instance
  * or a {@link RemoteRestRuntimeEngineFactory}.
  */
 public interface RemoteRestRuntimeEngineBuilder extends RemoteRuntimeEngineBuilder<RemoteRestRuntimeEngineBuilder, RemoteRestRuntimeEngineFactory> {
@@ -37,10 +37,20 @@ public interface RemoteRestRuntimeEngineBuilder extends RemoteRuntimeEngineBuild
      *     <li>kie-wb</li>
      *     <li>jbpm-console</li></ul></li>
      * </ul>
-     * 
+     *
      * @param instanceUrl The URL of the application
      * @return The builder instance
      */
     RemoteRestRuntimeEngineBuilder addUrl(URL instanceUrl);
-    
+
+    /**
+     * This method should be used with the "org.kie.task.insecure" property (used on the server side).
+     * </br>
+     * This allows users to send task commands that refer to other users besides the user used for authenticating
+     * the REST request.
+     *
+     * @return An instance of this builder
+     */
+    RemoteRestRuntimeEngineBuilder disableTaskSecurity();
+
 }
