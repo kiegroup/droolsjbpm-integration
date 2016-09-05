23f9728f6d91608946d20489c7be43d71430d440
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-09-05 16:51:57 +1000

    JBPM-4650: Fix NPE when listing case definitions and instances
    Moved common invokeCaseOperation to abstract class.

---
+++ kie-server-parent/kie-server-remote/kie-server-rest/kie-server-rest-case-mgmt/src/main/java/org/kie/server/remote/rest/casemgmt/AbstractCaseResource.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.server.remote.rest.casemgmt;
+
+import java.text.MessageFormat;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Variant;
+
+import org.jbpm.casemgmt.api.CaseNotFoundException;
+import org.jbpm.services.api.DeploymentNotFoundException;
+import org.kie.server.remote.rest.common.Header;
+import org.kie.server.services.api.KieServerRegistry;
+import org.kie.server.services.casemgmt.CaseManagementRuntimeDataServiceBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.kie.server.remote.rest.casemgmt.Messages.*;
+import static org.kie.server.remote.rest.common.util.RestUtils.*;
+
+public abstract class AbstractCaseResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractCaseResource.class);
+
+    protected CaseManagementRuntimeDataServiceBase caseManagementRuntimeDataServiceBase;
+    protected KieServerRegistry context;
+
+    public AbstractCaseResource() {
+    }
+
+    public AbstractCaseResource(
+            final CaseManagementRuntimeDataServiceBase caseManagementRuntimeDataServiceBase,
+            final KieServerRegistry context) {
+        this.caseManagementRuntimeDataServiceBase = caseManagementRuntimeDataServiceBase;
+        this.context = context;
+    }
+
+    protected Response invokeCaseOperation(HttpHeaders headers, String containerId, String caseId, CaseOperation operation) {
+        Variant v = getVariant(headers);
+        String type = getContentType(headers);
+        Header conversationIdHeader = buildConversationIdHeader(containerId, context, headers);
+        try {
+            return operation.invoke(v, type, conversationIdHeader);
+        } catch (CaseNotFoundException e) {
+            return notFound(
+                    MessageFormat.format(CASE_INSTANCE_NOT_FOUND, caseId), v, conversationIdHeader);
+        } catch (DeploymentNotFoundException e) {
+            return notFound(
+                    MessageFormat.format(CONTAINER_NOT_FOUND, containerId), v, conversationIdHeader);
+        } catch (Exception e) {
+            logger.error("Unexpected error during processing {}", e.getMessage(), e);
+            return internalServerError(
+                    MessageFormat.format(UNEXPECTED_ERROR, e.getMessage()), v, conversationIdHeader);
+        }
+    }
+
+}
--- kie-server-parent/kie-server-remote/kie-server-rest/kie-server-rest-case-mgmt/src/main/java/org/kie/server/remote/rest/casemgmt/CaseQueryResource.java
+++ kie-server-parent/kie-server-remote/kie-server-rest/kie-server-rest-case-mgmt/src/main/java/org/kie/server/remote/rest/casemgmt/CaseQueryResource.java
@@ -15,16 +15,10 @@
 
 package org.kie.server.remote.rest.casemgmt;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.List;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
@@ -32,45 +26,30 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Variant;
 
-import org.jbpm.casemgmt.api.CaseNotFoundException;
-import org.jbpm.services.api.DeploymentNotFoundException;
-import org.kie.api.runtime.process.ProcessInstance;
-import org.kie.server.api.model.cases.CaseAdHocFragmentList;
-import org.kie.server.api.model.cases.CaseCommentList;
-import org.kie.server.api.model.cases.CaseDefinition;
 import org.kie.server.api.model.cases.CaseDefinitionList;
 import org.kie.server.api.model.cases.CaseInstanceList;
-import org.kie.server.api.model.cases.CaseMilestoneList;
-import org.kie.server.api.model.cases.CaseRoleAssignmentList;
-import org.kie.server.api.model.cases.CaseStageList;
-import org.kie.server.api.model.instance.NodeInstanceList;
-import org.kie.server.api.model.instance.ProcessInstanceList;
 import org.kie.server.remote.rest.common.Header;
 import org.kie.server.services.api.KieServerRegistry;
 import org.kie.server.services.casemgmt.CaseManagementRuntimeDataServiceBase;
-import org.kie.server.services.casemgmt.CaseManagementServiceBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.kie.server.api.rest.RestURI.*;
-import static org.kie.server.remote.rest.casemgmt.Messages.*;
 import static org.kie.server.remote.rest.common.util.RestUtils.*;
 
 @Path("server/" + CASE_QUERY_URI)
-public class CaseQueryResource {
+public class CaseQueryResource extends AbstractCaseResource {
 
     private static final Logger logger = LoggerFactory.getLogger(CaseQueryResource.class);
-
-    private CaseManagementRuntimeDataServiceBase caseManagementRuntimeDataServiceBase;
-    private KieServerRegistry context;
 
     public CaseQueryResource() {
 
     }
 
-    public CaseQueryResource(CaseManagementRuntimeDataServiceBase caseManagementRuntimeDataServiceBase, KieServerRegistry context) {
-        this.caseManagementRuntimeDataServiceBase = caseManagementRuntimeDataServiceBase;
-        this.context = context;
+    public CaseQueryResource(
+            final CaseManagementRuntimeDataServiceBase caseManagementRuntimeDataServiceBase,
+            final KieServerRegistry context) {
+        super(caseManagementRuntimeDataServiceBase, context);
     }
 
     @GET
@@ -81,7 +60,7 @@ public class CaseQueryResource {
             @QueryParam("page") @DefaultValue("0") Integer page, @QueryParam("pageSize") @DefaultValue("10") Integer pageSize) {
 
         return invokeCaseOperation(headers,
-                null,
+                "",
                 null,
                 (Variant v, String type, Header... customHeaders) -> {
 
@@ -102,7 +81,6 @@ public class CaseQueryResource {
     /*
      * case definition methods
      */
-
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response getCaseDefinitions(@javax.ws.rs.core.Context HttpHeaders headers,
@@ -110,7 +88,7 @@ public class CaseQueryResource {
             @QueryParam("page") @DefaultValue("0") Integer page, @QueryParam("pageSize") @DefaultValue("10") Integer pageSize) {
 
         return invokeCaseOperation(headers,
-                null,
+                "",
                 null,
                 (Variant v, String type, Header... customHeaders) -> {
 
@@ -120,31 +98,6 @@ public class CaseQueryResource {
                     logger.debug("Returning OK response with content '{}'", responseObject);
                     return createCorrectVariant(responseObject, headers, Response.Status.OK, customHeaders);
                 });
-    }
-
-    /*
-     * internal methods
-     */
-
-    protected Response invokeCaseOperation(HttpHeaders headers, String containerId, String caseId, CaseOperation operation) {
-        Variant v = getVariant(headers);
-        String type = getContentType(headers);
-        Header conversationIdHeader = buildConversationIdHeader(containerId, context, headers);
-        try {
-
-            return operation.invoke(v, type, conversationIdHeader);
-
-        } catch (CaseNotFoundException e) {
-            return notFound(
-                    MessageFormat.format(CASE_INSTANCE_NOT_FOUND, caseId), v, conversationIdHeader);
-        } catch (DeploymentNotFoundException e) {
-            return notFound(
-                    MessageFormat.format(CONTAINER_NOT_FOUND, containerId), v, conversationIdHeader);
-        } catch (Exception e) {
-            logger.error("Unexpected error during processing {}", e.getMessage(), e);
-            return internalServerError(
-                    MessageFormat.format(UNEXPECTED_ERROR, e.getMessage()), v, conversationIdHeader);
-        }
     }
 
 }
