3bf0cc2ebf98ff0a718a4aeb6f5f624cdb9fe2fd
Maciej Swiderski <mswiders@redhat.com>
2016-10-27 18:49:04 +0200

    RHBPMS-4426 - KIE Server does not delete tasks of aborted processes

--- kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
+++ kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
@@ -74,6 +74,8 @@ public class KieServerConstants {
 
     public static final String CFG_SYNC_DEPLOYMENT = "org.kie.server.sync.deploy";
 
+    public static final String CFG_JBPM_TASK_CLEANUP_LISTENER = "org.jbpm.task.cleanup.enabled";
+
     public static final String KIE_SERVER_PARAM_MODULE_METADATA = "KieModuleMetaData";
 
     public static final String CAPABILITY_BRM = "BRM"; // Business Rules Management
