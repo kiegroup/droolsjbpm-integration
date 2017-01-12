8f46deb034ec77de164eae256aecdad2c519e082
Karel Suta <ksuta@redhat.com>
2017-01-12 18:12:26 +0100

    JBPM-5551 - reproducer

---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/java/org/kie/server/integrationtests/jbpm/TimerIntegrationTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.server.integrationtests.jbpm;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.server.api.model.ReleaseId;
+import org.kie.server.integrationtests.shared.KieServerDeployer;
+
+
+public class TimerIntegrationTest extends JbpmKieServerBaseIntegrationTest {
+
+    private static ReleaseId releaseId = new ReleaseId("org.kie.server.testing", "definition-project",
+            "1.0.0.Final");
+
+    @BeforeClass
+    public static void buildAndDeployArtifacts() {
+        KieServerDeployer.buildAndDeployCommonMavenParent();
+        KieServerDeployer.buildAndDeployMavenProject(ClassLoader.class.getResource("/kjars-sources/definition-project").getFile());
+
+        createContainer(CONTAINER_ID, releaseId);
+    }
+
+    @Test
+    public void testFindTaskAssignedAsBusinessAdmin() throws Exception {
+        Thread.sleep(100000);
+    }
+}
