e94beb38de0daa3d187539149ac3ac988c3f689c
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-03-29 17:26:40 +0200

    RHBPMS-4661 - KIE Server: system properties to configure Executor are not applied (#884)

--- kie-server-parent/kie-server-services/kie-server-services-common/src/test/java/org/kie/server/services/impl/KieServerImplTest.java
+++ kie-server-parent/kie-server-services/kie-server-services-common/src/test/java/org/kie/server/services/impl/KieServerImplTest.java
@@ -25,6 +25,7 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
 import org.kie.scanner.MavenRepository;
+import org.kie.server.api.KieServerConstants;
 import org.kie.server.api.model.KieContainerResource;
 import org.kie.server.api.model.KieContainerResourceFilter;
 import org.kie.server.api.model.KieContainerResourceList;
@@ -40,6 +41,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+
+import static org.junit.Assert.*;
 
 public class KieServerImplTest {
 
@@ -137,6 +140,66 @@ public class KieServerImplTest {
         createEmptyKjar(artifactId, version2);
         // the scanner operation is async, so it will take some time until the releaseId gets updated
         assertReleaseIds(containerId, latestReleaseId, new ReleaseId(GROUP_ID, artifactId, version2), 10000L);
+    }
+
+    @Test
+    public void testExecutorPropertiesInStateRepository() {
+        KieServerStateFileRepository stateRepository = new KieServerStateFileRepository(REPOSITORY_DIR);
+        KieServerState state = stateRepository.load(KIE_SERVER_ID);
+
+        String executorInterval = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_INTERVAL);
+        String executorRetries = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_RETRIES);
+        String executorPool = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_POOL);
+        String executorTimeUnit = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_TIME_UNIT);
+        String executorJMSQueue = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_JMS_QUEUE);
+        String executorDisabled = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_DISABLED);
+
+        assertNull(executorInterval);
+        assertNull(executorRetries);
+        assertNull(executorPool);
+        assertNull(executorTimeUnit);
+        assertNull(executorJMSQueue);
+        assertNull(executorDisabled);
+        try {
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_INTERVAL, "4");
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_RETRIES, "7");
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_POOL, "11");
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_TIME_UNIT, "HOURS");
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_JMS_QUEUE, "queue/MY.OWN.QUEUE");
+            System.setProperty(KieServerConstants.CFG_EXECUTOR_DISABLED, "true");
+
+            stateRepository.clearCache();
+
+            state = stateRepository.load(KIE_SERVER_ID);
+
+            executorInterval = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_INTERVAL);
+            executorRetries = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_RETRIES);
+            executorPool = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_POOL);
+            executorTimeUnit = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_TIME_UNIT);
+            executorJMSQueue = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_JMS_QUEUE);
+            executorDisabled = state.getConfiguration().getConfigItemValue(KieServerConstants.CFG_EXECUTOR_DISABLED);
+
+            assertNotNull(executorInterval);
+            assertNotNull(executorRetries);
+            assertNotNull(executorPool);
+            assertNotNull(executorTimeUnit);
+            assertNotNull(executorJMSQueue);
+            assertNotNull(executorDisabled);
+
+            assertEquals("4", executorInterval);
+            assertEquals("7", executorRetries);
+            assertEquals("11", executorPool);
+            assertEquals("HOURS", executorTimeUnit);
+            assertEquals("queue/MY.OWN.QUEUE", executorJMSQueue);
+            assertEquals("true", executorDisabled);
+        } finally {
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_INTERVAL);
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_RETRIES);
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_POOL);
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_TIME_UNIT);
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_JMS_QUEUE);
+            System.clearProperty(KieServerConstants.CFG_EXECUTOR_DISABLED);
+        }
     }
 
     private void assertReleaseIds(String containerId, ReleaseId configuredReleaseId, ReleaseId resolvedReleaseId, long timeoutMillis) throws InterruptedException {
