dff345dd438977783fafd752574f5618b777bed0
Jiří Locker <jiri.locker@gmail.com>
2017-06-16 14:47:11 +0200

    Clean up OptaPlanner related code
    * Avoid rawtype usages
    * Unify license headers
    * Remov redundant 'throws' clauses
    * Fix imports
    * Improve test method names
    * Fix deprecated antrun configuration

--- kie-server-parent/kie-server-services/kie-server-services-optaplanner/src/main/java/org/kie/server/services/optaplanner/OptaplannerCommandServiceImpl.java
+++ kie-server-parent/kie-server-services/kie-server-services-optaplanner/src/main/java/org/kie/server/services/optaplanner/OptaplannerCommandServiceImpl.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -46,7 +46,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OptaplannerCommandServiceImpl
-        implements KieContainerCommandService {
+        implements KieContainerCommandService<String> {
 
     private static final Logger logger = LoggerFactory.getLogger(OptaplannerCommandServiceImpl.class);
 
@@ -177,7 +177,7 @@ public class OptaplannerCommandServiceImpl
                              command,
                              e);
                 // return failure result
-                responses.add(new ServiceResponse(ServiceResponse.ResponseType.FAILURE,
+                responses.add(new ServiceResponse<>(ServiceResponse.ResponseType.FAILURE,
                                                   e.getMessage()));
             }
         }
