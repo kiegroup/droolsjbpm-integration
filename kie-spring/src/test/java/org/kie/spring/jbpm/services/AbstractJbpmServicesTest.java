9b19ef0f545fd1254771446a2eb9c990af49ed9d
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-07-26 12:15:59 +0200

    RHBPMS-4811 - Improve jbpm services to allow its usage in spring with resource local (#1047) (#1053)

---
+++ kie-spring/src/test/java/org/kie/spring/jbpm/services/AbstractJbpmServicesTest.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.spring.jbpm.services;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.drools.compiler.kie.builder.impl.InternalKieModule;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.builder.model.KieBaseModel;
+import org.kie.api.builder.model.KieModuleModel;
+import org.kie.api.builder.model.KieSessionModel;
+import org.kie.api.conf.EqualityBehaviorOption;
+import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.runtime.conf.ClockTypeOption;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.scanner.MavenRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.kie.scanner.MavenRepository.*;
+
+public class AbstractJbpmServicesTest {
+
+    protected static final Logger logger = LoggerFactory.getLogger(AbstractJbpmServicesTest.class);
+
+    protected static final String ARTIFACT_ID = "test-module";
+    protected static final String GROUP_ID = "org.jbpm.test";
+    protected static final String VERSION = "1.0.0-SNAPSHOT";
+
+    @BeforeClass
+    public static void generalSetup() {
+        System.setProperty(javax.naming.Context.INITIAL_CONTEXT_FACTORY, "bitronix.tm.jndi.BitronixInitialContextFactory");
+    }
+
+    @AfterClass
+    public static void generalCleanup() {
+        System.clearProperty(javax.naming.Context.INITIAL_CONTEXT_FACTORY);
+    }
+
+    protected static void cleanupSingletonSessionId() {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        if (tempDir.exists()) {
+
+            String[] jbpmSerFiles = tempDir.list(new FilenameFilter() {
+
+                @Override
+                public boolean accept(File dir, String name) {
+
+                    return name.endsWith("-jbpmSessionId.ser");
+                }
+            });
+            for (String file : jbpmSerFiles) {
+
+                new File(tempDir, file).delete();
+            }
+        }
+    }
+
+    @Before
+    public void setup() {
+
+        cleanupSingletonSessionId();
+        KieServices ks = KieServices.Factory.get();
+        ReleaseId releaseId = ks.newReleaseId(GROUP_ID, ARTIFACT_ID, VERSION);
+        List<String> processes = new ArrayList<String>();
+        processes.add("jbpm/services/hiring.bpmn2");
+
+        InternalKieModule kJar1 = createKieJar(ks, releaseId, processes);
+        File pom = new File("target/kmodule", "pom.xml");
+        pom.getParentFile().mkdir();
+        try {
+            FileOutputStream fs = new FileOutputStream(pom);
+            fs.write(getPom(releaseId).getBytes());
+            fs.close();
+        } catch (Exception e) {
+
+        }
+        MavenRepository repository = getMavenRepository();
+        repository.deployArtifact(releaseId, kJar1, pom);
+
+
+    }
+
+    protected String getPom(ReleaseId releaseId, ReleaseId... dependencies) {
+        String pom =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                        "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">\n" +
+                        "  <modelVersion>4.0.0</modelVersion>\n" +
+                        "\n" +
+                        "  <groupId>" + releaseId.getGroupId() + "</groupId>\n" +
+                        "  <artifactId>" + releaseId.getArtifactId() + "</artifactId>\n" +
+                        "  <version>" + releaseId.getVersion() + "</version>\n" +
+                        "\n";
+        if (dependencies != null && dependencies.length > 0) {
+            pom += "<dependencies>\n";
+            for (ReleaseId dep : dependencies) {
+                pom += "<dependency>\n";
+                pom += "  <groupId>" + dep.getGroupId() + "</groupId>\n";
+                pom += "  <artifactId>" + dep.getArtifactId() + "</artifactId>\n";
+                pom += "  <version>" + dep.getVersion() + "</version>\n";
+                pom += "</dependency>\n";
+            }
+            pom += "</dependencies>\n";
+        }
+        pom += "</project>";
+        return pom;
+    }
+
+    protected InternalKieModule createKieJar(KieServices ks, ReleaseId releaseId, List<String> resources) {
+        return createKieJar(ks, releaseId, resources, null);
+    }
+
+    protected InternalKieModule createKieJar(KieServices ks, ReleaseId releaseId, List<String> resources, Map<String, String> extraResources ) {
+
+
+        KieFileSystem kfs = createKieFileSystemWithKProject(ks);
+        kfs.writePomXML( getPom(releaseId) );
+
+
+        for (String resource : resources) {
+            kfs.write("src/main/resources/KBase-test/" + resource, ResourceFactory.newClassPathResource(resource));
+        }
+        if (extraResources != null) {
+            for (Map.Entry<String, String> entry : extraResources.entrySet()) {
+                kfs.write(entry.getKey(), ResourceFactory.newByteArrayResource(entry.getValue().getBytes()));
+            }
+        }
+
+        KieBuilder kieBuilder = ks.newKieBuilder(kfs);
+        if (!kieBuilder.buildAll().getResults().getMessages().isEmpty()) {
+            for (Message message : kieBuilder.buildAll().getResults().getMessages()) {
+                logger.error("Error Message: ({}) {}", message.getPath(), message.getText());
+            }
+            throw new RuntimeException(
+                    "There are errors builing the package, please check your knowledge assets!");
+        }
+
+        return ( InternalKieModule ) kieBuilder.getKieModule();
+    }
+
+
+
+    protected KieFileSystem createKieFileSystemWithKProject(KieServices ks) {
+        KieModuleModel kproj = ks.newKieModuleModel();
+
+        KieBaseModel kieBaseModel1 = kproj.newKieBaseModel("KBase-test").setDefault(true).addPackage("*")
+                .setEqualsBehavior( EqualityBehaviorOption.EQUALITY )
+                .setEventProcessingMode( EventProcessingOption.STREAM );
+
+
+        kieBaseModel1.newKieSessionModel("ksession-test").setDefault(true)
+                .setType(KieSessionModel.KieSessionType.STATEFUL)
+                .setClockType( ClockTypeOption.get("realtime") )
+                .newWorkItemHandlerModel("Log", "new org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler()");
+
+
+        KieFileSystem kfs = ks.newKieFileSystem();
+        kfs.writeKModuleXML(kproj.toXML());
+        return kfs;
+    }
+}
