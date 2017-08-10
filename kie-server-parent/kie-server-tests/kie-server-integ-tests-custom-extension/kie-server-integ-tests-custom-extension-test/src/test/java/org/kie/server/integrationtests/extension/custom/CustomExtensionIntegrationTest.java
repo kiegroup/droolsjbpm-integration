74e5df74bb5dcc9f9aa79ab42b12c85459130499
Karel Suta <krlsuta@gmail.com>
2017-08-10 14:34:11 +0200

    JBPM-6271: Kie server custom extension REST client (#1116)

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-custom-extension/kie-server-integ-tests-custom-extension-test/src/test/java/org/kie/server/integrationtests/extension/custom/CustomExtensionIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-custom-extension/kie-server-integ-tests-custom-extension-test/src/test/java/org/kie/server/integrationtests/extension/custom/CustomExtensionIntegrationTest.java
@@ -17,32 +17,25 @@ package org.kie.server.integrationtests.extension.custom;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import java.util.Map;
 
 import org.assertj.core.api.Assertions;
-import org.drools.core.runtime.impl.ExecutionResultImpl;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.ExecutionResults;
-import org.kie.server.api.KieServerConstants;
-import org.kie.server.api.marshalling.Marshaller;
-import org.kie.server.api.marshalling.MarshallerFactory;
 import org.kie.server.api.model.ReleaseId;
-import org.kie.server.integrationtests.config.TestConfig;
+import org.kie.server.client.CustomExtensionClient;
+import org.kie.server.client.KieServicesClient;
 import org.kie.server.integrationtests.shared.KieServerDeployer;
 import org.kie.server.integrationtests.shared.KieServerReflections;
 import org.kie.server.integrationtests.shared.basetests.KieServerBaseIntegrationTest;
-import org.kie.server.integrationtests.shared.basetests.RestOnlyBaseIntegrationTest;
+import org.kie.server.integrationtests.shared.basetests.RestJmsSharedBaseIntegrationTest;
 
-public class CustomExtensionIntegrationTest extends RestOnlyBaseIntegrationTest {
+public class CustomExtensionIntegrationTest extends RestJmsSharedBaseIntegrationTest {
 
     private static ReleaseId releaseId = new ReleaseId("org.kie.server.testing", "stateless-session-kjar", "1.0.0-SNAPSHOT");
 
@@ -60,16 +53,21 @@ public class CustomExtensionIntegrationTest extends RestOnlyBaseIntegrationTest 
     private static final String MARY_SURNAME = "doe";
     private static final String MARY_OUT_IDENTIFIER = "mary doe";
 
-    private static ClassLoader kjarClassLoader;
+    private CustomExtensionClient customClient;
 
     @BeforeClass
     public static void deployArtifacts() {
         KieServerDeployer.buildAndDeployCommonMavenParent();
         KieServerDeployer.buildAndDeployMavenProject(ClassLoader.class.getResource("/kjars-sources/stateless-session-kjar").getFile());
 
-        kjarClassLoader = KieServices.Factory.get().newKieContainer(releaseId).getClassLoader();
+        kieContainer = KieServices.Factory.get().newKieContainer(releaseId);
 
         KieServerBaseIntegrationTest.createContainer(CONTAINER_ID, releaseId);
+    }
+
+    @After
+    public void closeClient() throws Exception {
+        customClient.close();
     }
 
     @AfterClass
@@ -77,40 +75,28 @@ public class CustomExtensionIntegrationTest extends RestOnlyBaseIntegrationTest 
         disposeAllContainers();
     }
 
+    @Override
+    protected void addExtraCustomClasses(Map<String, Class<?>> extraClasses) throws Exception {
+        extraClasses.put(PERSON_CLASS_NAME, Class.forName(PERSON_CLASS_NAME, true, kieContainer.getClassLoader()));
+    }
+
+    @Override
+    protected void setupClients(KieServicesClient client) {
+        customClient = client.getServicesClient(CustomExtensionClient.class);
+    }
+
     @Test
     public void testStatelessCall() throws Exception {
-        Set<Class<?>> kjarClasses = new HashSet<>(Arrays.asList(kjarClassLoader.loadClass(PERSON_CLASS_NAME)));
-        Marshaller marshaller = MarshallerFactory.getMarshaller(kjarClasses, marshallingFormat, kjarClassLoader);
-
-        Object john = createPersonInstance(JOHN_NAME, JOHN_SURNAME, kjarClassLoader);
-        Object mary = createPersonInstance(MARY_NAME, MARY_SURNAME, kjarClassLoader);
+        Object john = createPersonInstance(JOHN_NAME, JOHN_SURNAME, kieContainer.getClassLoader());
+        Object mary = createPersonInstance(MARY_NAME, MARY_SURNAME, kieContainer.getClassLoader());
         List<Object> persons = new ArrayList<>(Arrays.asList(john, mary));
 
-        String requestBody = marshaller.marshall(persons);
-
-        Entity<?> entity = Entity.entity(requestBody, getMediaType());
-        WebTarget clientRequest = newRequest(getCustomEndpointUrl());
-        Response response = clientRequest.request(getMediaType())
-                .header(KieServerConstants.KIE_CONTENT_TYPE_HEADER, marshallingFormat)
-                .post(entity);
-
-        String marshalledResponse = response.readEntity(String.class);
-        ExecutionResults result = marshaller.unmarshall(marshalledResponse, ExecutionResultImpl.class);
+        ExecutionResults result = customClient.insertFireReturn(CONTAINER_ID, KIE_SESSION, persons);
 
         Object john2 = result.getValue(JOHN_OUT_IDENTIFIER);
         Assertions.assertThat(KieServerReflections.valueOf(john2, PERSON_SURNAME_FIELD)).isEqualTo(JOHN_EXPECTED_SURNAME);
         Object mary2 = result.getValue(MARY_OUT_IDENTIFIER);
         Assertions.assertThat(KieServerReflections.valueOf(mary2, PERSON_SURNAME_FIELD)).isEqualTo(MARY_SURNAME);
-    }
-
-    private String getCustomEndpointUrl() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(TestConfig.getKieServerHttpUrl());
-        sb.append("/containers/instances/");
-        sb.append(CONTAINER_ID);
-        sb.append("/ksession/");
-        sb.append(KIE_SESSION);
-        return sb.toString();
     }
 
     private Object createPersonInstance(String firstname, String surname, ClassLoader loader) {
