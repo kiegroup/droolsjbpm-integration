014e4b4dbdab97e05e06632d6dd111f47f3326bf
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-10-03 16:11:17 +0200

    JBPM-6449 - Kie Server Router should retry when updating controller config in case of controller's unavailability, JBPM-6448 - Connectivity lost between kie server router and kie server (#1219)

--- kie-server-parent/kie-server-router/kie-server-router-proxy/src/main/java/org/kie/server/router/handlers/AdminHttpHandler.java
+++ kie-server-parent/kie-server-router/kie-server-router-proxy/src/main/java/org/kie/server/router/handlers/AdminHttpHandler.java
@@ -18,9 +18,14 @@ package org.kie.server.router.handlers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.jboss.logging.Logger;
 import org.json.JSONObject;
@@ -33,6 +38,7 @@ import org.kie.server.router.proxy.aggragate.ResponseAggregator;
 import org.kie.server.router.proxy.aggragate.XstreamXMLResponseAggregator;
 import org.kie.server.router.repository.ConfigurationMarshaller;
 import org.kie.server.router.spi.ConfigRepository;
+import org.kie.server.router.utils.FailedHostInfo;
 import org.kie.server.router.utils.HttpUtils;
 
 import io.undertow.server.HttpHandler;
@@ -44,7 +50,9 @@ public class AdminHttpHandler implements HttpHandler {
     
     private static final Logger log = Logger.getLogger(AdminHttpHandler.class);
 
-    private static final String CONTROLLER = System.getProperty(KieServerRouterConstants.CONTROLLER);
+    private String CONTROLLER = System.getProperty(KieServerRouterConstants.CONTROLLER);
+    private int interval = Integer.parseInt(System.getProperty(KieServerRouterConstants.KIE_SERVER_CONTROLLER_ATTEMPT_INTERVAL, "10"));
+    private int attemptsLimit = Integer.parseInt(System.getProperty(KieServerRouterConstants.KIE_SERVER_RECOVERY_ATTEMPT_LIMIT, "100"));
     
 //    private KieServerProxyClient proxyClient;
     private Configuration configuration = new Configuration();
@@ -55,6 +63,15 @@ public class AdminHttpHandler implements HttpHandler {
 
     private Set<String> controllerContainers = new HashSet<>();
 
+    private ScheduledExecutorService executorService;
+    
+    private ScheduledFuture<?> failedHostsReconnects;
+    private ScheduledFuture<?> addToControllerAttempts;
+    private ScheduledFuture<?> removeFromControllerAttempts;
+    
+    private CopyOnWriteArrayList<FailedHostInfo> failedHosts = new CopyOnWriteArrayList<>();
+    private CopyOnWriteArrayList<ContainerInfo> containersToAddToController = new CopyOnWriteArrayList<>();
+    private CopyOnWriteArrayList<String> containersToRemoveFromController = new CopyOnWriteArrayList<>();
 
     private static final String CONTAINER_SPEC_JSON = "{\n" +
             "    \"container-id\" : \"#1@\",\n" +
@@ -72,9 +89,10 @@ public class AdminHttpHandler implements HttpHandler {
             "    \"status\" : \"STARTED\"\n" +
             "  }";
         
-    public AdminHttpHandler(Configuration configuration, ConfigRepository repository) {
+    public AdminHttpHandler(Configuration configuration, ConfigRepository repository, ScheduledExecutorService executorService) {
         this.configuration = configuration;
         this.repository = repository;
+        this.executorService = executorService;
 
         this.aggregators.add(new JSONResponseAggregator());
         this.aggregators.add(new XstreamXMLResponseAggregator());
@@ -99,80 +117,53 @@ public class AdminHttpHandler implements HttpHandler {
             try {
             
             
-            JSONObject jsonData = new JSONObject(data);
-            
-            String containerId = jsonData.getString("containerId");
-            String alias = jsonData.getString("alias");
-            String serverId = jsonData.getString("serverId");
-            String serverUrl = jsonData.getString("serverUrl");
-            String releaseId = jsonData.getString("releaseId");
-
-            ContainerInfo containerInfo = new ContainerInfo(containerId, alias, releaseId);
-            
-            if (path.startsWith("/add")) {
-                log.infof("Added %s as server location for container %s ", serverUrl, containerId);
+                JSONObject jsonData = new JSONObject(data);
                 
-                synchronized (configuration) {                
-                    configuration.addContainerHost(containerId, serverUrl);
-                    configuration.addContainerHost(alias, serverUrl);
-                    configuration.addServerHost(serverId, serverUrl);
-
-                    configuration.addContainerInfo(containerInfo);
-                    
-                    repository.persist(configuration);
-                }
-
-                if (CONTROLLER != null && releaseId != null && !controllerContainers.contains(containerId)) {
-                    try {
-                        String[] gav = releaseId.split(":");
-                        String jsonPayload = CONTAINER_SPEC_JSON
-                                .replaceFirst("#1@", containerId)
-                                .replaceFirst("#2@", alias)
-                                .replaceFirst("#3@", gav[0])
-                                .replaceFirst("#4@", gav[1])
-                                .replaceFirst("#5@", gav[2]);
-                        HttpUtils.putHttpCall(CONTROLLER + "/management/servers/" + KieServerInfoHandler.getRouterId() + "/containers/" + containerId, jsonPayload);
-                        controllerContainers.add(containerId);
-                        log.infof("Added %s container into controller at %s ", containerId, CONTROLLER);
-                    } catch (Exception e) {
-                        log.warn("Exception when notifying controller about new container " + e.getMessage(), e);
-                    }
-                }
-
-                ResponseCodeHandler.HANDLE_200.handleRequest(exchange);
-            
-            } else if (path.startsWith("/remove")) {
-
-                log.infof("Removed %s as server location for container %s ", serverUrl, containerId);
-                synchronized (configuration) {
-                    configuration.removeContainerHost(containerId, serverUrl);
-                    configuration.removeContainerHost(alias, serverUrl);
-                    configuration.removeServerHost(serverId, serverUrl);
-
-                    configuration.removeContainerInfo(containerInfo);
-                    
-                    repository.persist(configuration);
-                }
-
-                if (CONTROLLER != null && controllerContainers.contains(containerId)) {
-                    List<String> hostsPerContainer = configuration.getHostsPerContainer().getOrDefault(containerId, Collections.emptyList());
-                    if (hostsPerContainer.isEmpty()) {
-
-                        try {
-                            HttpUtils.deleteHttpCall(CONTROLLER + "/management/servers/" + KieServerInfoHandler.getRouterId() + "/containers/" + containerId);
-                            controllerContainers.remove(containerId);
-                            log.infof("Removed %s container from controller at %s ", containerId, CONTROLLER);
-                        } catch (Exception e) {
-                            log.warn("Exception when notifying controller about deleted containers " + e.getMessage(), e);
-                        }
-                    }
-                }
+                String containerId = jsonData.getString("containerId");
+                String alias = jsonData.getString("alias");
+                String serverId = jsonData.getString("serverId");
+                String serverUrl = jsonData.getString("serverUrl");
+                String releaseId = jsonData.getString("releaseId");
+    
+                ContainerInfo containerInfo = new ContainerInfo(containerId, alias, releaseId);
                 
-                ResponseCodeHandler.HANDLE_200.handleRequest(exchange);
-            } else {
-                exchange.getResponseHeaders().put(Headers.STATUS, "");
-                ResponseCodeHandler.HANDLE_404.handleRequest(exchange);
-            }
+                if (path.startsWith("/add")) {
+                    log.infof("Added %s as server location for container %s ", serverUrl, containerId);
+                    
+                    synchronized (configuration) {                
+                        configuration.addContainerHost(containerId, serverUrl);
+                        configuration.addContainerHost(alias, serverUrl);
+                        configuration.addServerHost(serverId, serverUrl);
+    
+                        configuration.addContainerInfo(containerInfo);
+                        
+                        repository.persist(configuration);
+                    }
+    
+                    updateControllerOnAdd(containerId, releaseId, alias, containerInfo);
+    
+                    ResponseCodeHandler.HANDLE_200.handleRequest(exchange);
+                
+                } else if (path.startsWith("/remove")) {
+    
+                    log.infof("Removed %s as server location for container %s ", serverUrl, containerId);
+                    synchronized (configuration) {
+                        configuration.removeContainerHost(containerId, serverUrl);
+                        configuration.removeContainerHost(alias, serverUrl);
+                        configuration.removeServerHost(serverId, serverUrl);
+    
+                        configuration.removeContainerInfo(containerInfo);
+                        
+                        repository.persist(configuration);
+                    }
+                    
+                    updateControllerOnRemove(containerId);
+                    
+                    ResponseCodeHandler.HANDLE_200.handleRequest(exchange);
+                } else {
+                    exchange.getResponseHeaders().put(Headers.STATUS, "");
+                    ResponseCodeHandler.HANDLE_404.handleRequest(exchange);
+                }
             } catch (Exception e) {
                 log.error("Error while performing admin operation", e);
                 
@@ -198,8 +189,148 @@ public class AdminHttpHandler implements HttpHandler {
     }
     public void removeUnavailableServer(String url) {
         synchronized (configuration) {
-            configuration.removeUnavailableServer(url);
+            FailedHostInfo failedHost = configuration.removeUnavailableServer(url);
             repository.persist(configuration);
+            
+            failedHosts.add(failedHost);
+            log.debug("Scheduling host checks...");
+            failedHostsReconnects = executorService.scheduleAtFixedRate(() -> {
+                if (failedHosts.isEmpty()) {
+                    return;
+                }
+                Iterator<FailedHostInfo> it = failedHosts.iterator();
+                while (it.hasNext()) {
+                    FailedHostInfo fHost = (FailedHostInfo) it.next();
+                    if (attemptsLimit == fHost.getAttempts()) {
+                        it.remove();
+                        log.info("Host " + fHost.getServerUrl() + " has reached reconnect attempts limit " + attemptsLimit + " quiting");
+                        continue;
+                    }
+                    try {
+                        HttpUtils.getHttpCall(fHost.getServerUrl());
+                        log.info("Server at " + fHost.getServerUrl() + " is back online");
+                        failedHostsReconnects.cancel(false);
+                        
+                        synchronized (configuration) {
+                            for (String containerId : fHost.getContainers()) {
+                                configuration.addContainerHost(containerId, fHost.getServerUrl());
+                            }
+                            configuration.addServerHost(fHost.getServerId(), fHost.getServerUrl());
+
+                            repository.persist(configuration);
+                        }                        
+                    } catch (Exception e) {
+                        log.debug("Host " + fHost.getServerUrl() + " is still not available, attempting to reconnect in " + interval + " seconds, error " + e.getMessage());
+                    } finally {
+                        fHost.attempted();
+                    }
+                }
+            },
+            interval, interval, TimeUnit.SECONDS);
+        }
+    }
+    
+    protected void pushToController(String releaseId, String containerId, String alias) throws Exception {
+        String[] gav = releaseId.split(":");
+        String jsonPayload = CONTAINER_SPEC_JSON
+                .replaceFirst("#1@", containerId)
+                .replaceFirst("#2@", alias)
+                .replaceFirst("#3@", gav[0])
+                .replaceFirst("#4@", gav[1])
+                .replaceFirst("#5@", gav[2]);
+        HttpUtils.putHttpCall(CONTROLLER + "/management/servers/" + KieServerInfoHandler.getRouterId() + "/containers/" + containerId, jsonPayload);        
+        log.infof("Added %s container into controller at %s ", containerId, CONTROLLER);        
+    }
+    
+    protected void dropFromController(String containerId) throws Exception {
+        HttpUtils.deleteHttpCall(CONTROLLER + "/management/servers/" + KieServerInfoHandler.getRouterId() + "/containers/" + containerId);
+        
+        log.infof("Removed %s container from controller at %s ", containerId, CONTROLLER);
+    }
+    
+    protected void updateControllerOnRemove(String containerId) {
+        if (CONTROLLER != null && controllerContainers.contains(containerId)) {
+            List<String> hostsPerContainer = configuration.getHostsPerContainer().getOrDefault(containerId, Collections.emptyList());
+            if (hostsPerContainer.isEmpty()) {
+
+                try {
+                    controllerContainers.remove(containerId);
+                    dropFromController(containerId);
+                } catch (Exception e) {
+                    log.warn("Exception when notifying controller about deleted containers " + e.getMessage() + " next attempt in " + interval + " seconds");
+                    log.debug(e);
+                    containersToRemoveFromController.add(containerId);
+                    if (removeFromControllerAttempts == null) {
+                        removeFromControllerAttempts = executorService.scheduleAtFixedRate(() -> {
+                            
+                            try {
+                                List<String> sent = new ArrayList<>();
+                                for (String container : containersToRemoveFromController) {
+
+                                    if (controllerContainers.contains(container)) {
+                                        // skip given container if it's back in controllers containers                                            
+                                        sent.add(container);
+                                        continue;
+                                    }
+                                    dropFromController(container);
+                                    sent.add(container);
+                                }
+                                containersToRemoveFromController.removeAll(sent);
+                                if (containersToRemoveFromController.isEmpty()) {
+                                    removeFromControllerAttempts.cancel(false);
+                                    removeFromControllerAttempts = null;
+                                }
+                            } catch (Exception ex2) {
+                                log.warn("Exception when notifying controller about deleted containers " + e.getMessage() + " next attempt in " + interval + " seconds");
+                                log.debug(ex2);
+                            }
+                        },
+                        interval, interval, TimeUnit.SECONDS);
+                    }
+                }
+            }
+        }
+    }
+    
+    protected void updateControllerOnAdd(String containerId, String releaseId, String alias, ContainerInfo containerInfo) {
+        if (CONTROLLER != null && releaseId != null && !controllerContainers.contains(containerId)) {
+            try {
+                controllerContainers.add(containerId);
+                pushToController(releaseId, containerId, alias);
+                
+                log.infof("Added %s container into controller at %s ", containerId, CONTROLLER);
+            } catch (Exception e) {
+                log.warn("Exception when notifying controller about new container " + e.getMessage());
+                containersToAddToController.add(containerInfo);
+                if (addToControllerAttempts == null) {
+                    addToControllerAttempts = executorService.scheduleAtFixedRate(() -> {
+                        
+                        try {
+                            List<ContainerInfo> sent = new ArrayList<>();
+                            for (ContainerInfo container : containersToAddToController) {
+                                if (!controllerContainers.contains(container.getContainerId())) {
+                                    
+                                    sent.add(container);
+                                    continue;
+                                }
+                                
+                                pushToController(container.getReleaseId(), container.getContainerId(), container.getAlias());
+                                sent.add(container);
+                            }
+                            
+                            containersToAddToController.removeAll(sent);
+                            if (containersToAddToController.isEmpty()) {
+                                addToControllerAttempts.cancel(false);
+                                addToControllerAttempts = null;
+                            }
+                        } catch (Exception ex2) {
+                            log.warn("Exception when notifying controller about deleted containers " + e.getMessage() + " next attempt in " + interval + " seconds");
+                            log.debug("Stacktrace", ex2);
+                        }
+                    },
+                    interval, interval, TimeUnit.SECONDS);
+                }
+            }
         }
     }
 }
