052f95ba949dad9c8dcbcb26b839fc0a856ec628
Luca Molteni <volothamp@gmail.com>
2017-10-24 15:24:53 +0200

    refactor

--- kie-maven-plugin/src/main/java/org/kie/maven/plugin/GenerateModelMojo.java
+++ kie-maven-plugin/src/main/java/org/kie/maven/plugin/GenerateModelMojo.java
@@ -1,69 +1,28 @@
 package org.kie.maven.plugin;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
-import javax.inject.Inject;
-
-import org.apache.commons.compress.utils.IOUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.apache.maven.artifact.resolver.filter.CumulativeScopeArtifactFilter;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.PlexusContainer;
 import org.drools.compiler.compiler.io.memory.MemoryFile;
 import org.drools.compiler.compiler.io.memory.MemoryFileSystem;
 import org.drools.compiler.kie.builder.impl.InternalKieModule;
 import org.drools.compiler.kie.builder.impl.KieBuilderImpl;
-import org.drools.compiler.kie.builder.impl.KieContainerImpl;
-import org.drools.compiler.kie.builder.impl.KieMetaInfoBuilder;
-import org.drools.compiler.kie.builder.impl.KieProject;
 import org.drools.compiler.kie.builder.impl.MemoryKieModule;
-import org.drools.compiler.kie.builder.impl.ResultsImpl;
-import org.drools.compiler.kie.builder.impl.ZipKieModule;
-import org.drools.compiler.kproject.ReleaseIdImpl;
-import org.drools.compiler.kproject.models.KieModuleModelImpl;
 import org.drools.modelcompiler.builder.CanonicalModelKieProject;
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieRepository;
-import org.kie.api.builder.Message;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.builder.model.KieModuleModel;
-
-import static org.drools.compiler.kie.builder.impl.KieBuilderImpl.setDefaultsforEmptyKieModule;
 
 @Mojo(name = "generateModel",
         requiresDependencyResolution = ResolutionScope.NONE,
@@ -71,79 +30,30 @@ import static org.drools.compiler.kie.builder.impl.KieBuilderImpl.setDefaultsfor
         defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GenerateModelMojo extends AbstractKieMojo {
 
-    /**
-     * Directory containing the generated JAR.
-     */
-    @Parameter(required = true, defaultValue = "${project.build.outputDirectory}")
-    private File outputDirectory;
+    @Parameter(required = true, defaultValue = "${project.build.directory}")
+    private File targetDirectory;
 
-    @Parameter(required = true, defaultValue = "${project}")
-    private MavenProject project;
+    @Parameter(required = true, defaultValue = "${project.basedir}")
+    private File projectDir;
 
     @Parameter
     private Map<String, String> properties;
-
-    /**
-     * Param passed by the Maven Incremental compiler to identify the value used in the kieMap to identify the
-     * KieModuleMetaInfo from the current complation
-     */
-    @Parameter(required = false, defaultValue = "${compilation.ID}")
-    private String compilationID;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 
-        List<InternalKieModule> kmoduleDeps = new ArrayList<InternalKieModule>();
-
-        try {
-            Set<URL> urls = new HashSet<URL>();
-            for (String element : project.getCompileClasspathElements()) {
-                urls.add(new File(element).toURI().toURL());
-            }
-
-            project.setArtifactFilter(new CumulativeScopeArtifactFilter(Arrays.asList("compile",
-                                                                                      "runtime")));
-            for (Artifact artifact : project.getArtifacts()) {
-                File file = artifact.getFile();
-                if (file != null) {
-                    urls.add(file.toURI().toURL());
-                    KieModuleModel depModel = getDependencyKieModel(file);
-                    if (depModel != null) {
-                        ReleaseId releaseId = new ReleaseIdImpl(artifact.getGroupId(),
-                                                                artifact.getArtifactId(),
-                                                                artifact.getVersion());
-                        kmoduleDeps.add(new ZipKieModule(releaseId,
-                                                         depModel,
-                                                         file));
-                    }
-                }
-            }
-            urls.add(outputDirectory.toURI().toURL());
-
-            ClassLoader projectClassLoader = URLClassLoader.newInstance(urls.toArray(new URL[0]),
-                                                                        getClass().getClassLoader());
-
-            Thread.currentThread().setContextClassLoader(projectClassLoader);
-        } catch (DependencyResolutionRequiredException e) {
-            throw new RuntimeException(e);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
         KieServices ks = KieServices.Factory.get();
 
         try {
             setSystemProperties(properties);
 
-            final File rootDirectory = project.getFile().getParentFile();
-            final KieBuilderImpl kieBuilder = (KieBuilderImpl) ks.newKieBuilder(rootDirectory);
-            KieBuilder builder = kieBuilder.buildAll(CanonicalModelKieProject::new);
+            final KieBuilderImpl kieBuilder = (KieBuilderImpl) ks.newKieBuilder(projectDir);
+            kieBuilder.buildAll(CanonicalModelKieProject::new);
 
             InternalKieModule kieModule = (InternalKieModule) kieBuilder.getKieModule();
-            final Collection<String> fileNames = kieModule.getFileNames();
-            List<String> generatedFiles = fileNames
+            List<String> generatedFiles = kieModule.getFileNames()
                     .stream()
                     .filter(f -> f.endsWith("java"))
                     .collect(Collectors.toList());
@@ -152,60 +62,20 @@ public class GenerateModelMojo extends AbstractKieMojo {
 
             for (String generatedFile : generatedFiles) {
                 MemoryFile f = (MemoryFile) mfs.getFile(generatedFile);
-
-                final String baseDir = project.getBasedir().getPath();
-                final Path newFile = Paths.get(baseDir, "target", f.getPath().toPortableString());
+                final Path newFile = Paths.get(targetDirectory.getPath(), f.getPath().toPortableString());
 
                 try {
                     Files.createDirectories(newFile);
                     Files.copy(f.getContents(), newFile, StandardCopyOption.REPLACE_EXISTING);
+                    getLog().info("Generating " + newFile);
                 } catch (IOException e) {
                     throw new MojoExecutionException("Unable to write file", e);
                 }
             }
-
-            System.out.println("builder");
         } finally {
             Thread.currentThread().setContextClassLoader(contextClassLoader);
         }
 
-        getLog().info("KieModule successfully built!");
-        System.out.println("Hello world!");
-    }
-
-    public static File bytesToTempKJARFile(ReleaseId releaseId, byte[] bytes, String extension) {
-        File file = new File(System.getProperty("java.io.tmpdir"), releaseId.getArtifactId() + "-" + releaseId.getVersion() + extension);
-        try {
-            new PrintWriter(file).close();
-            FileOutputStream fos = new FileOutputStream(file, false);
-            fos.write(bytes);
-            fos.flush();
-            fos.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return file;
-    }
-
-    private KieModuleModel getDependencyKieModel(File jar) {
-        ZipFile zipFile = null;
-        try {
-            zipFile = new ZipFile(jar);
-            ZipEntry zipEntry = zipFile.getEntry(KieModuleModelImpl.KMODULE_JAR_PATH);
-            if (zipEntry != null) {
-                KieModuleModel kieModuleModel = KieModuleModelImpl.fromXML(zipFile.getInputStream(zipEntry));
-                setDefaultsforEmptyKieModule(kieModuleModel);
-                return kieModuleModel;
-            }
-        } catch (Exception e) {
-        } finally {
-            if (zipFile != null) {
-                try {
-                    zipFile.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-        return null;
+        getLog().info("DSL successfully generated");
     }
 }
