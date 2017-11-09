3bdf8a56a73f8161b06984069b673bcc0b68d2d4
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-11-09 14:12:40 +0100

    RHBPMS-4819: upgrade of batik

--- jbpm-process-svg/src/test/java/org/jbpm/process/svg/TestEvalutionSVG.java
+++ jbpm-process-svg/src/test/java/org/jbpm/process/svg/TestEvalutionSVG.java
@@ -15,6 +15,10 @@
 
 package org.jbpm.process.svg;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -28,15 +32,11 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import static org.junit.Assert.*;
 
 public class TestEvalutionSVG {
 
