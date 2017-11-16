e916032edd47aa46d15f3a11909b4804ee20a7e8
Radovan Stancel <rstancel@redhat.com>
2017-11-16 10:56:10 +0100

    [RHBMS-4312] Loading pasword from a keystore

--- kie-server-parent/kie-server-common/src/main/java/org/kie/server/common/KeyStoreHelperUtil.java
+++ kie-server-parent/kie-server-common/src/main/java/org/kie/server/common/KeyStoreHelperUtil.java
@@ -7,9 +7,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KeyStoreHelperUtil {
+
     private static final Logger logger = LoggerFactory.getLogger(KeyStoreHelperUtil.class);
 
-    public static String loadPassword(KieServerConfig config) {
+    public static String loadPassword() {
+        String passwordKey;
+        KeyStoreHelper keyStoreHelper = new KeyStoreHelper();
+
+        try {
+            passwordKey = keyStoreHelper.getPasswordKey();
+        } catch (RuntimeException re) {
+            logger.warn("Unable to load key store. Using password from configuration");
+            passwordKey = System.getProperty(KieServerConstants.CFG_KIE_PASSWORD, "kieserver1!");
+        }
+
+        return passwordKey;
+    }
+
+    public static String loadControllerPassword(KieServerConfig config) {
         String passwordKey;
         KeyStoreHelper keyStoreHelper = new KeyStoreHelper();
 
