8fc05f6760b850cf62a52c19c918c145e5859009
Jiri Petrlik <jiripetrlik@gmail.com>
2018-09-24 13:56:33 +0200

    [BAQE-470] - Add tests for planner in OSGI

---
+++ kie-osgi/kie-karaf-itests/src/test/java/org/kie/karaf/itest/planner/domain/CloudBalance.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.karaf.itest.planner.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.Solution;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.impl.score.buildin.hardsoft.HardSoftScoreDefinition;
+import org.optaplanner.persistence.xstream.api.score.buildin.hardsoft.HardSoftScoreXStreamConverter;
+
+@PlanningSolution
+@XStreamAlias("CloudBalance")
+public class CloudBalance implements Solution<HardSoftScore> {
+
+    private List<CloudComputer> computerList;
+
+    private List<CloudProcess> processList;
+
+    @XStreamConverter(value = HardSoftScoreXStreamConverter.class, types = {HardSoftScoreDefinition.class})
+    private HardSoftScore score;
+
+    @ValueRangeProvider(id = "computerRange")
+    public List<CloudComputer> getComputerList() {
+        return computerList;
+    }
+
+    public void setComputerList(List<CloudComputer> computerList) {
+        this.computerList = computerList;
+    }
+
+    @PlanningEntityCollectionProperty
+    public List<CloudProcess> getProcessList() {
+        return processList;
+    }
+
+    public void setProcessList(List<CloudProcess> processList) {
+        this.processList = processList;
+    }
+
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftScore score) {
+        this.score = score;
+    }
+
+    public Collection<? extends Object> getProblemFacts() {
+        List<Object> facts = new ArrayList<Object>();
+        facts.addAll(computerList);
+        // Do not add the planning entity's (processList) because that will be done automatically
+        return facts;
+    }
+}
