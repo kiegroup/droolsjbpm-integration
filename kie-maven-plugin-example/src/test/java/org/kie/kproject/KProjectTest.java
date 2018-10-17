b1b637afea4a6b82710d0daf6ae264d789de24de
Luca Molteni <volothamp@gmail.com>
2018-10-17 17:12:06 +0200

    Also run DMN test

--- kie-maven-plugin-example/src/test/java/org/kie/kproject/KProjectTest.java
+++ kie-maven-plugin-example/src/test/java/org/kie/kproject/KProjectTest.java
@@ -15,17 +15,26 @@
 
 package org.kie.kproject;
 
-import org.junit.Ignore;
+import java.math.BigDecimal;
+
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.FactHandle;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.kie.dmn.core.api.DMNFactory;
 import org.kie.firealarm.Fire;
 import org.kie.firealarm.Room;
 import org.kie.firealarm.Sprinkler;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class KProjectTest {
 
@@ -48,5 +57,23 @@ public class KProjectTest {
         kSession.delete(fireFH);
         rules = kSession.fireAllRules();
         assertEquals(3, rules);
+    }
+
+    @Test
+    public void testSolutionCase1() {
+        DMNRuntime runtime = DMNRuntimeUtil.createRuntime("org/kie/example/0020-vacation-days.dmn", this.getClass());
+        DMNModel dmnModel = runtime.getModel("https://www.drools.org/kie-dmn", "0020-vacation-days");
+        assertThat(dmnModel, notNullValue());
+
+        DMNContext context = DMNFactory.newContext();
+
+        context.set("Age", 16);
+        context.set("Years of Service", 1);
+
+        DMNResult dmnResult = runtime.evaluateAll(dmnModel, context);
+
+        DMNContext result = dmnResult.getContext();
+
+        assertThat(result.get("Total Vacation Days"), is(BigDecimal.valueOf(27)));
     }
 }
