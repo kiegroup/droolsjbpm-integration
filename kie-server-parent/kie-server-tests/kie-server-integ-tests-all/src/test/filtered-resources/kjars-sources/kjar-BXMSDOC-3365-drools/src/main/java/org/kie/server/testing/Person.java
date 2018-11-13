3cb11902ca8510836dee87fce7270aec5ec60083
tarilabs <matteo.mortari@gmail.com>
2018-11-13 17:49:15 +0100

    BXMSDOC-3365 provide missing content for Drools segments in KIE APIs doc

---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-all/src/test/filtered-resources/kjars-sources/kjar-BXMSDOC-3365-drools/src/main/java/org/kie/server/testing/Person.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.server.testing;
+
+public class Person {
+    private String fullname;
+    private Integer age;
+
+    public Person() {}
+
+    public Person(String fullname, Integer age) {
+        this.fullname = fullname;
+        this.age = age;
+    }
+    
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-all/src/test/java/org/kie/server/integrationtests/jbpm/JbpmMiscCommandSerializationIntegrationTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.server.integrationtests.jbpm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.command.BatchExecutionCommand;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.ExecutionResults;
+import org.kie.server.api.model.ReleaseId;
+import org.kie.server.api.model.ServiceResponse;
+import org.kie.server.integrationtests.shared.KieServerDeployer;
+
+import static org.junit.Assert.assertEquals;
+
+public class JbpmMiscCommandSerializationIntegrationTest extends JbpmKieServerBaseIntegrationTest {
+
+    private static ReleaseId releaseId = new ReleaseId("org.kie.server.testing", "kjar-BXMSDOC-3365-jbpm", "1.0.0");
+
+    private static final String CONTAINER_ID = "stateful-session";
+    private static final String PERSON_CLASS_NAME = "org.kie.server.testing.Person";
+
+    private static ClassLoader kjarClassLoader;
+
+    @BeforeClass
+    public static void deployArtifacts() {
+        KieServerDeployer.buildAndDeployCommonMavenParent();
+        KieServerDeployer.buildAndDeployMavenProject(ClassLoader.class.getResource("/kjars-sources/kjar-BXMSDOC-3365-jbpm").getFile());
+
+        kjarClassLoader = KieServices.Factory.get().newKieContainer(releaseId).getClassLoader();
+    }
+
+    @Before
+    public void cleanContainers() {
+        disposeAllContainers();
+        createContainer(CONTAINER_ID, releaseId);
+    }
+
+    @Override
+    protected void addExtraCustomClasses(Map<String, Class<?>> extraClasses) throws Exception {
+        extraClasses.put(PERSON_CLASS_NAME, Class.forName(PERSON_CLASS_NAME, true, kjarClassLoader));
+    }
+    
+    @Test
+    public void testStartProcessAndSignal() {
+        // BXMSDOC-3365 
+        Object person = createInstance(PERSON_CLASS_NAME, "john", 25);
+        
+        List<Command<?>> commands = new ArrayList<Command<?>>();
+        BatchExecutionCommand executionCommand = commandsFactory.newBatchExecution(commands);
+        
+        Command startProcessCommand = commandsFactory.newStartProcess("myProject.myProcess");
+        // ((IdentifiableResult) startProcessCommand).setOutIdentifier("myProcessId");
+        commands.add(startProcessCommand);
+
+        ServiceResponse<ExecutionResults> reply1 = ruleClient.executeCommandsWithResults(CONTAINER_ID, executionCommand);
+        assertEquals(ServiceResponse.ResponseType.SUCCESS, reply1.getType());
+        
+        commands.clear();
+        commands.add(commandsFactory.newSignalEvent(1, "start", person)); // in the doc ID '1001' is used.
+
+        ServiceResponse<ExecutionResults> reply2 = ruleClient.executeCommandsWithResults(CONTAINER_ID, executionCommand);
+        assertEquals(ServiceResponse.ResponseType.SUCCESS, reply2.getType());
+    }    
+
+}
