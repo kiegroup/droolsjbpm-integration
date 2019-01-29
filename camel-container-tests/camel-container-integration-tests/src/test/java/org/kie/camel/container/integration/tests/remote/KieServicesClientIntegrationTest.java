ce44b2f7dbdac49bf9781469ece0010fe985bee7
Jiri Petrlik <jiripetrlik@gmail.com>
2019-01-29 17:08:07 +0100

    [BAQE-829] Implement more tests for KieServices client

--- camel-container-tests/camel-container-integration-tests/src/test/java/org/kie/camel/container/integration/tests/remote/KieServicesClientIntegrationTest.java
+++ camel-container-tests/camel-container-integration-tests/src/test/java/org/kie/camel/container/integration/tests/remote/KieServicesClientIntegrationTest.java
@@ -15,15 +15,81 @@
 
 package org.kie.camel.container.integration.tests.remote;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.kie.camel.container.api.ExecutionServerCommand;
+import org.kie.server.api.model.KieContainerResource;
 import org.kie.server.api.model.KieContainerResourceList;
+import org.kie.server.api.model.KieContainerStatus;
+import org.kie.server.api.model.KieServerInfo;
+import org.kie.server.api.model.KieServerStateInfo;
 
 public class KieServicesClientIntegrationTest extends AbstractRemoteIntegrationTest {
 
+    private static final String KIE_SERVER_CAPABILITY = "KieServer";
+
     @Test
-    public void listContainerTest() {
+    public void testGetKieServerInfo() {
+        final ExecutionServerCommand executionServerCommand = new ExecutionServerCommand();
+        executionServerCommand.setClient("kieServices");
+        executionServerCommand.setOperation("getServerInfo");
+        Object response = runOnExecutionServer(executionServerCommand);
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response).isInstanceOf(KieServerInfo.class);
+
+        final KieServerInfo kieServerInfo = (KieServerInfo) response;
+        Assertions.assertThat(kieServerInfo.getCapabilities()).contains(KIE_SERVER_CAPABILITY);
+    }
+
+    @Test
+    public void testGetKieServerState() {
+        final ExecutionServerCommand executionServerCommand = new ExecutionServerCommand();
+        executionServerCommand.setClient("kieServices");
+        executionServerCommand.setOperation("getServerState");
+        Object response = runOnExecutionServer(executionServerCommand);
+        Assertions.assertThat(response).isNotNull();
+
+        final KieServerStateInfo kieServerStateInfo = (KieServerStateInfo) response;
+        Assertions.assertThat(kieServerStateInfo.getContainers()).isNotEmpty();
+    }
+
+    @Test
+    public void testGetContainerResource() {
+        final KieContainerResource kieContainerResource = getContainerResource(CONTAINER_ID);
+        Assertions.assertThat(kieContainerResource).isNotNull();
+        Assertions.assertThat(kieContainerResource.getContainerId()).isEqualTo(CONTAINER_ID);
+        Assertions.assertThat(kieContainerResource.getReleaseId()).isEqualToComparingFieldByField(RELEASE_ID);
+    }
+
+    @Test
+    public void testDeactivateActivateContainer() {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put("id", CONTAINER_ID);
+
+        final ExecutionServerCommand deactivateContainerCommand = new ExecutionServerCommand();
+        deactivateContainerCommand.setClient("kieServices");
+        deactivateContainerCommand.setOperation("deactivateContainer");
+        deactivateContainerCommand.setParameters(parameters);
+        runOnExecutionServer(deactivateContainerCommand);
+
+        KieContainerResource kieContainerResource = getContainerResource(CONTAINER_ID);
+        Assertions.assertThat(kieContainerResource.getStatus()).isEqualTo(KieContainerStatus.DEACTIVATED);
+
+        final ExecutionServerCommand activateContainerCommand = new ExecutionServerCommand();
+        activateContainerCommand.setClient("kieServices");
+        activateContainerCommand.setOperation("activateContainer");
+        activateContainerCommand.setParameters(parameters);
+        runOnExecutionServer(activateContainerCommand);
+
+        kieContainerResource = getContainerResource(CONTAINER_ID);
+        Assertions.assertThat(kieContainerResource.getStatus()).isEqualTo(KieContainerStatus.STARTED);
+    }
+
+    @Test
+    public void testListContainers() {
         final ExecutionServerCommand executionServerCommand = new ExecutionServerCommand();
         executionServerCommand.setClient("kieServices");
         executionServerCommand.setOperation("listContainers");
@@ -34,5 +100,20 @@ public class KieServicesClientIntegrationTest extends AbstractRemoteIntegrationT
         final KieContainerResourceList kieContainerResourceList = (KieContainerResourceList) response;
         Assertions.assertThat(kieContainerResourceList.getContainers()).hasSize(1);
         Assertions.assertThat(kieContainerResourceList.getContainers().get(0).getContainerId()).isEqualTo(CONTAINER_ID);
+    }
+
+    private KieContainerResource getContainerResource(String containerId) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put("id", containerId);
+        final ExecutionServerCommand executionServerCommand = new ExecutionServerCommand();
+        executionServerCommand.setClient("kieServices");
+        executionServerCommand.setOperation("getContainerInfo");
+        executionServerCommand.setParameters(parameters);
+        Object response = runOnExecutionServer(executionServerCommand);
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response).isInstanceOf(KieContainerResource.class);
+
+        final KieContainerResource kieContainerResource = (KieContainerResource) response;
+        return kieContainerResource;
     }
 }
