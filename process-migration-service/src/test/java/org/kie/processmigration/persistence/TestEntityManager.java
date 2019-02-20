ff2ddb9d1ced83f6489b9e34de126b5a0956f341
Ruben Romero Montes <rromerom@redhat.com>
2019-02-20 11:04:27 +0100

    Add missing header
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- process-migration-service/src/test/java/org/kie/processmigration/persistence/TestEntityManager.java
+++ process-migration-service/src/test/java/org/kie/processmigration/persistence/TestEntityManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kie.processmigration.persistence;
 
 import java.util.List;
@@ -21,7 +37,7 @@ import javax.persistence.metamodel.Metamodel;
 /**
  * Convenience EntityManager created to allow defining Spies for EntityManager
  * With Mockito 1.x and JUnit 4 is not possible to Spy final classes
- * 
+ *
  */
 public class TestEntityManager implements EntityManager {
 
