4b05a2fb6074f061b9db2fc743c88be25ad92abe
Luca Molteni <volothamp@gmail.com>
2019-05-15 16:37:41 +0200

    Removed import

--- kie-plugins-testing/src/test/java/org/kie/maven/plugin/KieMavenPluginBaseIntegrationTest.java
+++ kie-plugins-testing/src/test/java/org/kie/maven/plugin/KieMavenPluginBaseIntegrationTest.java
@@ -17,7 +17,6 @@ package org.kie.maven.plugin;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import io.takari.maven.testing.TestResources;
