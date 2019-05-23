7772b16223cb7c00a132affec5403635608d02aa
Ruben Romero Montes <rromerom@redhat.com>
2019-05-23 08:58:51 +0200

    [JBPM-8487] Generate frontend tests results in junit format
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

---
+++ process-migration-service/frontend/jest.config.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = {
+  reporters: [
+        "default",
+                [
+                  "jest-junit",
+                   {
+                   /*
+                    * Removes spaces from test titles and makes first
+                    * letter of each word capitalized.
+                    *
+                    * unit test -> UnitTest
+                    *
+                    * See junit.xml report for resulting look.
+                    */
+                    titleTemplate: (vars) => {
+                            var str = vars.title.toLowerCase();
+                            str = str.split(' ');
+                            for (var i = 0; i < str.length; i++) {
+                                str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+                            }
+                            var result = str.join('');
+                            return result.replace(',');
+                        }
+
+                    }
+                ]
+  ],
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["js", "jsx"],
+  testRegex: "/__tests__/.*\\.test\\.(jsx?)$",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  }
+};
