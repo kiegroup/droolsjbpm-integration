bca4ccf4f3255b8894a8ffb2532e8f1cdb3fef83
Ruben Romero Montes <rromerom@redhat.com>
2019-05-29 22:59:04 +0200

    [JBPM-8487] Set outputName
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- process-migration-service/frontend/jest.config.js
+++ process-migration-service/frontend/jest.config.js
@@ -14,32 +14,32 @@
  * limitations under the License.
  */
 
+// eslint-disable-next-line no-undef
 module.exports = {
   reporters: [
-        "default",
-                [
-                  "jest-junit",
-                   {
-                   /*
-                    * Removes spaces from test titles and makes first
-                    * letter of each word capitalized.
-                    *
-                    * unit test -> UnitTest
-                    *
-                    * See junit.xml report for resulting look.
-                    */
-                    titleTemplate: (vars) => {
-                            var str = vars.title.toLowerCase();
-                            str = str.split(' ');
-                            for (var i = 0; i < str.length; i++) {
-                                str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-                            }
-                            var result = str.join('');
-                            return result.replace(',');
-                        }
-
-                    }
-                ]
+    "default",
+    [
+      "jest-junit",
+      {
+        /*
+         * Removes spaces from test titles and makes first
+         * letter of each word capitalized.
+         *
+         * unit test -> UnitTest
+         *
+         * See junit.xml report for resulting look.
+         */
+        titleTemplate: vars => {
+          var str = vars.title.toLowerCase();
+          str = str.split(" ");
+          for (var i = 0; i < str.length; i++) {
+            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+          }
+          var result = str.join("");
+          return result.replace(",");
+        }
+      }
+    ]
   ],
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["js", "jsx"],
