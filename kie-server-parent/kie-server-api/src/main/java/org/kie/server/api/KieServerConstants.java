43453871e41f1caf3a69a2f444d7cdce77534a37
Enrique Gonzalez <egonzale@redhat.com>
2019-11-09 14:57:06 +0100

    [JBPM-8885] Nodes after multi-Instance subprocess are not blurred eventhough process instance is completed. (#1934)
    Added a property to increase the number of nodes retrieved from database

--- kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
+++ kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
@@ -55,6 +55,7 @@ public class KieServerConstants {
     public static final String KIE_SERVER_MODE = "org.kie.server.mode";
     public static final String KIE_SERVER_INCLUDE_STACKTRACE = "org.kie.server.stacktrace.included";
     public static final String KIE_SERVER_STRICT_ID_FORMAT = "org.kie.server.strict.id.format";
+    public static final String KIE_SERVER_IMAGESERVICE_MAX_NODES = "org.kie.server.service.image.max_nodes";
 
     // configuration parameters
     public static final String CFG_PERSISTANCE_DS = "org.kie.server.persistence.ds";
