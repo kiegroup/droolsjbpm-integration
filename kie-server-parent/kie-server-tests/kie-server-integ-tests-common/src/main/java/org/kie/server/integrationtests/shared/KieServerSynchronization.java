f9dcf786e962631ed8b4cc9a7cff61c65055a20f
Luca Molteni <volothamp@gmail.com>
2020-01-09 14:02:55 +0100

    Increased timeout in KieServerSynchronization.java

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-common/src/main/java/org/kie/server/integrationtests/shared/KieServerSynchronization.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-common/src/main/java/org/kie/server/integrationtests/shared/KieServerSynchronization.java
@@ -55,7 +55,7 @@ import org.kie.server.controller.client.KieServerControllerClient;
 
 public class KieServerSynchronization {
 
-    private static final long SERVICE_TIMEOUT = 120000;
+    private static final long SERVICE_TIMEOUT = 240000;
     private static final long TIMEOUT_BETWEEN_CALLS = 800;
 
     public static void waitForJobToFinish(final JobServicesClient jobServicesClient, final Long jobId) throws Exception {
