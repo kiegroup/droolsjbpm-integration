87ac241969db7734dae55de7e9b4ba8b7ead7213
Karel Suta <ksuta@redhat.com>
2020-02-20 09:06:38 +0100

    Revert "Refactor KJar builds for Kie server tests to use installed Maven binary (#2019)"
    This reverts commit 1351e1d55e06d288c7a051b73a6b5c807b57a1d5.

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-common/src/main/java/org/kie/server/integrationtests/shared/KieServerDeployer.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-common/src/main/java/org/kie/server/integrationtests/shared/KieServerDeployer.java
@@ -18,16 +18,15 @@ package org.kie.server.integrationtests.shared;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.cli.MavenCli;
 import org.apache.maven.project.MavenProject;
 import org.appformer.maven.integration.MavenRepository;
 import org.appformer.maven.integration.embedder.MavenProjectLoader;
@@ -62,9 +61,14 @@ public class KieServerDeployer {
     }
 
     public static void buildAndDeployMavenProject(String basedir) {
+        // need to backup (and later restore) the current class loader, because the Maven/Plexus does some classloader
+        // magic which then results in CNFE in RestEasy client
         // run the Maven build which will create the kjar. The kjar is then either installed or deployed to local and
         // remote repo
         logger.debug("Building and deploying Maven project from basedir '{}'.", basedir);
+        ClassLoader classLoaderBak = Thread.currentThread().getContextClassLoader();
+        System.setProperty("maven.multiModuleProjectDirectory", basedir); // required by MavenCli 3.3.0+
+        MavenCli cli = new MavenCli();
         List<String> mvnArgs;
         if (TestConfig.isLocalServer()) {
             // just install into local repository when running the local server. Deploying to remote repo will fail
@@ -72,9 +76,9 @@ public class KieServerDeployer {
 
             // wrapping explicitly in ArrayList as we may need to update the list later (and Arrays.toList() returns
             // just read-only list)
-            mvnArgs = new ArrayList<>(Arrays.asList("mvn", "--quiet", "clean", "install"));
+            mvnArgs = new ArrayList<String>(Arrays.asList("-B", "clean", "install"));
         } else {
-            mvnArgs = new ArrayList<>(Arrays.asList("mvn", "--quiet", "-e", "clean", "deploy"));
+            mvnArgs = new ArrayList<String>(Arrays.asList("-B", "-e", "clean", "deploy"));
         }
 
         // use custom settings.xml file, if one specified
@@ -83,15 +87,13 @@ public class KieServerDeployer {
             mvnArgs.add("-s");
             mvnArgs.add(kjarsBuildSettingsXml);
         }
+        int mvnRunResult = cli.doMain(mvnArgs.toArray(new String[mvnArgs.size()]), basedir, System.out, System.err);
 
-        // Execute the Maven build using installed Maven binary
-        try (ProcessExecutor executor = new ProcessExecutor()) {
-            String command = mvnArgs.stream().collect(Collectors.joining(" "));
-            boolean executionSuccessful = executor.executeProcessCommand(command, Paths.get(basedir));
+        Thread.currentThread().setContextClassLoader(classLoaderBak);
 
-            if (!executionSuccessful) {
-                throw new RuntimeException("Error while building Maven project from basedir " + basedir);
-            }
+        if (mvnRunResult != 0) {
+            throw new RuntimeException("Error while building Maven project from basedir " + basedir +
+                    ". Return code=" + mvnRunResult);
         }
         logger.debug("Maven project successfully built and deployed!");
     }
