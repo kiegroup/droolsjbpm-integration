6fdbebdf26f984e800702cc339af148be6eef449
Enrique Gonzalez Martinez <egonzale@egonzale.remote.csb>
2020-05-13 11:23:26 +0200

    [BPAL-1526] Search for tasks / process instances / cases by multiple constraints

--- kie-server-parent/kie-server-services/kie-server-services-jbpm/src/main/java/org/kie/server/services/jbpm/ConvertUtils.java
+++ kie-server-parent/kie-server-services/kie-server-services-jbpm/src/main/java/org/kie/server/services/jbpm/ConvertUtils.java
@@ -577,6 +577,7 @@ public class ConvertUtils {
             var.setProcessInstanceId(desc.getProcessInstanceId());
             var.setProcessVariables(desc.getProcessVariables());
             var.setInputVariables(desc.getInputdata());
+            var.setStatus(desc.getStatus());
             data.add(var);
         }
         ProcessInstanceUserTaskWithVariablesList result = new ProcessInstanceUserTaskWithVariablesList();
