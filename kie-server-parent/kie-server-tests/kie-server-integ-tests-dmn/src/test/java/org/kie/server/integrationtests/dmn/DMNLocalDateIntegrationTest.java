4646a257ea4cb641817308e84c3a10ec37651fbf
Jozef Marko <jomarko@redhat.com>
2020-05-27 08:56:48 +0200

    JBPM-9173: Create reproducer test for dmn date on kie server evaluation
    This PR is not a fix.
    For more details see: https://issues.redhat.com/browse/JBPM-9173

---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-dmn/src/test/java/org/kie/server/integrationtests/dmn/DMNLocalDateIntegrationTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.server.integrationtests.dmn;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.server.api.model.KieServiceResponse.ResponseType;
+import org.kie.server.api.model.ReleaseId;
+import org.kie.server.api.model.ServiceResponse;
+import org.kie.server.integrationtests.shared.KieServerDeployer;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class DMNLocalDateIntegrationTest
+        extends DMNKieServerBaseIntegrationTest {
+
+    private static final ReleaseId kjar1 = new ReleaseId(
+            "org.kie.server.testing", "local-date-unary-check",
+            "1.0.0.Final");
+
+    private static final String CONTAINER_1_ID = "local-date-unary-check";
+    private static final String CONTAINER_1_ALIAS = "localdate";
+
+    @BeforeClass
+    public static void deployArtifacts() {
+        KieServerDeployer.buildAndDeployCommonMavenParent();
+        KieServerDeployer.buildAndDeployMavenProjectFromResource("/kjars-sources/local-date-unary-check");
+
+        kieContainer = KieServices.Factory.get().newKieContainer(kjar1);
+        createContainer(CONTAINER_1_ID, kjar1, CONTAINER_1_ALIAS);
+    }
+
+    @Override
+    protected void addExtraCustomClasses(Map<String, Class<?>> extraClasses)
+            throws Exception {
+    }
+
+    @Test
+    @Ignore("JBPM-9173")
+    public void test_evaluateLocaleDateUnaryCheck() {
+        DMNContext dmnContext = dmnClient.newContext();
+        final Map<String, Object> personData = new HashMap<>();
+        personData.put("id", "person-id-1");
+        personData.put("name", "Eager Join");
+        personData.put("dojoining", LocalDate.of(2021, 8, 11));
+        dmnContext.set("PersonData", personData);
+        ServiceResponse<DMNResult> evaluateAll = dmnClient.evaluateAll(CONTAINER_1_ID,
+                                                                       "https://kiegroup.org/dmn/_5851203A-5DA1-4020-B7D1-E23C89634164",
+                                                                       "Test",
+                                                                       dmnContext);
+
+        assertEquals(ResponseType.SUCCESS, evaluateAll.getType());
+
+        DMNResult dmnResult = evaluateAll.getResult();
+
+        final Map<String, Object> expectedPerson = new HashMap<>();
+        expectedPerson.put("id", "abc");
+        expectedPerson.put("name", "xyz");
+
+        assertThat(dmnResult.getDecisionResults().size(), is(1));
+        assertThat(dmnResult.getDecisionResultByName("PersonDecision").getResult(), is(expectedPerson));
+    }
+}
