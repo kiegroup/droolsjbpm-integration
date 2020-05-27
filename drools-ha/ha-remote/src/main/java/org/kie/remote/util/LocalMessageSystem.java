024d553acabcc5fe33cf0dc7f5cb9aca0873dc90
Daniele Zonca <dzonca@redhat.com>
2020-05-27 18:30:15 +0200

    Revert "[Drools 5321] Failing test org.kie.hacep.LocalStorageKieSessionTest.fireUntilHaltTest on timeout (#2096)"
    This reverts commit 006f060c2e6eee983c33f07deff6ba6463f39e0c.

--- drools-ha/ha-remote/src/main/java/org/kie/remote/util/LocalMessageSystem.java
+++ drools-ha/ha-remote/src/main/java/org/kie/remote/util/LocalMessageSystem.java
@@ -22,57 +22,35 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class LocalMessageSystem {
 
     private Map<String, BlockingQueue<Object>> queues = new HashMap<>();
-    private static Logger logger = LoggerFactory.getLogger(LocalMessageSystem.class);
 
     private LocalMessageSystem() { }
 
     private BlockingQueue<Object> queueForTopic(String topic) {
-        if(logger.isDebugEnabled()) {
-            logger.debug("queueForTopic:{}", topic);
-        }
         return queues.computeIfAbsent( topic, k -> new LinkedBlockingQueue<>() );
     }
 
     public void put(String topic, Object message) {
-        if(logger.isDebugEnabled()) {
-            logger.debug("put topic:{} msg:{}", topic, message);
-        }
         if (!queueForTopic(topic).offer(message)) {
             throw new IllegalStateException("msg :"+message +" not added in the topic:" +topic);
         }
     }
 
     public Object peek(String topic) {
-        if(logger.isDebugEnabled()) {
-            logger.debug("peek topic:{}", topic);
-        }
         return queueForTopic(topic).peek();
     }
 
     public Object poll(String topic) {
-        if(logger.isDebugEnabled()) {
-            logger.debug("poll topic:{}", topic);
-        }
         return queueForTopic(topic).poll();
     }
 
     public static LocalMessageSystem get() {
-        if(logger.isDebugEnabled()) {
-            logger.debug("get");
-        }
         return LazyHolder.get();
     }
 
     public Object poll(String topic, int durationMillis) {
-        if(logger.isDebugEnabled()) {
-            logger.debug("poll topic:{} durationMillis:{}", topic, durationMillis);
-        }
         try {
             return queueForTopic(topic).poll(durationMillis, TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
