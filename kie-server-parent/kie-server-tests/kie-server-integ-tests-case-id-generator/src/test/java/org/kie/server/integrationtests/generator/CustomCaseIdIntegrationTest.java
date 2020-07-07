47ebd00c7723547850d15868b4a8ac71d8428cec
Gonzalo Muñoz <gmunozfe@redhat.com>
2020-07-07 19:57:34 +0200

    [JBPM-9094] Integration tests for new customCaseIdPrefix ability

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-case-id-generator/src/test/java/org/kie/server/integrationtests/generator/CustomCaseIdIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-case-id-generator/src/test/java/org/kie/server/integrationtests/generator/CustomCaseIdIntegrationTest.java
@@ -13,22 +13,26 @@
  * limitations under the License.
 */
 
-package org.kie.server.integrationtests.policy;
+package org.kie.server.integrationtests.generator;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
 
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kie.api.KieServices;
+import org.kie.server.api.exception.KieServicesException;
 import org.kie.server.api.model.ReleaseId;
 import org.kie.server.api.model.cases.CaseFile;
 import org.kie.server.integrationtests.config.TestConfig;
 import org.kie.server.integrationtests.shared.KieServerDeployer;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class CustomCaseIdIntegrationTest extends CaseIdBaseIntegrationTest {
 
@@ -36,6 +40,7 @@ public class CustomCaseIdIntegrationTest extends CaseIdBaseIntegrationTest {
             "1.0.0.Final");
 
     private static final String CASE_HR_DEF_ID = "UserTaskCase";
+    private static final String PREFIX_VARIABLE_LOWER_CAPITALIZE = "EmptyCaseIdExpressionPrefixVariableLowerCapitalize";
 
     private static final String CONTAINER_ID = "insurance";
 
@@ -53,7 +58,7 @@ public class CustomCaseIdIntegrationTest extends CaseIdBaseIntegrationTest {
     }
 
     @Test
-    public void testCustomCaseId() throws Exception {
+    public void testCustomCaseId() {
         Assume.assumeFalse(TestConfig.isLocalServer());
 
         Map<String, Object> data = new HashMap<>();
@@ -66,5 +71,38 @@ public class CustomCaseIdIntegrationTest extends CaseIdBaseIntegrationTest {
         String caseId = caseClient.startCase(CONTAINER_ID, CASE_HR_DEF_ID, caseFile);
         assertNotNull(caseId);
         assertTrue("Created Case Id doesn't start with expected value, value is: " + caseId, caseId.startsWith("HR-01234"));
+    }
+    
+    @Test
+    public void testCustomCaseIdWithOptionalParams() {
+        Assume.assumeFalse(TestConfig.isLocalServer());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("var1", "VALUE1");
+        data.put("country", "spa");
+        CaseFile caseFile = CaseFile.builder()
+                .addUserAssignments(CASE_OWNER_ROLE, USER_YODA)
+                .data(data)
+                .build();
+
+        String caseId = caseClient.startCase(CONTAINER_ID, PREFIX_VARIABLE_LOWER_CAPITALIZE, caseFile);
+        assertNotNull(caseId);
+        assertTrue("Created Case Id doesn't match expected value, value is: " + caseId, 
+                    caseId.matches(CONTAINER_ID+"-value1-Spa-01234[0-9]+"));
+                
+    }
+    
+    @Test
+    public void testCustomCaseIdWithoutParamsThrowsException() {
+        Assume.assumeFalse(TestConfig.isLocalServer());
+
+        CaseFile caseFile = CaseFile.builder()
+                .addUserAssignments(CASE_OWNER_ROLE, USER_YODA)
+                .data(Collections.emptyMap())
+                .build();
+
+        assertThatThrownBy(() -> caseClient.startCase(CONTAINER_ID, PREFIX_VARIABLE_LOWER_CAPITALIZE, caseFile))
+                .isInstanceOf(KieServicesException.class)
+                .hasMessageContaining("Case Id Prefix cannot be generated");
     }
 }
