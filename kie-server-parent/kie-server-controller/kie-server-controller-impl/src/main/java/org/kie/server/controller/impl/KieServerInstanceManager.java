f81d01ca3e5703403e97b8186976d1b1a9aaf703
Xiaofeng Bai <bxf12315@gmail.com>
2020-09-15 13:41:02 +0800

    JBPM-9273 System error occurred while adding deployment unit

--- kie-server-parent/kie-server-controller/kie-server-controller-impl/src/main/java/org/kie/server/controller/impl/KieServerInstanceManager.java
+++ kie-server-parent/kie-server-controller/kie-server-controller-impl/src/main/java/org/kie/server/controller/impl/KieServerInstanceManager.java
@@ -22,9 +22,11 @@ import java.util.ServiceLoader;
 import org.kie.server.api.KieServerConstants;
 import org.kie.server.api.model.KieContainerResource;
 import org.kie.server.api.model.KieContainerResourceList;
+import org.kie.server.api.model.KieContainerStatus;
 import org.kie.server.api.model.KieScannerResource;
 import org.kie.server.api.model.KieScannerStatus;
 import org.kie.server.api.model.KieServerConfigItem;
+import org.kie.server.api.model.KieServiceResponse;
 import org.kie.server.api.model.Message;
 import org.kie.server.api.model.ReleaseId;
 import org.kie.server.api.model.ServiceResponse;
@@ -81,6 +83,7 @@ public class KieServerInstanceManager {
                                                         logger.debug("Scanner failed to start on server instance {} due to {}",
                                                                      container.getUrl(),
                                                                      response.getMsg());
+                                                        container.setStatus(KieContainerStatus.FAILED);
                                                     }
                                                     collectContainerInfo(containerSpec,
                                                                          client,
@@ -140,6 +143,7 @@ public class KieServerInstanceManager {
                                                         logger.debug("Scanner (scan now) failed on server instance {} due to {}",
                                                                      container.getUrl(),
                                                                      response.getMsg());
+                                                        container.setStatus(KieContainerStatus.FAILED);
                                                     }
                                                     collectContainerInfo(containerSpec,
                                                                          client,
@@ -168,6 +172,9 @@ public class KieServerInstanceManager {
 
                 if (response.getType() != ServiceResponse.ResponseType.SUCCESS) {
                     log("Container {} failed to start on server instance {} due to {}", container, response, containerSpec);
+                    if (response.getType() == ServiceResponse.ResponseType.FAILURE) {
+                        container.setStatus(KieContainerStatus.FAILED);
+                    }
                 }
 
                 collectContainerInfo(containerSpec, client, container);
@@ -383,6 +390,8 @@ public class KieServerInstanceManager {
                     container.setServerTemplateId(serverTemplate.getId());
                     container.setStatus(containerResource.getStatus());
                     container.setMessages(containerResource.getMessages());
+                } else {
+                    container.setStatus(KieContainerStatus.FAILED);
                 }
 
                 return null;
