e1e89a759a704c2ab817b8f0100dc002606eebf6
Jiri Petrlik <jiripetrlik@gmail.com>
2020-10-06 14:06:51 +0200

    [JBPM-9414] Adding integration tests for RHPAM-3106

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/FlightRequest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/FlightRequest.java
@@ -2,53 +2,16 @@
 package com.jboss.soap.service.acmedemo;
 
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for flightRequest complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="flightRequest"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="startCity" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="endCity" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="startDate" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="endDate" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "flightRequest", propOrder = {
-    "startCity",
-    "endCity",
-    "startDate",
-    "endDate"
-})
 public class FlightRequest
     implements Serializable
 {
 
     private final static long serialVersionUID = 1L;
-    @XmlElement(required = true)
     protected String startCity;
-    @XmlElement(required = true)
     protected String endCity;
-    @XmlElement(required = true)
     protected String startDate;
-    @XmlElement(required = true)
     protected String endDate;
 
     /**
--- kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/ListAvailablePlanesResponse.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/ListAvailablePlanesResponse.java
@@ -2,41 +2,13 @@
 package com.jboss.soap.service.acmedemo;
 
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for listAvailablePlanesResponse complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="listAvailablePlanesResponse"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="return" type="{http://service.soap.jboss.com/AcmeDemo/}flight" minOccurs="0"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "listAvailablePlanesResponse", propOrder = {
-    "_return"
-})
 public class ListAvailablePlanesResponse
     implements Serializable
 {
 
     private final static long serialVersionUID = 1L;
-    @XmlElement(name = "return")
     protected Flight _return;
 
     /**
