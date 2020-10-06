191b7b5aac72a1f6f2a4aa0ad83aba5b027b24e8
Jiri Petrlik <jiripetrlik@gmail.com>
2020-10-06 14:06:51 +0200

    [JBPM-9414] Adding integration tests for RHPAM-3106

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/ListAvailablePlanes.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/webservice-project/src/main/java/com/jboss/soap/service/acmedemo/ListAvailablePlanes.java
@@ -2,34 +2,8 @@
 package com.jboss.soap.service.acmedemo;
 
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for listAvailablePlanes complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="listAvailablePlanes"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="in" type="{http://service.soap.jboss.com/AcmeDemo/}flightRequest" minOccurs="0"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
- * </pre>
- * 
- * 
- */
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "listAvailablePlanes", propOrder = {
-    "in"
-})
 public class ListAvailablePlanes
     implements Serializable
 {
