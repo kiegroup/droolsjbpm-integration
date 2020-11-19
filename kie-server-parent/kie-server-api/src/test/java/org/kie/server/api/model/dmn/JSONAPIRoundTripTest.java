5e5cda891dbc8a34323ec2b77d8ab750c3899682
tarilabs <matteo.mortari@gmail.com>
2020-11-19 10:28:27 +0100

    .

--- kie-server-parent/kie-server-api/src/test/java/org/kie/server/api/model/dmn/JSONAPIRoundTripTest.java
+++ kie-server-parent/kie-server-api/src/test/java/org/kie/server/api/model/dmn/JSONAPIRoundTripTest.java
@@ -126,20 +126,26 @@ public class JSONAPIRoundTripTest {
 
         @Override
         public boolean equals(Object obj) {
-            if (this == obj)
+            if (this == obj) {
                 return true;
-            if (obj == null)
+            }
+            if (obj == null) {
                 return false;
-            if (getClass() != obj.getClass())
+            }
+            if (getClass() != obj.getClass()) {
                 return false;
+            }
             Applicant other = (Applicant) obj;
-            if (age != other.age)
+            if (age != other.age) {
                 return false;
+            }
             if (name == null) {
-                if (other.name != null)
+                if (other.name != null) {
                     return false;
-            } else if (!name.equals(other.name))
+                }
+            } else if (!name.equals(other.name)) {
                 return false;
+            }
             return true;
         }
 
