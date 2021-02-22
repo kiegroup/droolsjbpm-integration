3f4725b92a1fbe130ba30d66b26592e8cc28bef9
egonzale <egonzale@redhat.com>
2021-02-22 10:13:07 +0100

    [JBPM-9624] Return the process variables for synchronous execution use cases

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/java/org/kie/server/integrationtests/jbpm/JbpmKieServerBaseIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/java/org/kie/server/integrationtests/jbpm/JbpmKieServerBaseIntegrationTest.java
@@ -61,6 +61,7 @@ public abstract class JbpmKieServerBaseIntegrationTest extends RestJmsSharedBase
     protected static final String CONTAINER_ID_NOTIFICATION = "notification-project";
    
     protected static final String PROCESS_ID_RESTART = "restart-project.simple";
+    protected static final String PROCESS_SYNC_ID = "restart-project.syncProcess";
     protected static final String PROCESS_ID_USERTASK = "definition-project.usertask";
     protected static final String PROCESS_ID_USERTASK2 = "definition-project.usertask2";
     protected static final String PROCESS_ID_EVALUATION = "definition-project.evaluation";
