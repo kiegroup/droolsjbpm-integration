914e853dfa001d60ad5e10be7f64805cf2b929a6
tarilabs <matteo.mortari@gmail.com>
2021-02-25 12:54:16 +0100

    with test for model-specific type definition and JSON checks

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-dmn/src/test/java/org/kie/server/integrationtests/dmn/modelspecific/ModelSpecificIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-dmn/src/test/java/org/kie/server/integrationtests/dmn/modelspecific/ModelSpecificIntegrationTest.java
@@ -18,6 +18,8 @@ package org.kie.server.integrationtests.dmn.modelspecific;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.parser.OpenAPIV3Parser;
@@ -49,9 +51,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
+/**
+ * This test performs basics integrity check for the DMN model-specific endpoints, using the simple DMN models contained in `ms-endpoints` kjar.
+ *  - show test strategy applied to previously existing endpoints (non model-specific)
+ *  - basic consistency test per each endpoint
+ *  - demonstrate error headers for non dmnresult endpoint
+ *  - demonstrate decision service coercion as specified in requirements, for non dmn result endpoint
+ *  - validate OAS produced for the containerId
+ *  - demonstrate model-specific endpoints are consistent with InputData definitions, such as JSON date/time.
+ */
 public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
 
     private static final String X_KOGITO_DECISION_MESSAGES = "X-Kogito-decision-messages";
@@ -72,6 +83,7 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
     public static void deployArtifacts() {
         KieServerDeployer.buildAndDeployCommonMavenParent();
         KieServerDeployer.buildAndDeployMavenProjectFromResource("/kjars-sources/ms-endpoints");
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
     
     @Before
@@ -92,7 +104,7 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
 
     @Override
     protected KieServicesClient createDefaultClient() throws Exception {
-        return null;
+        return null; // intentional, as the new model-specific endpoint do not rely on the Kie-Server-Client Java API.
     }
 
     @Override
@@ -100,8 +112,12 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
         // no extra classes.
     }
 
+    private String baseContainerPath() {
+        return TestConfig.getKieServerHttpUrl() + "/containers/" + CONTAINER_ID;
+    }
+
     private HttpResponse makePOST(String pathInContainer, String payload) throws Exception {
-        HttpPost httpPost = new HttpPost(TestConfig.getKieServerHttpUrl() + "/containers/" + CONTAINER_ID + pathInContainer);
+        HttpPost httpPost = new HttpPost(baseContainerPath() + pathInContainer);
         httpPost.setEntity(new StringEntity(payload));
         httpPost.setHeader("Accept", "application/json");
         httpPost.setHeader("Content-type", "application/json");
@@ -113,7 +129,7 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
     }
 
     private HttpResponse makeGET(String pathInContainer, String accept) throws Exception {
-        HttpGet httpGet = new HttpGet(TestConfig.getKieServerHttpUrl() + "/containers/" + CONTAINER_ID + pathInContainer);
+        HttpGet httpGet = new HttpGet(baseContainerPath() + pathInContainer);
         httpGet.setHeader("Accept", accept);
         UsernamePasswordCredentials creds = new UsernamePasswordCredentials(TestConfig.getUsername(), TestConfig.getPassword());
         httpGet.addHeader(new BasicScheme().authenticate(creds, httpGet, null));
@@ -124,7 +140,7 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
 
     private String responseAsString(HttpResponse response) throws Exception {
         String asString = EntityUtils.toString(response.getEntity());
-        LOG.info("Response:\n\n{}\n\n", asString);
+        LOG.debug("Response:\n\n{}\n\n", asString);
         return asString;
     }
 
@@ -219,6 +235,36 @@ public class ModelSpecificIntegrationTest extends KieServerBaseIntegrationTest {
         String jsonString = responseAsString(response);
         Object readValue = mapper.readValue(jsonString, Object.class);
         Assertions.assertThat(readValue).extracting("dmnContext").extracting("Greeting Message").isEqualTo("Hello John Doe");
+    }
+
+    @Test
+    public void allTypes() {
+        final String JSON = "{\n" +
+                            "    \"InputBoolean\": true,\n" +
+                            "    \"InputDate\": \"2020-04-02\",\n" +
+                            "    \"InputDTDuration\": \"P1D\",\n" +
+                            "    \"InputDateAndTime\": \"2020-04-02T09:00:00\",\n" +
+                            "    \"InputNumber\": 1,\n" +
+                            "    \"InputString\": \"John Doe\",\n" +
+                            "    \"InputTime\": \"09:00\",\n" +
+                            "    \"InputYMDuration\": \"P1M\"\n" +
+                            "}";
+        RestAssured.given()
+                   .auth().preemptive().basic(TestConfig.getUsername(), TestConfig.getPassword())
+                   .contentType(ContentType.JSON)
+                   .accept(ContentType.JSON)
+                   .body(JSON)
+                   .post(baseContainerPath() + "/dmn/models/" + "OneOfEachType")
+                   .then()
+                   .statusCode(200)
+                   .body("DecisionBoolean", is(Boolean.FALSE))
+                   .body("DecisionDate", is("2020-04-03")) // as JSON is not schema aware, here we assert the RAW string
+                   .body("DecisionDTDuration", is("PT48H"))
+                   .body("DecisionDateAndTime", is("2020-04-02T10:00:00"))
+                   .body("DecisionNumber", is(2))
+                   .body("DecisionString", is("Hello, John Doe"))
+                   .body("DecisionTime", is("10:00:00"))
+                   .body("DecisionYMDuration", is("P2M"));
     }
 
     @Test
