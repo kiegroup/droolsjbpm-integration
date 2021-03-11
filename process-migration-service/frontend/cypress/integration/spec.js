5c0f8053eccef49c4d6886f28eddbef208afbc12
prashuts <pts@redhat.com>
2021-03-11 18:26:01 +0530

    Add spec.js

---
+++ process-migration-service/frontend/cypress/integration/spec.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+describe('Verify Process Instance Migration Homepage',function(){
+    it('Login and verify homepage headers',function(){
+        cy.visit('http://localhost:3000/')
+
+    })
+})
