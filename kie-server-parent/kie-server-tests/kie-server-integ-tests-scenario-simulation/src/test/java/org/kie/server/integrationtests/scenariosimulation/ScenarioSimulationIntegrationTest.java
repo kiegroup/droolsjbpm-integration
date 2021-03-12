4b853d1deca389951d11615390367a9d07bb9713
Yeser Amer <yamer@redhat.com>
2021-03-12 17:26:08 +0100

    KOGIT0-4190: Fixed tests

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-scenario-simulation/src/test/java/org/kie/server/integrationtests/scenariosimulation/ScenarioSimulationIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-scenario-simulation/src/test/java/org/kie/server/integrationtests/scenariosimulation/ScenarioSimulationIntegrationTest.java
@@ -106,7 +106,7 @@ public class ScenarioSimulationIntegrationTest
         assertEquals(1, ruleResponseFail.getResult().getRunCount());
         assertEquals("Test Scenario execution failed", ruleResponseFail.getMsg());
         assertFalse(ruleResponseFail.getResult().getFailures().isEmpty());
-        assertEquals("#1: KO scenario: Failed in \"Greeting Message\": The expected value is \"\"Hello John\"\" but the actual one is \"\"Hello John 1\"\"",
+        assertEquals("#1: KO scenario: Failed in \"String\": The expected value is \"John\" but the actual one is \"John 1\"",
                      ruleResponseFail.getResult().getFailures().get(0).getErrorMessage());
     }
 
