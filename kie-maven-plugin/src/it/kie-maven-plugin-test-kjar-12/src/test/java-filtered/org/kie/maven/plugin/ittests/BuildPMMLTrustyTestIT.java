9ce283a45fdf732a658b903b8957c70db1af8fa7
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-04-16 11:58:51 +0200

    [DROOLS-6275] Fixing BuildPMMLTrustyTestIT. Minor correction on documentation (#2458)

--- kie-maven-plugin/src/it/kie-maven-plugin-test-kjar-12/src/test/java-filtered/org/kie/maven/plugin/ittests/BuildPMMLTrustyTestIT.java
+++ kie-maven-plugin/src/it/kie-maven-plugin-test-kjar-12/src/test/java-filtered/org/kie/maven/plugin/ittests/BuildPMMLTrustyTestIT.java
@@ -18,8 +18,10 @@ package org.kie.maven.plugin.ittests;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -32,9 +34,9 @@ public class BuildPMMLTrustyTestIT {
     private static final String GAV_ARTIFACT_ID = "kie-maven-plugin-test-kjar-12";
     private static final String GAV_VERSION = "${org.kie.version}";
 
-
     private static final String PMML_FILE_NAME = "logisticregressionirisdata/logisticRegressionIrisData.pmml";
-    private static final String EXAMPLE_PMML_CLASS = "compoundnestedpredicatescorecard/PMMLRuleMappersImpl.class";
+    private static final List<String> EXAMPLE_PMML_CLASSES = Arrays.asList("compoundnestedpredicatescorecard/CompoundNestedPredicateScorecardFactory.class",
+                                                                           "compoundnestedpredicatescorecard/compoundnestedpredicatescorecard/PMMLRuleMappersImpl.class");
 
     @Test
     public void testContentKjarWithPMML() throws Exception {
@@ -51,6 +53,6 @@ public class BuildPMMLTrustyTestIT {
 
         Assertions.assertThat(jarContent).isNotEmpty();
         Assertions.assertThat(jarContent).contains(PMML_FILE_NAME);
-        Assertions.assertThat(jarContent).contains(EXAMPLE_PMML_CLASS);
+        EXAMPLE_PMML_CLASSES.forEach(examplePMMLClass ->  Assertions.assertThat(jarContent).contains(examplePMMLClass));
     }
 }
