a0958dcd9ac5f95e33d706ef6f0f7956361f2182
bklimas <boguslaw.klimas@grandparade.co.uk>
2021-08-23 21:16:54 +0200

    [JBPM-9859] Fixing after review

--- kie-server-parent/kie-server-services/kie-server-services-jbpm/src/main/java/org/kie/server/services/jbpm/locator/ProcessContainerLocatorProvider.java
+++ kie-server-parent/kie-server-services/kie-server-services-jbpm/src/main/java/org/kie/server/services/jbpm/locator/ProcessContainerLocatorProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kie.server.services.jbpm.locator;
 
 import java.util.HashMap;
@@ -9,12 +24,10 @@ import org.kie.server.services.api.ContainerLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.kie.server.api.KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR;
+
 public class ProcessContainerLocatorProvider {
     private static final Logger logger = LoggerFactory.getLogger(ProcessContainerLocatorProvider.class);
-
-    private String processInstanceLocatorName =
-        System.getProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR,
-            ByProcessInstanceIdContainerLocator.class.getSimpleName());
 
     private static final ProcessContainerLocatorProvider INSTANCE = new ProcessContainerLocatorProvider();
     private final Map<String, ContainerLocatorFactory> locators = new HashMap<>();
@@ -32,19 +45,18 @@ public class ProcessContainerLocatorProvider {
     }
 
     public ContainerLocator getLocator(final Number processInstanceId) {
+        String processInstanceLocatorName = System.getProperty(KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR,
+                                                                ByProcessInstanceIdContainerLocator.class.getSimpleName());
+
         ContainerLocatorFactory containerLocatorFactory = locators.get(processInstanceLocatorName);
 
         if (containerLocatorFactory == null) {
-            throw new IllegalStateException("No container locator factory found under name " + processInstanceLocatorName);
+            throw new IllegalStateException("No container locator factory found under name " + processInstanceLocatorName
+                + " please review '" + KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR + "' property value");
         }
 
-        logger.info("Container locator factory was finding '{}'", containerLocatorFactory.getClass().getSimpleName());
+        logger.info("Container locator factory was found '{}'", containerLocatorFactory.getClass().getSimpleName());
         return containerLocatorFactory.create(processInstanceId);
-    }
-
-    public void readSystemProperty() {
-        processInstanceLocatorName = System.getProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR,
-                ByProcessInstanceIdContainerLocator.class.getSimpleName());
     }
 
     public static ProcessContainerLocatorProvider get() {
--- kie-server-parent/kie-server-services/kie-server-services-jbpm/src/test/java/org/kie/server/services/jbpm/locator/ProcessContainerLocatorProviderTest.java
+++ kie-server-parent/kie-server-services/kie-server-services-jbpm/src/test/java/org/kie/server/services/jbpm/locator/ProcessContainerLocatorProviderTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kie.server.services.jbpm.locator;
 
 import org.hamcrest.MatcherAssert;
@@ -13,7 +28,6 @@ public class ProcessContainerLocatorProviderTest {
     @Before
     public void setUp() {
         System.clearProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR);
-        ProcessContainerLocatorProvider.get().readSystemProperty();
     }
 
     @Test
@@ -27,7 +41,6 @@ public class ProcessContainerLocatorProviderTest {
     public void testShouldReturnLocatorSetInProperty() {
         System.setProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR,
             ByContextMappingInfoContainerLocator.class.getSimpleName());
-        ProcessContainerLocatorProvider.get().readSystemProperty();
 
         ContainerLocator containerLocator = ProcessContainerLocatorProvider.get().getLocator(1L);
 
@@ -38,7 +51,6 @@ public class ProcessContainerLocatorProviderTest {
     public void testShouldReturnLocatorFromServices() {
         System.setProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR,
             "ContainerLocatorFactoryMock");
-        ProcessContainerLocatorProvider.get().readSystemProperty();
 
         ContainerLocator containerLocator = ProcessContainerLocatorProvider.get().getLocator(1L);
 
@@ -48,7 +60,6 @@ public class ProcessContainerLocatorProviderTest {
     @Test(expected = IllegalStateException.class)
     public void testLocatorNotExists() {
         System.setProperty(KieServerConstants.KIE_SERVER_PROCESS_INSTANCE_CONTAINER_LOCATOR, "NotExistsContainerLocator");
-        ProcessContainerLocatorProvider.get().readSystemProperty();
 
         ProcessContainerLocatorProvider.get().getLocator(1L);
     }
