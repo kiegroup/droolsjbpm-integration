62dc5088ebbcff9ca6141be20088627b4e3168f6
ruromero <rromerom@redhat.com>
2021-12-22 18:02:02 +0100

    [RHPAM-4038] Add client certificate tests
    Signed-off-by: ruromero <rromerom@redhat.com>

--- kie-server-parent/kie-server-remote/kie-server-client/src/test/java/org/kie/server/client/BaseKieServicesClientTest.java
+++ kie-server-parent/kie-server-remote/kie-server-client/src/test/java/org/kie/server/client/BaseKieServicesClientTest.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -15,26 +15,35 @@
 
 package org.kie.server.client;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Collections;
+
 import org.junit.Rule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.util.Collections;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 public abstract class BaseKieServicesClientTest {
     private static Logger logger = LoggerFactory.getLogger(BaseKieServicesClientTest.class);
 
     private final   int    port              = findFreePort();
+    private final int sslPort = findFreePort();
     protected final String mockServerBaseUri = "http://localhost:" + port;
+    protected final String mockSecureServerBaseUri = "https://localhost:" + sslPort;
     protected final KieServicesConfiguration config;
+    protected final KieServicesConfiguration sslConfig;
 
     public BaseKieServicesClientTest() {
         try {
             config = KieServicesFactory.newRestConfiguration( mockServerBaseUri, null, null );
             // set capabilities so the client will not to try to get them from the server
+            config.setCapabilities(Collections.emptyList());
+
+            sslConfig = KieServicesFactory.newRestConfiguration(mockSecureServerBaseUri, null, null);
             config.setCapabilities(Collections.emptyList());
         } catch ( Exception e ) {
             // nothing to do.
@@ -45,7 +54,15 @@ public abstract class BaseKieServicesClientTest {
     // we need the rule (and thus the mock server) per class so that the tests can be executed in parallel without affecting
     // one another. That means each test class needs it own port that the server listens on
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule( port );
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig()
+            .port(port)
+            .httpsPort(sslPort)
+            .needClientAuth(true)
+            .keystorePath("src/test/resources/2wayssl/wiremock.jks")
+            .keystorePassword("password")
+            .trustStorePath("src/test/resources/2wayssl/server-truststore.jks")
+            .trustStorePassword("password")
+    );
 
     public static int findFreePort() {
         int port = 0;
