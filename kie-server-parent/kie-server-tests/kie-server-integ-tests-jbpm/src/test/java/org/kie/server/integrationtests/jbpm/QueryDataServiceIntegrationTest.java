c78d5b9abd15e7d8b555eea681da13ec8637193e
Karel Suta <ksuta@redhat.com>
2023-05-02 12:25:00 +0200

    [7.67.x] Stabilize QueryDataServiceIntegrationTest.testErrorHandlingFailedToSignal()
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/java/org/kie/server/integrationtests/jbpm/QueryDataServiceIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/java/org/kie/server/integrationtests/jbpm/QueryDataServiceIntegrationTest.java
@@ -774,7 +774,8 @@ public class QueryDataServiceIntegrationTest extends JbpmKieServerBaseIntegratio
                 // expected
             }
 
-            errors = queryClient.query(query.getName(), QueryServicesClient.QUERY_MAP_ERROR, 0, 10, ExecutionErrorInstance.class);
+            // Database can contain errors from tests executed before, returning up to 100 errors to make sure all are returned.
+            errors = queryClient.query(query.getName(), QueryServicesClient.QUERY_MAP_ERROR, 0, 100, ExecutionErrorInstance.class);
             errors = filterErrorsByProcessInstanceId(errors, processInstanceId);
             assertNotNull(errors);
             assertEquals(1, errors.size());
