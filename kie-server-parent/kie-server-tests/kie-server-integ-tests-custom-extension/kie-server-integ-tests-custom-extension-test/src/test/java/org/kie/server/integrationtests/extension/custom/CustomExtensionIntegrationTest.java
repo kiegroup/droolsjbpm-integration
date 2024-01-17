4e62f2d3d7e9cd1cbaf73a8234d301e02811b573
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-01-17 18:00:43 +0900

    [RHPAM-4876] exclusion on kie server affect custom endpoints - Add resteasy-jackson2-provider in kie-server.war

--- kie-server-parent/kie-server-tests/kie-server-integ-tests-custom-extension/kie-server-integ-tests-custom-extension-test/src/test/java/org/kie/server/integrationtests/extension/custom/CustomExtensionIntegrationTest.java
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-custom-extension/kie-server-integ-tests-custom-extension-test/src/test/java/org/kie/server/integrationtests/extension/custom/CustomExtensionIntegrationTest.java
@@ -17,23 +17,35 @@ package org.kie.server.integrationtests.extension.custom;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import org.assertj.core.api.Assertions;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJacksonProvider;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.ExecutionResults;
+import org.kie.server.api.marshalling.MarshallingFormat;
 import org.kie.server.api.model.ReleaseId;
 import org.kie.server.client.CustomExtensionClient;
 import org.kie.server.client.KieServicesClient;
+import org.kie.server.common.rest.Authenticator;
 import org.kie.server.integrationtests.shared.KieServerDeployer;
 import org.kie.server.integrationtests.shared.KieServerReflections;
 import org.kie.server.integrationtests.shared.basetests.KieServerBaseIntegrationTest;
 import org.kie.server.integrationtests.shared.basetests.RestJmsSharedBaseIntegrationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomExtensionIntegrationTest extends RestJmsSharedBaseIntegrationTest {
 
@@ -94,12 +106,48 @@ public class CustomExtensionIntegrationTest extends RestJmsSharedBaseIntegration
         ExecutionResults result = customClient.insertFireReturn(CONTAINER_ID, KIE_SESSION, persons);
 
         Object john2 = result.getValue(JOHN_OUT_IDENTIFIER);
-        Assertions.assertThat(KieServerReflections.valueOf(john2, PERSON_SURNAME_FIELD)).isEqualTo(JOHN_EXPECTED_SURNAME);
+        assertThat(KieServerReflections.valueOf(john2, PERSON_SURNAME_FIELD)).isEqualTo(JOHN_EXPECTED_SURNAME);
         Object mary2 = result.getValue(MARY_OUT_IDENTIFIER);
-        Assertions.assertThat(KieServerReflections.valueOf(mary2, PERSON_SURNAME_FIELD)).isEqualTo(MARY_SURNAME);
+        assertThat(KieServerReflections.valueOf(mary2, PERSON_SURNAME_FIELD)).isEqualTo(MARY_SURNAME);
     }
 
     private Object createPersonInstance(String firstname, String surname, ClassLoader loader) {
         return KieServerReflections.createInstance(PERSON_CLASS_NAME, loader, firstname, surname);
+    }
+
+    @Test
+    public void testCustomJsonResourceEndpoint() {
+        // RHPAM-4876
+        if (configuration.getMarshallingFormat() != MarshallingFormat.JSON) {
+            return;
+        }
+
+        Client httpClient = new ResteasyClientBuilder()
+                .establishConnectionTimeout(configuration.getTimeout(), TimeUnit.MILLISECONDS)
+                .socketTimeout(configuration.getTimeout(), TimeUnit.MILLISECONDS)
+                .register(new Authenticator(configuration.getUserName(), configuration.getPassword()))
+                .register(new ResteasyJacksonProvider())
+                .build();
+
+        Map<String, Object> testPayload = new HashMap<>();
+        testPayload.put("key1", "value1");
+        testPayload.put("key2", 123);
+
+        String endpoint = configuration.getServerUrl() + "/containers/custom-json/1";
+
+        Response response = httpClient.target(endpoint)
+                .request()
+                .post(Entity.entity(testPayload, MediaType.APPLICATION_JSON));
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        Object entity = response.readEntity(Map.class);
+        assertThat(entity).isNotNull()
+                .isInstanceOf(Map.class);
+        Map<String, Object> responseMap = (Map<String, Object>) entity;
+        assertThat(responseMap).containsEntry("key1", "value1")
+                .containsEntry("key2", 123)
+                .containsEntry("key3", "value3");
+
+        httpClient.close();
     }
 }
