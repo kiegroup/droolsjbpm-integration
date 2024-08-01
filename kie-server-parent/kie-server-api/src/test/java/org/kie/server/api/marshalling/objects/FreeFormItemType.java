3af4ed06f0178cc0d149a3f23ba2c96257d25e54
Enrique <enrique.gonzalez.martinez1@ibm.com>
2024-08-01 08:25:49 +0200

    [FIX] custom fix when xml adapter has names (#3052)

---
+++ kie-server-parent/kie-server-api/src/test/java/org/kie/server/api/marshalling/objects/FreeFormItemType.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.server.api.marshalling.objects;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "FreeFormItemType", propOrder = {
+    "itemValue"
+})
+public class FreeFormItemType implements Serializable, Cloneable
+{
+
+    private final static long serialVersionUID = 1L;
+
+    @XmlElement(required = true)
+    protected String itemValue;
+
+    /**
+     * Gets the value of the itemValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getItemValue() {
+        return itemValue;
+    }
+
+    /**
+     * Sets the value of the itemValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setItemValue(String value) {
+        this.itemValue = value;
+    }
+
+}
